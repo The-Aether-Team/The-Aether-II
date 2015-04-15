9c599fe48c9613abf77a9a79ad370c35f0ed734f
Brendan Freeman <brenwwe@hotmail.com>
2015-04-15 20:17:25 +1000

    Added base ItemAetherFood.class.

---
+++ src/main/java/com/gildedgames/aether/items/consumables/ItemAetherFood.java
@@ -0,0 +1,18 @@
+package com.gildedgames.aether.items.consumables;
+
+import net.minecraft.item.ItemFood;
+
+public class ItemAetherFood extends ItemFood 
+{
+
+	public ItemAetherFood(int amount, boolean isWolfFood) 
+	{
+		super(amount, isWolfFood);
+	}
+	
+	public ItemAetherFood(int amount, float saturation, boolean isWolfFood)
+    {
+		super(amount, saturation, isWolfFood);
+    }
+
+}
