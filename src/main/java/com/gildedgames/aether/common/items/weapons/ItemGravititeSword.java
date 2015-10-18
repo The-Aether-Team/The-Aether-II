4c60f172bcc2272d70bed35bbabe6355e15927b6
Collin1971 <collin785@gmail.com>
2015-10-18 15:08:39 -0500

    Added Zanite and Gravitite Sword ability

--- src/main/java/com/gildedgames/aether/common/items/weapons/ItemGravititeSword.java
+++ src/main/java/com/gildedgames/aether/common/items/weapons/ItemGravititeSword.java
@@ -1,5 +1,6 @@
 package com.gildedgames.aether.common.items.weapons;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
@@ -13,6 +14,21 @@ public class ItemGravititeSword extends ItemAetherSword
 	public ItemGravititeSword()
 	{
 		super(ToolMaterial.EMERALD);
+	}
+
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+	{
+		if (attacker.isSneaking())
+		{
+			// This prevents players from sending entities into the air rapidly
+			if (target.hurtTime == target.maxHurtTime && target.deathTime <= 0)
+			{
+				target.addVelocity(0.0D, 0.5D, 0.0D);
+			}
+		}
+
+		return super.hitEntity(stack, target, attacker);
 	}
 
 	@Override
--- src/main/java/com/gildedgames/aether/common/items/weapons/ItemZaniteSword.java
+++ src/main/java/com/gildedgames/aether/common/items/weapons/ItemZaniteSword.java
@@ -1,7 +1,11 @@
 package com.gildedgames.aether.common.items.weapons;
 
+import com.gildedgames.aether.common.items.ItemsAether;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -13,6 +17,32 @@ public class ItemZaniteSword extends ItemAetherSword
 	public ItemZaniteSword()
 	{
 		super(ToolMaterial.IRON);
+	}
+
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+	{
+		float damage = 6f + (2.0F * stack.getItemDamage() / stack.getItem().getMaxDamage());
+
+		if (target instanceof EntityPlayer)
+		{
+			target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), damage);
+		}
+		else
+		{
+			target.attackEntityFrom(DamageSource.causeMobDamage(attacker), damage);
+		}
+
+		stack.damageItem(1, attacker);
+
+		return false;
+	}
+
+	@Override
+	public float getDamageVsEntity()
+	{
+		// We apply our own DamageSource in hitEntity(stack, target, attacker).
+		return 0.0f;
 	}
 
 	@Override
