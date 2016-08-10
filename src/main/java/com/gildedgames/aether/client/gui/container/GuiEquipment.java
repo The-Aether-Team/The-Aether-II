44389dc920bc6affe84df3cef737f15c88639f71
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-08-11 07:59:34 +0800

    Implemented button to disable pattern rendering

--- src/main/java/com/gildedgames/aether/client/gui/container/GuiEquipment.java
+++ src/main/java/com/gildedgames/aether/client/gui/container/GuiEquipment.java
@@ -7,6 +7,10 @@ import com.gildedgames.aether.api.player.IPlayerAetherCapability;
 import com.gildedgames.aether.api.items.properties.ItemEquipmentType;
 import com.gildedgames.aether.common.entities.companions.EntityCompanion;
 import com.gildedgames.aether.common.player.PlayerAether;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
@@ -37,6 +41,8 @@ public class GuiEquipment extends GuiContainer
 
 	private final PlayerAether aePlayer;
 
+	private static final PatternButton patternButton = new PatternButton(0, 0, 0, true);
+
 	public GuiEquipment(PlayerAether aePlayer)
 	{
 		super(new ContainerEquipment(aePlayer));
@@ -55,6 +61,11 @@ public class GuiEquipment extends GuiContainer
 
 		this.xSize = 179 * 2;
 		this.ySize = 169;
+
+		this.patternButton.xPosition = this.width / 2 + 179 - 26;
+		this.patternButton.yPosition = this.height / 2 - (166 / 2) + 7;
+
+		this.addButton(this.patternButton);
 	}
 
 	@Override
@@ -68,11 +79,14 @@ public class GuiEquipment extends GuiContainer
 
 		this.drawTexturedModalRect(this.width / 2 - 90 - 179 / 2, this.height / 2 - 169 / 2, 0, 0, 179, 169);
 
-		//this.mc.renderEngine.bindTexture(textureAccessoriesPattern);
+		if (this.patternButton.pressedDown)
+		{
+			this.mc.renderEngine.bindTexture(textureAccessoriesPattern);
 
-		//GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
+			GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 
-		//this.drawTexturedModalRect(this.width / 2 - 90 - 179 / 2, this.height / 2 - 150 / 2, 0, 0, 179, 150);
+			this.drawTexturedModalRect(this.width / 2 - 90 - 179 / 2, this.height / 2 - 169 / 2, 0, 0, 179, 169);
+		}
 
 		this.mc.renderEngine.bindTexture(aePlayer.getPlayer().capabilities.isCreativeMode ? textureBackpackCreative : textureBackpack);
 
@@ -80,11 +94,14 @@ public class GuiEquipment extends GuiContainer
 
 		this.fontRendererObj.drawString(I18n.format("container.crafting"), this.width / 2 + (this.aePlayer.getPlayer().capabilities.isCreativeMode ? 70 : 51), this.height / 2 - 135 / 2, 4210752);
 
-		//this.mc.renderEngine.bindTexture(aePlayer.getPlayer().capabilities.isCreativeMode ? textureBackpackCreativePattern : textureBackpackPattern);
+		if (this.patternButton.pressedDown)
+		{
+			this.mc.renderEngine.bindTexture(aePlayer.getPlayer().capabilities.isCreativeMode ? textureBackpackCreativePattern : textureBackpackPattern);
 
-		//GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
+			GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 
-		//this.drawTexturedModalRect(this.width / 2 + 90 - 176 / 2, this.height / 2 - 166 / 2, 0, 0, 176, 166);
+			this.drawTexturedModalRect(this.width / 2 + 90 - 176 / 2, this.height / 2 - 166 / 2, 0, 0, 176, 166);
+		}
 
 		this.drawPlayer(mouseX, mouseY);
 
@@ -168,7 +185,7 @@ public class GuiEquipment extends GuiContainer
 
 		GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 
-		GuiInventory.drawEntityOnScreen(this.width / 2 - 48, this.height / 2 + 18, 40, (this.guiLeft + 88) - mouseX, (this.guiTop + 42) - mouseY, this.mc.thePlayer);
+		GuiInventory.drawEntityOnScreen(this.width / 2 - 48, this.height / 2 + 10, 37, (this.guiLeft + 88) - mouseX, (this.guiTop + 42) - mouseY, this.mc.thePlayer);
 
 		//		if (player.currentCompanion != null)
 		//		{
@@ -196,4 +213,92 @@ public class GuiEquipment extends GuiContainer
 	//		this.mc.fontRendererObj.drawStringWithShadow("x", coinX - ((this.mc.fontRendererObj.getStringWidth("x" + String.valueOf(coinAmount)) / 2) + 2) + 6, coinY + 1, 0xffffffff);
 	//		this.mc.fontRendererObj.drawStringWithShadow(String.valueOf(coinAmount), coinX - ((this.mc.fontRendererObj.getStringWidth("x" + String.valueOf(coinAmount)) / 2) + 2) + 13, coinY + 2, 0xffffffff);
 	//	}
+
+	private static class PatternButton extends GuiButton
+	{
+
+		private static final ResourceLocation PRESSED = new ResourceLocation("aether", "textures/gui/inventory/accessories/patternButtonPressed.png");
+
+		private static final ResourceLocation UNPRESSED = new ResourceLocation("aether", "textures/gui/inventory/accessories/patternButtonUnpressed.png");
+
+		private static final ResourceLocation PRESSED_HOVERED = new ResourceLocation("aether", "textures/gui/inventory/accessories/patternButtonPressedHovered.png");
+
+		private static final ResourceLocation UNPRESSED_HOVERED = new ResourceLocation("aether", "textures/gui/inventory/accessories/patternButtonUnpressedHovered.png");
+
+		private static final ResourceLocation SYMBOL = new ResourceLocation("aether", "textures/gui/inventory/accessories/patternSymbol.png");
+
+		private static final ResourceLocation SYMBOL_HOVERED = new ResourceLocation("aether", "textures/gui/inventory/accessories/patternSymbolHovered.png");
+
+		private boolean pressedDown;
+
+		public PatternButton(int buttonId, int x, int y, boolean pressedDown)
+		{
+			super(buttonId, x, y, 18, 14, "");
+
+			this.pressedDown = pressedDown;
+		}
+
+		@Override
+		public void mouseReleased(int mouseX, int mouseY)
+		{
+			this.pressedDown = !this.pressedDown;
+		}
+
+		@Override
+		public void drawButton(Minecraft mc, int mouseX, int mouseY)
+		{
+			if (this.visible)
+			{
+				this.hovered = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+
+				if (pressedDown)
+				{
+					if (this.hovered)
+					{
+						mc.getTextureManager().bindTexture(PRESSED_HOVERED);
+					}
+					else
+					{
+						mc.getTextureManager().bindTexture(PRESSED);
+					}
+				}
+				else
+				{
+					if (this.hovered)
+					{
+						mc.getTextureManager().bindTexture(UNPRESSED_HOVERED);
+					}
+					else
+					{
+						mc.getTextureManager().bindTexture(UNPRESSED);
+					}
+				}
+
+				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
+				GlStateManager.enableBlend();
+				GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+				GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+
+				this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 0, this.width, this.height);
+
+				GlStateManager.popMatrix();
+
+				this.mouseDragged(mc, mouseX, mouseY);
+
+				if (this.hovered)
+				{
+					mc.getTextureManager().bindTexture(SYMBOL_HOVERED);
+				}
+				else
+				{
+					mc.getTextureManager().bindTexture(SYMBOL);
+				}
+
+				this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 0, this.width, this.height);
+			}
+		}
+
+	}
+
 }
