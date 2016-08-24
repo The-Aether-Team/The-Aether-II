e7935026f4cd71f2f07523717f0dbd7b19eecbea
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-08-25 01:08:42 +0800

    Golden Amber now drops no matter what axe level. Made amber double drop with Skyroot Axes. Fixes #52

--- src/main/java/com/gildedgames/aether/common/blocks/natural/BlockGoldenOakLog.java
+++ src/main/java/com/gildedgames/aether/common/blocks/natural/BlockGoldenOakLog.java
@@ -1,9 +1,12 @@
 package com.gildedgames.aether.common.blocks.natural;
 
 import com.gildedgames.aether.common.blocks.BlocksAether;
+import com.gildedgames.aether.common.blocks.util.ISkyrootMinable;
 import com.gildedgames.aether.common.items.ItemsAether;
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,39 +15,40 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
-public class BlockGoldenOakLog extends BlockAetherLog
+public class BlockGoldenOakLog extends BlockAetherLog implements ISkyrootMinable
 {
 	@Override
 	public void harvestBlock(World world, EntityPlayer player, BlockPos pos, IBlockState state, TileEntity te, ItemStack stack)
 	{
-		if (player.getActiveItemStack() != null)
-		{
-			Item heldItem = player.getActiveItemStack().getItem();
-
-			if (heldItem instanceof ItemTool)
-			{
-				Item.ToolMaterial material = ((ItemTool) heldItem).getToolMaterial();
-
-				if (material.getHarvestLevel() >= 2)
-				{
-					this.dropGoldenAmber(world, pos, world.rand);
-				}
-			}
-		}
+		Block.spawnAsEntity(world, pos, new ItemStack(ItemsAether.golden_amber, world.rand.nextInt(3) + 1));
 
 		super.harvestBlock(world, player, pos, state, te, stack);
-	}
-
-	private void dropGoldenAmber(World world, BlockPos pos, Random random)
-	{
-		Block.spawnAsEntity(world, pos, new ItemStack(ItemsAether.golden_amber, random.nextInt(3) + 1));
 	}
 
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune)
 	{
 		return Item.getItemFromBlock(BlocksAether.skyroot_log);
+	}
+
+	@Override
+	public boolean canBlockDropDoubles(EntityLivingBase player, ItemStack stack, IBlockState state)
+	{
+		return true;
+	}
+
+	@Override
+	public Collection<ItemStack> getAdditionalDrops(World world, BlockPos pos, IBlockState state, EntityLivingBase living)
+	{
+		List<ItemStack> drops = Lists.newArrayList();
+
+		drops.add(new ItemStack(state.getBlock()));
+		drops.add(new ItemStack(ItemsAether.golden_amber, world.rand.nextInt(3) + 1));
+
+		return drops;
 	}
 }
