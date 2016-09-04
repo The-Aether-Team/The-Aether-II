e82da8f3941bb79eb4c7c25026254775bd6f24f8
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-09-04 22:01:01 +0800

    Element attack types now display visual particles when attacking a creature.

--- src/main/java/com/gildedgames/aether/common/capabilities/entity/properties/EntityProperties.java
+++ src/main/java/com/gildedgames/aether/common/capabilities/entity/properties/EntityProperties.java
@@ -6,14 +6,21 @@ import com.gildedgames.aether.api.capabilites.entity.properties.ElementalState;
 import com.gildedgames.aether.api.capabilites.entity.properties.IEntityProperties;
 import com.gildedgames.aether.api.capabilites.entity.properties.IEntityPropertiesCapability;
 import com.google.common.collect.Lists;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.List;
+
+import static net.minecraft.realms.Tezzelator.t;
 
 public class EntityProperties implements IEntityPropertiesCapability
 {
@@ -104,10 +111,16 @@ public class EntityProperties implements IEntityPropertiesCapability
 			return;
 		}
 
+		List<ElementalState> elements = Lists.newArrayList();
 		double overallDamage = 0.0D;
 
 		for (ElementalDamageSource damageSource : sourceProperties.getElementalDamageSources())
 		{
+			if (!elements.contains(damageSource.getElementalState()))
+			{
+				elements.add(damageSource.getElementalState());
+			}
+
 			overallDamage += damageSource.getDamage();
 		}
 
@@ -123,6 +136,85 @@ public class EntityProperties implements IEntityPropertiesCapability
 		double finalResult = minusDamageSources + withModifiers;
 
 		event.setAmount((float) finalResult);
+
+		World world = victim.getEntityWorld();
+
+		if (world instanceof WorldServer)
+		{
+			WorldServer worldServer = (WorldServer)world;
+
+			if (elements.contains(ElementalState.WATER))
+			{
+				for (int i = 0; i < 4; ++i)
+				{
+					double motionX = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionY = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionZ = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+
+					worldServer.spawnParticle(EnumParticleTypes.WATER_SPLASH, victim.posX + motionX, victim.posY + (victim.height / 2) + motionY, victim.posZ + motionZ, 1, motionX, motionY, motionZ, (world.rand.nextBoolean() ? 0.05D : -0.05D));
+				}
+			}
+
+			if (elements.contains(ElementalState.FROST))
+			{
+				for (int i = 0; i < 4; ++i)
+				{
+					double motionX = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionY = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionZ = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+
+					worldServer.spawnParticle(EnumParticleTypes.BLOCK_DUST, victim.posX + motionX, victim.posY + (victim.height / 2) + motionY, victim.posZ + motionZ, 1, motionX, motionY, motionZ, (world.rand.nextBoolean() ? 0.05D : -0.05D), Block.getStateId(Blocks.ICE.getDefaultState()));
+				}
+			}
+
+			if (elements.contains(ElementalState.EARTH))
+			{
+				for (int i = 0; i < 4; ++i)
+				{
+					double motionX = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionY = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionZ = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+
+					worldServer.spawnParticle(EnumParticleTypes.BLOCK_DUST, victim.posX + motionX, victim.posY + (victim.height / 2) + motionY, victim.posZ + motionZ, 1, motionX, motionY, motionZ, (world.rand.nextBoolean() ? 0.05D : -0.05D), Block.getStateId(Blocks.DIRT.getDefaultState()));
+				}
+			}
+
+			if (elements.contains(ElementalState.AIR))
+			{
+				for (int i = 0; i < 3; ++i)
+				{
+					double motionX = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionY = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionZ = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+
+					worldServer.spawnParticle(EnumParticleTypes.CLOUD, victim.posX + motionX, victim.posY + (victim.height / 2) + motionY, victim.posZ + motionZ, 1, motionX, motionY, motionZ, (world.rand.nextBoolean() ? 0.05D : -0.05D));
+				}
+			}
+
+			if (elements.contains(ElementalState.FIRE))
+			{
+				for (int i = 0; i < 3; ++i)
+				{
+					double motionX = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionY = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionZ = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+
+					worldServer.spawnParticle(EnumParticleTypes.FLAME, victim.posX + motionX, victim.posY + (victim.height / 2) + motionY, victim.posZ + motionZ, 1, motionX, motionY, motionZ, (world.rand.nextBoolean() ? 0.05D : -0.05D));
+				}
+			}
+
+			if (elements.contains(ElementalState.BLIGHT))
+			{
+				for (int i = 0; i < 3; ++i)
+				{
+					double motionX = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionY = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+					double motionZ = (world.rand.nextBoolean() ? 0.25D : -0.25D) * world.rand.nextFloat();
+
+					worldServer.spawnParticle(EnumParticleTypes.SPELL_WITCH, victim.posX + motionX, victim.posY + (victim.height / 2) + motionY, victim.posZ + motionZ, 1, motionX, motionY, motionZ, (world.rand.nextBoolean() ? 0.05D : -0.05D));
+				}
+			}
+		}
 	}
 
 	public static class Storage implements Capability.IStorage<IEntityPropertiesCapability>
