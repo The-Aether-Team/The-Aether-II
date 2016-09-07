abda261fb178921ca3e82838427e2b3fec2e8f3c
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-09-07 08:41:47 +0800

    Adjusted ring-rendering for non-slim players.

--- src/main/java/com/gildedgames/aether/client/renderer/entities/living/RenderPlayerHelper.java
+++ src/main/java/com/gildedgames/aether/client/renderer/entities/living/RenderPlayerHelper.java
@@ -52,12 +52,12 @@ public class RenderPlayerHelper
 
 		if (ring1 != null)
 		{
-			RenderPlayerHelper.renderItem(ring1, event.getPartialTicks(), event.getInterpolatedPitch(), event.getSwingProgress(), event.getEquipProgress(), skinType == "slim" ? 0.075F : 0.11F, skinType == "slim" ? 0.0752F : 0.112F, -0.25F);
+			RenderPlayerHelper.renderItem(ring1, event.getPartialTicks(), event.getInterpolatedPitch(), event.getSwingProgress(), event.getEquipProgress(), skinType == "slim" ? 0.075F : 0.09F, skinType == "slim" ? 0.0752F : 0.112F, -0.20F);
 		}
 
 		if (ring2 != null)
 		{
-			RenderPlayerHelper.renderItem(ring2, event.getPartialTicks(), event.getInterpolatedPitch(), event.getSwingProgress(), event.getEquipProgress(), skinType == "slim" ? 0.18F : 0.22F, skinType == "slim" ? 0.017F : 0.052F, -0.24F);
+			RenderPlayerHelper.renderItem(ring2, event.getPartialTicks(), event.getInterpolatedPitch(), event.getSwingProgress(), event.getEquipProgress(), skinType == "slim" ? 0.18F : 0.195F, skinType == "slim" ? 0.017F : 0.0552F, -0.19F);
 		}
 	}
 
