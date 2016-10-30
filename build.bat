8c979cad3afa6478bc7a39d9e2a296652bd364f6
Collin1971 <collin785@gmail.com>
2016-10-30 15:59:07 -0500

    Gradle builds now start with more memory, should curb OutOfMemory exceptions

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 title Building the Aether II...
+set GRADLE_OPTS=-Xmx2G
 call gradlew.bat build
 
 pause
--- setup_decomp_workspace.bat
+++ setup_decomp_workspace.bat
@@ -1,5 +1,6 @@
 @echo off
 title Setting up your Forge workspace...
+set GRADLE_OPTS=-Xmx2G
 call gradlew.bat setupDecompWorkspace
 
 pause
