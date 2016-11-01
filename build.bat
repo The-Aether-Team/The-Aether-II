3786815108f2974eef6c792863128e433caeecd8
Collin1971 <collin785@gmail.com>
2016-11-01 09:43:30 -0500

    Moved Gradle arguments to gradle.properties

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 title Building the Aether II...
-set GRADLE_OPTS=-Xmx2G
 call gradlew.bat build
 
 pause
--- setup_decomp_workspace.bat
+++ setup_decomp_workspace.bat
@@ -1,6 +1,5 @@
 @echo off
 title Setting up your Forge workspace...
-set GRADLE_OPTS=-Xmx2G
 call gradlew.bat setupDecompWorkspace
 
 pause
