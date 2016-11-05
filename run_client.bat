0c79dfb03ea3601d828ac22120fc9eff2ce6a452
Collin1971 <collin785@gmail.com>
2016-11-05 15:31:07 -0500

    Added run scripts

---
+++ run_client.bat
@@ -0,0 +1,6 @@
+@echo off
+title Running the Aether II...
+call gradlew.bat runClient
+
+pause
+exit
---
+++ run_server.bat
@@ -0,0 +1,6 @@
+@echo off
+title Running the Aether II...
+call gradlew.bat runServer
+
+pause
+exit
