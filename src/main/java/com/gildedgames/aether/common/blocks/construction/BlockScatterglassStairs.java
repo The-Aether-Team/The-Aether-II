c2649c7ade17ed90002479b8fe9119d97945070a
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2016-11-08 23:12:13 +0800

    Fixed Scatterglass Stairs rendering. Fixes #282.

--- src/main/java/com/gildedgames/aether/common/blocks/construction/BlockScatterglassStairs.java
+++ src/main/java/com/gildedgames/aether/common/blocks/construction/BlockScatterglassStairs.java
@@ -1,7 +1,10 @@
 package com.gildedgames.aether.common.blocks.construction;
 
+import com.gildedgames.aether.common.blocks.BlocksAether;
 import com.gildedgames.aether.common.blocks.util.BlockCustomStairs;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
@@ -15,13 +18,24 @@ public class BlockScatterglassStairs extends BlockCustomStairs
 	public BlockScatterglassStairs(IBlockState state)
 	{
 		super(state);
+
+		this.setLightOpacity(3);
+	}
+
+	@Override
+	public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face)
+	{
+		return false;
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side)
 	{
-		return super.shouldSideBeRendered(state, world, pos, side);
+		IBlockState iblockstate = world.getBlockState(pos.offset(side));
+		Block block = iblockstate.getBlock();
+
+		return block != this;
 	}
 
 	@Override
