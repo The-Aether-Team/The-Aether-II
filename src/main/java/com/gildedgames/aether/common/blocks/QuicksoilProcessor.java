dba18d171cf84dcff1089eb086512a47644f95d2
Collin1971 <collin785@gmail.com>
2016-11-10 12:43:08 -0600

    Fixed a compile error

--- src/main/java/com/gildedgames/aether/common/blocks/QuicksoilProcessor.java
+++ src/main/java/com/gildedgames/aether/common/blocks/QuicksoilProcessor.java
@@ -1,7 +1,6 @@
 package com.gildedgames.aether.common.blocks;
 
 import com.gildedgames.aether.common.ReflectionAether;
-import com.sun.xml.internal.bind.v2.model.annotation.Quick;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
