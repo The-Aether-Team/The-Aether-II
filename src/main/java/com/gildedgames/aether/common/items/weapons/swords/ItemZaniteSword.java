d165dcef12c8103468d3e924acad19d4aad0ea6b
chrisloyde <KnightBroseph@gmail.com>
2017-01-20 13:12:56 -0700

    Zanite Sword Bug - Issue semi-resolved
    Zanite Sword now follows new combat rules. The damage increase effect only affects spam attacks rather then fully charged attacks (which is a continued bug).

--- src/main/java/com/gildedgames/aether/common/items/weapons/swords/ItemZaniteSword.java
+++ src/main/java/com/gildedgames/aether/common/items/weapons/swords/ItemZaniteSword.java
@@ -6,26 +6,21 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 
+import javax.tools.Tool;
+
 public class ItemZaniteSword extends ItemAetherSword
 {
+	private float mobDamage = 6;
 	public ItemZaniteSword()
 	{
 		super(ToolMaterial.IRON, ItemAbilityType.PASSIVE);
 	}
 
+
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
 	{
-		/**************************
-		 |*	It appears that stack.getItemDamage() / stack.getItem().getMaxDamage() never returns anything >= 1. So it adds small decimals to the health decrese
-		 |* Originally I felt we could just multiply stack.getItemDamage by 2, but that essentially only allowed a max of +2 damage (so 1 more heart)
-		 |* This really doesn't feel powerful.
-		 |* what I ended up figuring out: Whatever number we multiply stack.getItemDamage() by (in this case 4) is the TOTAL extra damage that will be dealt to
-		 |* to the enemy when the sword is on its last hit. As in, the final hit of the sword will deal 4 extra damage, prior hits will deal building up to this
-		 |* point. 
-		 |* Also, this item breaks when item damage is 250. Just in case someone wanted to add an added effect or extra damage on that last hit.
-		 */
-		float damage = 6f + ((float) (stack.getItemDamage() * 4) / stack.getItem().getMaxDamage()); // on last hit, sword will deal 10 damage (5 hearts)
+		float damage = ToolMaterial.IRON.getDamageVsEntity() + ((float) (stack.getItemDamage() * 4) / stack.getItem().getMaxDamage()); // on last hit, sword will deal 10 damage (5 hearts)
 
 		if (target instanceof EntityPlayer)
 		{
@@ -37,14 +32,6 @@ public class ItemZaniteSword extends ItemAetherSword
 		}
 
 		stack.damageItem(1, attacker);
-
 		return false;
-	}
-
-	@Override
-	public float getDamageVsEntity()
-	{
-		// We apply our own DamageSource in hitEntity(stack, target, attacker).
-		return 0.0f;
 	}
 }
