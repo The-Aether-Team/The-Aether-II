cf27195976b3e7c4838f754357c22bb62eb79095
Brandon James Pearce <brandon@gildedgames.com>
2017-11-07 11:04:58 +0800

    feature: Dome shape now makes use of Non-Uniform and Uniform selection modes.

--- src/main/java/com/gildedgames/orbis/common/player/godmode/selection_types/SelectionTypeDome.java
+++ src/main/java/com/gildedgames/orbis/common/player/godmode/selection_types/SelectionTypeDome.java
@@ -3,9 +3,11 @@ package com.gildedgames.orbis.common.player.godmode.selection_types;
 import com.gildedgames.aether.api.orbis.IShape;
 import com.gildedgames.aether.api.orbis_core.data.shapes.DomeShape;
 import com.gildedgames.aether.common.AetherCore;
+import com.gildedgames.aether.common.capabilities.entity.player.PlayerAether;
 import com.gildedgames.orbis.client.player.godmode.selection_types.ISelectionTypeClient;
 import com.gildedgames.orbis.client.player.godmode.selection_types.SelectionTypeClientDome;
 import com.gildedgames.orbis.common.player.PlayerOrbisModule;
+import com.gildedgames.orbis.common.player.PlayerSelectionModule;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -40,6 +42,7 @@ public class SelectionTypeDome implements ISelectionType
 	@Override
 	public IShape createShape(final BlockPos start, final BlockPos end, final PlayerOrbisModule module, final World world)
 	{
-		return new DomeShape(start, (int) Math.sqrt(start.distanceSq(end)));
+		return new DomeShape(start, end,
+				PlayerAether.getPlayer(module.getEntity()).getSelectionModule().currentSelectionMode() == PlayerSelectionModule.CENTERED);
 	}
 }
