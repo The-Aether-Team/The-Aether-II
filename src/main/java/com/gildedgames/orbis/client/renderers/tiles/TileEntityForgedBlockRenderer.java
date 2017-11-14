e7510569b3b433d18fcbc54c58653058de441d3e
Brandon James Pearce <brandon@gildedgames.com>
2017-11-14 18:38:59 +0800

    fix: Memory leak with cache using ItemStacks as keys (not safe, might have to check BlockDataContainers for this as well)

--- src/main/java/com/gildedgames/orbis/client/renderers/tiles/TileEntityForgedBlockRenderer.java
+++ src/main/java/com/gildedgames/orbis/client/renderers/tiles/TileEntityForgedBlockRenderer.java
@@ -16,21 +16,18 @@ import com.google.common.cache.*;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.lwjgl.opengl.GL11;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -40,30 +37,23 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class TileEntityForgedBlockRenderer extends TileEntitySpecialRenderer<TileEntityForgedBlock>
-		implements RemovalListener<ItemStack, Optional<RenderBlueprint>>
+		implements RemovalListener<BlockFilterLayer, Optional<RenderBlueprint>>
 {
 
 	private static final Minecraft mc = Minecraft.getMinecraft();
 
 	public final BakedModel baked = new BakedModel();
 
-	private final LoadingCache<ItemStack, Optional<RenderBlueprint>> blueprintCache = CacheBuilder.newBuilder()
+	private final LoadingCache<BlockFilterLayer, Optional<RenderBlueprint>> blueprintCache = CacheBuilder.newBuilder()
 			.maximumSize(1000)
 			.expireAfterWrite(10, TimeUnit.MINUTES)
 			.removalListener(this)
 			.build(
-					new CacheLoader<ItemStack, Optional<RenderBlueprint>>()
+					new CacheLoader<BlockFilterLayer, Optional<RenderBlueprint>>()
 					{
 						@Override
-						public Optional<RenderBlueprint> load(final ItemStack key)
+						public Optional<RenderBlueprint> load(final BlockFilterLayer layer)
 						{
-							final BlockFilterLayer layer = ItemBlockPalette.getFilterLayer(key);
-
-							if (layer == null)
-							{
-								return Optional.absent();
-							}
-
 							int sizeX = Math.max(2, layer.getReplacementBlocks().size() / 2);
 
 							while (layer.getReplacementBlocks().size() <= (sizeX - 1) * (sizeX - 1))
@@ -102,8 +92,6 @@ public class TileEntityForgedBlockRenderer extends TileEntitySpecialRenderer<Til
 						}
 					});
 
-	private BlockRendererDispatcher blockRenderer;
-
 	private ItemStack stack;
 
 	public TileEntityForgedBlockRenderer()
@@ -122,7 +110,14 @@ public class TileEntityForgedBlockRenderer extends TileEntitySpecialRenderer<Til
 
 		try
 		{
-			final RenderBlueprint blueprint = this.blueprintCache.get(this.stack).orNull();
+			final BlockFilterLayer layer = ItemBlockPalette.getFilterLayer(this.stack);
+
+			if (layer == null)
+			{
+				return;
+			}
+
+			final RenderBlueprint blueprint = this.blueprintCache.get(layer).orNull();
 
 			if (blueprint == null)
 			{
@@ -160,68 +155,8 @@ public class TileEntityForgedBlockRenderer extends TileEntitySpecialRenderer<Til
 		}
 	}
 
-	private void renderBlock(final IBlockState state)
-	{
-		final boolean inGuiContext = OpenGLHelper.isInGuiContext();
-
-		final BlockRendererDispatcher blockRendererDispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
-
-		final IBakedModel model = blockRendererDispatcher.getModelForState(state);
-
-		final Tessellator tessellator = Tessellator.getInstance();
-		final VertexBuffer buffer = tessellator.getBuffer();
-		this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-
-		if (!inGuiContext)
-		{
-			RenderHelper.disableStandardItemLighting();
-		}
-
-		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GlStateManager.enableBlend();
-		GlStateManager.disableCull();
-
-		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
-
-		if (!inGuiContext)
-		{
-			GlStateManager.translate(0.5F, 0.5F, 0.5F);
-
-			GlStateManager.scale(0.40F, 0.40F, 0.40F);
-
-			GlStateManager.rotate(45.0F, 0.0F, 1.0F, 0.0F);
-
-			GlStateManager.translate(-0.5F, -0.5F, -0.5F);
-		}
-		else
-		{
-			GlStateManager.disableLighting();
-			this.transformForGui();
-			ItemCameraTransforms
-					.applyTransformSide(model.getItemCameraTransforms().getTransform(ItemCameraTransforms.TransformType.GUI), true);
-		}
-
-		blockRendererDispatcher.getBlockModelRenderer()
-				.renderModel(mc.world, model, state, BlockPos.ORIGIN, buffer, false);
-
-		buffer.setTranslation(0, 0, 0);
-
-		tessellator.draw();
-
-		if (!inGuiContext)
-		{
-			RenderHelper.enableStandardItemLighting();
-		}
-		else
-		{
-			GlStateManager.enableLighting();
-		}
-
-		GlStateManager.resetColor();
-	}
-
 	@Override
-	public void onRemoval(final RemovalNotification<ItemStack, Optional<RenderBlueprint>> notification)
+	public void onRemoval(final RemovalNotification<BlockFilterLayer, Optional<RenderBlueprint>> notification)
 	{
 		final Optional<RenderBlueprint> opt = notification.getValue();
 
@@ -236,11 +171,6 @@ public class TileEntityForgedBlockRenderer extends TileEntitySpecialRenderer<Til
 		{
 			blueprint.onRemoved();
 		}
-	}
-
-	public void transformForGui()
-	{
-		GlStateManager.translate(0.5F, 0.0F, 0.0F);
 	}
 
 	public static class DummyTile extends TileEntityForgedBlock
