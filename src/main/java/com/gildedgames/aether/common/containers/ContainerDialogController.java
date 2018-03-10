4799a3ef94e21cd0b6f85aa6f23cd3d50eea3425
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-03-10 17:40:25 +0800

    fix: A cleaner fix for the JEI issue in dialog controllers

--- src/main/java/com/gildedgames/aether/common/containers/ContainerDialogController.java
+++ src/main/java/com/gildedgames/aether/common/containers/ContainerDialogController.java
@@ -1,12 +1,10 @@
 package com.gildedgames.aether.common.containers;
 
-import com.gildedgames.orbis.client.gui.util.GuiFrameNoContainer;
-import com.gildedgames.orbis.client.rect.Pos2D;
-import com.gildedgames.orbis.client.rect.Rect;
-import com.gildedgames.orbis.client.rect.RectBuilder;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
 
-public class ContainerDialogController extends GuiFrameNoContainer implements Rect {
+public class ContainerDialogController extends Container
+{
 
 	private final EntityPlayer player;
 
@@ -15,99 +13,9 @@ public class ContainerDialogController extends GuiFrameNoContainer implements Re
 		this.player = player;
 	}
 
-
 	@Override
-	public void init() {
-
-	}
-
-	@Override
-	public RectBuilder rebuild() {
-		return null;
-	}
-
-	@Override
-	public float degrees() {
-		return 0;
-	}
-
-	@Override
-	public float originX() {
-		return 0;
-	}
-
-	@Override
-	public float originY() {
-		return 0;
-	}
-
-	@Override
-	public float scale() {
-		return 0;
-	}
-
-	@Override
-	public float maxX() {
-		return 0;
-	}
-
-	@Override
-	public float maxY() {
-		return 0;
-	}
-
-	@Override
-	public float centerX() {
-		return 0;
-	}
-
-	@Override
-	public float centerY() {
-		return 0;
-	}
-
-	@Override
-	public Pos2D center() {
-		return null;
-	}
-
-	@Override
-	public float x() {
-		return 0;
-	}
-
-	@Override
-	public float y() {
-		return 0;
-	}
-
-	@Override
-	public float width() {
-		return 0;
-	}
-
-	@Override
-	public float height() {
-		return 0;
-	}
-
-	@Override
-	public boolean isCenteredX() {
-		return false;
-	}
-
-	@Override
-	public boolean isCenteredY() {
-		return false;
-	}
-
-	@Override
-	public boolean intersects(float v, float v1) {
-		return false;
-	}
-
-	@Override
-	public boolean intersects(Rect rect) {
-		return false;
+	public boolean canInteractWith(final EntityPlayer playerIn)
+	{
+		return playerIn.getDistanceSq(this.player) <= 64.0D;
 	}
 }
