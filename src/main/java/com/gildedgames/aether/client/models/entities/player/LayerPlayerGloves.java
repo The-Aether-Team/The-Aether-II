ac68e3a67cf3efa7a48899008417da8a9a7be99d
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-05-04 10:38:20 +0800

    fix: Rotation issues for Steve. Need to give new textures to Alex gloves

--- src/main/java/com/gildedgames/aether/client/models/entities/player/LayerPlayerGloves.java
+++ src/main/java/com/gildedgames/aether/client/models/entities/player/LayerPlayerGloves.java
@@ -3,38 +3,38 @@ package com.gildedgames.aether.client.models.entities.player;
 import com.gildedgames.aether.common.capabilities.entity.player.PlayerAether;
 import com.gildedgames.aether.common.items.armor.ItemAetherGloves;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumHandSide;
 
 public class LayerPlayerGloves extends LayerBipedArmor
 {
 	private final RenderLivingBase<?> renderer;
+
+	private ModelPlayer modelArmorSlim;
 
 	public LayerPlayerGloves(RenderLivingBase<?> rendererIn)
 	{
 		super(rendererIn);
 
 		this.renderer = rendererIn;
-		this.modelArmor = new ModelBiped(1.0f);
+		this.modelArmor = new ModelBiped(0.15f);
+		this.modelArmorSlim = new ModelPlayer(0.15f, true);
 	}
 
 	@Override
 	public void doRenderLayer(EntityLivingBase entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks,
 			float netHeadYaw, float headPitch, float scale)
 	{
-		this.renderGloves(PlayerAether.getPlayer(entity), limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale,
-				EnumHandSide.RIGHT);
-		this.renderGloves(PlayerAether.getPlayer(entity), limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale,
-				EnumHandSide.LEFT);
+		this.renderGloves(PlayerAether.getPlayer(entity), limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
 	}
 
 	private void renderGloves(PlayerAether player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks,
-			float netHeadYaw, float headPitch, float scale, EnumHandSide side)
+			float netHeadYaw, float headPitch, float scale)
 	{
 		ItemStack stack = player.getEquipmentModule().getInventory().getStackInSlot(2);
 
@@ -44,29 +44,15 @@ public class LayerPlayerGloves extends LayerBipedArmor
 
 			String skinType = DefaultPlayerSkin.getSkinType(player.getEntity().getUniqueID());
 
-			ModelBiped t = this.modelArmor;
+			ModelBiped t = skinType.equals("slim") ? this.modelArmorSlim : this.modelArmor;
 
-			boolean left = side == EnumHandSide.RIGHT;
-			boolean right = side == EnumHandSide.LEFT;
-
-			t.bipedRightArm.showModel = left;
-			t.bipedLeftArm.showModel = right;
-
-			t.setModelAttributes(this.renderer.getMainModel());
-			t.setLivingAnimations(player.getEntity(), limbSwing, limbSwingAmount, partialTicks);
+			t.bipedRightArm.showModel = true;
+			t.bipedLeftArm.showModel = true;
 
 			GlStateManager.pushMatrix();
 
-			if (skinType.equals("slim"))
-			{
-				GlStateManager.scale(0.7F, 1F, 0.7F);
-				GlStateManager.translate(left ? -0.15 : 0.15, -0.04F, -0.03F);
-			}
-			else
-			{
-				GlStateManager.scale(0.725F, 1F, 0.725F);
-				GlStateManager.translate(left ? -0.17 : 0.17, -0.05F, -0.03F);
-			}
+			t.setModelAttributes(this.renderer.getMainModel());
+			t.setLivingAnimations(player.getEntity(), limbSwing, limbSwingAmount, partialTicks);
 
 			this.renderer.bindTexture(glove.getGloveTexture(player.getEntity()));
 
