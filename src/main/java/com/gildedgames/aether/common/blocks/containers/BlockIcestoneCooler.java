c4eebca15d0d6b31d8744733deb7e21c6d833877
devilbringer <devilbringerpayn@gmail.com>
2018-05-06 16:39:18 +0100

    fix: Non-Full block Aether Utility Blocks no longer act as full blocks.

--- src/main/java/com/gildedgames/aether/common/blocks/containers/BlockIcestoneCooler.java
+++ src/main/java/com/gildedgames/aether/common/blocks/containers/BlockIcestoneCooler.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -19,6 +20,7 @@ import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockIcestoneCooler extends BlockContainer
@@ -35,6 +37,12 @@ public class BlockIcestoneCooler extends BlockContainer
 		this.setSoundType(SoundType.WOOD);
 
 		this.setDefaultState(this.getBlockState().getBaseState().withProperty(PROPERTY_FACING, EnumFacing.NORTH));
+	}
+
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+	{
+		return BlockFaceShape.UNDEFINED;
 	}
 
 	@Override
