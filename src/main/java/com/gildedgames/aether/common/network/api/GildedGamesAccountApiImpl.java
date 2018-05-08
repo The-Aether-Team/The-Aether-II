f4f7b22e329f3524779ec1de63845dda39b53913
JellySquid <jellysquid@protonmail.com>
2018-05-07 22:12:36 -0500

    fix: Removed usages of Apache HTTPComponents on server-side, resolves startup crash.
    This needs to be extended to GameAnalytics at one point or another, but the net code there is much less straightforward.

--- src/main/java/com/gildedgames/aether/common/network/api/GildedGamesAccountApiImpl.java
+++ src/main/java/com/gildedgames/aether/common/network/api/GildedGamesAccountApiImpl.java
@@ -2,45 +2,51 @@ package com.gildedgames.aether.common.network.api;
 
 import com.gildedgames.aether.api.net.IGildedGamesAccountApi;
 import com.gildedgames.aether.api.net.data.UserFeatures;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
 import com.google.gson.Gson;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.UUID;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class GildedGamesAccountApiImpl implements IGildedGamesAccountApi
 {
 	private static final String API_BASE = "https://api.gildedgames.com";
 
-	private final ListeningExecutorService service = MoreExecutors.listeningDecorator(new ThreadPoolExecutor(0, 2, 1L, TimeUnit.MINUTES, new LinkedBlockingQueue<>()));
-
-	private final HttpClient client = HttpClientBuilder.create().build();
+	private final ExecutorService service = new ThreadPoolExecutor(0, 2, 1L, TimeUnit.MINUTES, new LinkedBlockingQueue<>());
 
 	private final Gson gson = new Gson();
 
 	public Future<UserFeatures> retrieveUserFeatures(UUID uuid)
 	{
 		return this.service.submit(() -> {
-			HttpGet request = new HttpGet(GildedGamesAccountApiImpl.API_BASE + "/minecraft/user/" + uuid.toString() + "/features");
-			HttpResponse response = this.client.execute(request);
+			URL url = new URL(GildedGamesAccountApiImpl.API_BASE + "/minecraft/user/" + uuid.toString() + "/features");
 
-			try (InputStream stream = response.getEntity().getContent()) {
-				try (InputStreamReader reader = new InputStreamReader(stream)) {
-					UserFeatures features = this.gson.fromJson(reader, UserFeatures.class);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(15000);
+			connection.setReadTimeout(15000);
+			connection.connect();
 
-					return features;
+			if (connection.getResponseCode() != 200)
+			{
+				throw new IOException("Expected 200 OK response");
+			}
+
+			UserFeatures features;
+
+			try (InputStream stream = connection.getInputStream())
+			{
+				try (InputStreamReader reader = new InputStreamReader(stream))
+				{
+					features = this.gson.fromJson(reader, UserFeatures.class);
 				}
 			}
+
+			return features;
 		});
 	}
 }
