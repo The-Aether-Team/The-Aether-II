b8cb7b6eb376e5a97cd7dd7e3b60fd2f9d3b0954
JellySquid <jellysquid@protonmail.com>
2018-06-01 18:35:00 -0500

    change: Mobs will now avoid Quicksoil unless absolutely dire

--- src/main/java/com/gildedgames/aether/common/blocks/natural/BlockQuicksoil.java
+++ src/main/java/com/gildedgames/aether/common/blocks/natural/BlockQuicksoil.java
@@ -3,6 +3,10 @@ package com.gildedgames.aether.common.blocks.natural;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.pathfinding.PathNodeType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockQuicksoil extends Block
 {
@@ -13,5 +17,11 @@ public class BlockQuicksoil extends Block
 		this.setHardness(1.5f);
 
 		this.setSoundType(SoundType.SAND);
+	}
+
+	@Override
+	public PathNodeType getAiPathNodeType(IBlockState state, IBlockAccess world, BlockPos pos)
+	{
+		return PathNodeType.DANGER_OTHER;
 	}
 }
