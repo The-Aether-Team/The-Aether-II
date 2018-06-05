242175f82029481a92cf9251cf2aa1d42cefd4be
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-06-05 13:44:01 +0800

    change: Bumped Aether II version.

--- src/main/java/com/gildedgames/aether/common/AetherCore.java
+++ src/main/java/com/gildedgames/aether/common/AetherCore.java
@@ -23,9 +23,9 @@ public class AetherCore
 
 	public static final String MOD_ID = "aether";
 
-	public static final String MOD_VERSION = "1.12.2-1.0.6";
+	public static final String MOD_VERSION = "1.12.2-1.0.7";
 
-	public static final String MOD_DEPENDENCIES = "required-after:orbis_api@1.12.2-1.1.3";
+	public static final String MOD_DEPENDENCIES = "required-after:orbis_api@1.12.2-1.1.4";
 
 	public static final Logger LOGGER = LogManager.getLogger("AetherII");
 
