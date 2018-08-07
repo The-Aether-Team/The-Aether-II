afd07ca59abe6ddf321555e94ec4f38221f3bdf1
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-08-08 04:49:47 +0800

    fix: Edison and Tiv not rotating their render properly

--- src/main/java/com/gildedgames/aether/common/entities/util/EntityBodyHelperNoRotation.java
+++ src/main/java/com/gildedgames/aether/common/entities/util/EntityBodyHelperNoRotation.java
@@ -5,14 +5,18 @@ import net.minecraft.entity.EntityLivingBase;
 
 public class EntityBodyHelperNoRotation extends EntityBodyHelper
 {
+	private final EntityLivingBase living;
+
 	public EntityBodyHelperNoRotation(EntityLivingBase livingIn)
 	{
 		super(livingIn);
+
+		this.living = livingIn;
 	}
 
 	@Override
 	public void updateRenderAngles()
 	{
-		
+		this.living.renderYawOffset = this.living.rotationYaw;
 	}
 }
