f01080f1ed13bc65c7dbbea4d1196bdd1bdccf0b
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-10-22 02:21:08 +0800

    change: Nerfed Plumproot Mash

--- src/main/java/com/gildedgames/aether/common/items/ItemsAether.java
+++ src/main/java/com/gildedgames/aether/common/items/ItemsAether.java
@@ -209,7 +209,7 @@ public class ItemsAether
 
 	public static final Item swet_sugar = new ItemDropOnDeath();
 
-	public static final Item plumproot_mash = new ItemAetherFood(15, 1.0F, false).setConsumptionDuration(22);
+	public static final Item plumproot_mash = new ItemAetherFood(3, 1.0F, false).setConsumptionDuration(12);
 
 	public static final Item plumproot_pie = new ItemAetherFood(20, 1.0F, false).setConsumptionDuration(88);
 
