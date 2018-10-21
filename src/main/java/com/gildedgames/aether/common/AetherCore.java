4305f3a3d87c79aed6d2ef1a00f2aeac5675d540
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-10-21 23:28:10 +0800

    update: Version 1.1.11

--- src/main/java/com/gildedgames/aether/common/AetherCore.java
+++ src/main/java/com/gildedgames/aether/common/AetherCore.java
@@ -22,7 +22,7 @@ public class AetherCore
 
 	public static final String MOD_ID = "aether";
 
-	public static final String MOD_VERSION = "1.12.2-1.1.1";
+	public static final String MOD_VERSION = "1.12.2-1.1.11";
 
 	public static final String MOD_DEPENDENCIES = "required-after:orbis_api@1.12.2-1.1.11";
 
