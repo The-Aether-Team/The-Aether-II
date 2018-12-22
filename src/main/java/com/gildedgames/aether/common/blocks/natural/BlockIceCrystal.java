bcd27b135897edeebe10298909360b729802ba3f
chrisloyde <KnightBroseph@gmail.com>
2018-12-21 17:32:03 -0700

    fix: Ice Crystal drops
    - Ice crystal's no longer drop Ice crystals when destroyed.

--- src/main/java/com/gildedgames/aether/common/blocks/natural/BlockIceCrystal.java
+++ src/main/java/com/gildedgames/aether/common/blocks/natural/BlockIceCrystal.java
@@ -15,6 +15,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -78,6 +79,16 @@ public class BlockIceCrystal extends BlockBuilder implements IBlockMultiName {
         {
             worldIn.setBlockToAir(pos);
         }
+    }
+
+    public int quantityDropped(Random random)
+    {
+        return 0;
+    }
+
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return null;
     }
 
     @Override
