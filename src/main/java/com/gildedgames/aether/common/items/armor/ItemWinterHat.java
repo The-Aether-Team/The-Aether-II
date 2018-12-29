d87c4c83729310658c793b66824ed3bc5dea7c68
Onyx <sor1n.iliutza16@gmail.com>
2018-12-29 18:30:21 +0000

    Added winter hat icon + adjusted winter hat repair items and properties

--- src/main/java/com/gildedgames/aether/common/items/armor/ItemWinterHat.java
+++ src/main/java/com/gildedgames/aether/common/items/armor/ItemWinterHat.java
@@ -4,19 +4,25 @@ import javax.annotation.Nullable;
 
 import com.gildedgames.aether.client.models.items.ModelWinterHat;
 import com.gildedgames.aether.common.AetherCore;
+import com.gildedgames.aether.common.blocks.BlocksAether;
+import com.gildedgames.aether.common.items.ItemsAether;
 
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemWinterHat extends ItemArmor
 {
     public ItemWinterHat()
     {
-        super(ArmorMaterial.LEATHER, 0, EntityEquipmentSlot.HEAD);
+        super(ItemsAether.WINTER_ARMOR, 0, EntityEquipmentSlot.HEAD);
     }
 
     @Nullable
@@ -31,5 +37,12 @@ public class ItemWinterHat extends ItemArmor
     public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default)
     {
         return new ModelWinterHat();
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair)
+    {
+        if(repair.isItemEqual(new ItemStack(BlocksAether.cloudwool_block))) return true;
+        return super.getIsRepairable(toRepair, repair);
     }
 }
