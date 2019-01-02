399e6f487efa70e2fc197ea2efad15a347034f1f
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2019-01-03 03:54:42 +0800

    change: Aether II version

--- src/main/java/com/gildedgames/aether/common/AetherCore.java
+++ src/main/java/com/gildedgames/aether/common/AetherCore.java
@@ -22,7 +22,7 @@ public class AetherCore
 
 	public static final String MOD_ID = "aether";
 
-	public static final String MOD_VERSION = "0.1.0";
+	public static final String MOD_VERSION = "0.1.1";
 
 	public static final String MOD_DEPENDENCIES = "required-after:orbis_api@[0.1.0,)";
 
