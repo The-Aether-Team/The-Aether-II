b5c6b83951e2968d457fe1304d153617bbf4dd93
Angeline <angeline@gildedgames.com>
2019-02-18 02:27:57 -0600

    refactor: Migrate to using Phosphor as a submodule
    Improve build scripts

--- src/api/java/com/gildedgames/aether/api/lighting/IChunkLighting.java
+++
@@ -1,9 +0,0 @@
-package com.gildedgames.aether.api.lighting;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.EnumSkyBlock;
-
-public interface IChunkLighting
-{
-	int getCachedLightFor(EnumSkyBlock enumSkyBlock, BlockPos pos);
-}
--- src/api/java/com/gildedgames/aether/api/lighting/ILightingEngineProvider.java
+++
@@ -1,6 +0,0 @@
-package com.gildedgames.aether.api.lighting;
-
-public interface ILightingEngineProvider
-{
-	ILightingEngine getLightingEngine();
-}
