ad93f07118acecd95160bee9fcd06cb2ad307bfd
bconlon <aidan90803@icloud.com>
2019-12-08 09:52:22 -0800

    Fixed Moa Eggs, Presents, and Masonry Benches acting as full blocks.

--- src/main/java/com/gildedgames/aether/common/blocks/containers/BlockMasonryBench.java
+++ src/main/java/com/gildedgames/aether/common/blocks/containers/BlockMasonryBench.java
@@ -8,6 +8,7 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -20,6 +21,7 @@ import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockMasonryBench extends BlockContainer
@@ -36,6 +38,12 @@ public class BlockMasonryBench extends BlockContainer
 		this.setSoundType(SoundType.WOOD);
 
 		this.setDefaultState(this.getBlockState().getBaseState().withProperty(PROPERTY_FACING, EnumFacing.NORTH));
+	}
+
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+	{
+		return BlockFaceShape.UNDEFINED;
 	}
 
 	@Override
