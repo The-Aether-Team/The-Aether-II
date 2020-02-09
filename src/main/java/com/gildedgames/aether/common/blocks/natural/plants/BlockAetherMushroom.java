0304aff58fed92edb6871ce170b9a6be7744ea3c
bconlon <aidan90803@icloud.com>
2020-02-08 17:58:20 -0800

    Aether mushrooms now spread.

--- src/main/java/com/gildedgames/aether/common/blocks/natural/plants/BlockAetherMushroom.java
+++ src/main/java/com/gildedgames/aether/common/blocks/natural/plants/BlockAetherMushroom.java
@@ -1,7 +1,9 @@
 package com.gildedgames.aether.common.blocks.natural.plants;
 
 import com.gildedgames.aether.api.registrar.BlocksAether;
+import net.minecraft.block.BlockDirt;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -9,8 +11,69 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.common.IPlantable;
 
+import java.util.Random;
+
 public class BlockAetherMushroom extends BlockAetherFlowerBase implements IPlantable
 {
+	public BlockAetherMushroom()
+	{
+		this.setTickRandomly(true);
+	}
+
+	@Override
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+	{
+		if (rand.nextInt(25) == 0)
+		{
+			int i = 5;
+			int j = 4;
+
+			for (BlockPos blockpos : BlockPos.getAllInBoxMutable(pos.add(-4, -1, -4), pos.add(4, 1, 4)))
+			{
+				if (worldIn.getBlockState(blockpos).getBlock() == this)
+				{
+					--i;
+
+					if (i <= 0)
+					{
+						return;
+					}
+				}
+			}
+
+			BlockPos blockpos1 = pos.add(rand.nextInt(3) - 1, rand.nextInt(2) - rand.nextInt(2), rand.nextInt(3) - 1);
+
+			for (int k = 0; k < 4; ++k)
+			{
+				if (worldIn.isAirBlock(blockpos1) && this.canBlockStay(worldIn, blockpos1, this.getDefaultState()))
+				{
+					pos = blockpos1;
+				}
+
+				blockpos1 = pos.add(rand.nextInt(3) - 1, rand.nextInt(2) - rand.nextInt(2), rand.nextInt(3) - 1);
+			}
+
+			if (worldIn.isAirBlock(blockpos1) && this.canBlockStay(worldIn, blockpos1, this.getDefaultState()))
+			{
+				worldIn.setBlockState(blockpos1, this.getDefaultState(), 2);
+			}
+		}
+	}
+
+	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state)
+	{
+		if (pos.getY() >= 0 && pos.getY() < 256)
+		{
+			IBlockState iblockstate = worldIn.getBlockState(pos.down());
+
+			return worldIn.getLight(pos) < 13 && iblockstate.getBlock().canSustainPlant(iblockstate, worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
 	@Override
 	public boolean isSuitableSoilBlock(World world, BlockPos pos, final IBlockState state)
 	{
