70d6fff1df02793d0b1017747b8d02d5af48dd0c
bconlon <aidan90803@icloud.com>
2020-02-16 21:14:49 -0800

    Properly cancelled rain sounds in the Aether.

--- src/main/java/com/gildedgames/aether/client/events/listeners/PlaySoundListener.java
+++ src/main/java/com/gildedgames/aether/client/events/listeners/PlaySoundListener.java
@@ -5,21 +5,22 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.init.SoundEvents;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class PlaySoundListener
 {
-	@SubscribeEvent
-	public void onSoundEvent(PlaySoundEvent event)
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public static void onSoundEvent(final PlaySoundEvent event)
 	{
 		if (AetherHelper.isAether(Minecraft.getMinecraft().world))
 		{
 			if (event.getName().equals(SoundEvents.WEATHER_RAIN.getSoundName().getPath()) ||
 					event.getName().equals(SoundEvents.WEATHER_RAIN_ABOVE.getSoundName().getPath()))
 			{
-				event.setCanceled(true);
+				event.setResultSound(null);
 			}
 		}
 	}
