b1d7905c1f88142bbd0a474132fb104f67a32749
bconlon <bconlon@gildedgames.com>
2020-02-29 14:33:23 -0800

    Skyroot Poison Buckets now inflict toxin.

--- src/main/java/com/gildedgames/aether/common/items/consumables/ItemSkyrootConsumableBucket.java
+++ src/main/java/com/gildedgames/aether/common/items/consumables/ItemSkyrootConsumableBucket.java
@@ -1,10 +1,15 @@
 package com.gildedgames.aether.common.items.consumables;
 
+import com.gildedgames.aether.api.entity.effects.IAetherStatusEffectPool;
+import com.gildedgames.aether.api.entity.effects.IAetherStatusEffects;
+import com.gildedgames.aether.api.registrar.CapabilitiesAether;
 import com.gildedgames.aether.api.registrar.ItemsAether;
 import com.gildedgames.aether.common.init.CreativeTabsAether;
 import com.gildedgames.aether.common.items.IDropOnDeath;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.EnumAction;
@@ -16,6 +21,8 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class ItemSkyrootConsumableBucket extends Item implements IDropOnDeath
 {
@@ -36,23 +43,54 @@ public class ItemSkyrootConsumableBucket extends Item implements IDropOnDeath
 	}
 
 	@Override
-	public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase living)
+	public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entityLiving)
 	{
-		if (living instanceof EntityPlayer)
+		EntityPlayer entityPlayer = entityLiving instanceof EntityPlayer ? (EntityPlayer)entityLiving : null;
+
+		if (entityPlayer instanceof EntityPlayerMP)
 		{
-			EntityPlayer player = (EntityPlayer) living;
-
-			if (!((EntityPlayer) living).capabilities.isCreativeMode)
-			{
-				stack.shrink(1);
-			}
-
-			player.addStat(StatList.getObjectUseStats(this));
+			CriteriaTriggers.CONSUME_ITEM.trigger((EntityPlayerMP)entityPlayer, stack);
 		}
 
-		if (!world.isRemote)
+		if (entityPlayer != null)
 		{
-			this.applyEffect(stack, world, living);
+			entityPlayer.addStat(Objects.requireNonNull(StatList.getObjectUseStats(this)));
+
+			if (!world.isRemote)
+			{
+				if (stack.getItem() == ItemsAether.skyroot_milk_bucket)
+				{
+					entityPlayer.curePotionEffects(new ItemStack(Items.MILK_BUCKET));
+				}
+			}
+		}
+
+		if (entityPlayer == null || !entityPlayer.capabilities.isCreativeMode)
+		{
+			IAetherStatusEffectPool statusEffectPool = (entityPlayer != null)
+					? entityPlayer.getCapability(CapabilitiesAether.STATUS_EFFECT_POOL, null)
+					: entityLiving.getCapability(CapabilitiesAether.STATUS_EFFECT_POOL, null);
+
+			if (!world.isRemote)
+			{
+				if (statusEffectPool != null)
+				{
+					if (stack.getItem() == ItemsAether.skyroot_poison_bucket)
+					{
+						if (!statusEffectPool.isEffectApplied(IAetherStatusEffects.effectTypes.TOXIN))
+						{
+							statusEffectPool.applyStatusEffect(IAetherStatusEffects.effectTypes.TOXIN, 50);
+						}
+						else
+						{
+							statusEffectPool.modifyActiveEffectBuildup(IAetherStatusEffects.effectTypes.TOXIN,
+									statusEffectPool.getBuildupFromEffect(IAetherStatusEffects.effectTypes.TOXIN) + 50);
+						}
+					}
+				}
+			}
+
+			stack.shrink(1);
 		}
 
 		return stack.getCount() <= 0 ? new ItemStack(ItemsAether.skyroot_bucket) : stack;
