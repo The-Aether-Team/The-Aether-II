a251bd924aab7189cf7954954a5f5cec178a4f9f
bconlon <bconlon@gildedgames.com>
2020-03-17 10:37:29 -0700

    Fixed issue with Moas freezing upon falling out of the world.

--- src/main/java/com/gildedgames/aether/common/events/listeners/world/WorldFallListener.java
+++ src/main/java/com/gildedgames/aether/common/events/listeners/world/WorldFallListener.java
@@ -2,11 +2,13 @@ package com.gildedgames.aether.common.events.listeners.world;
 
 import com.gildedgames.aether.common.init.DimensionsAether;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod.EventBusSubscriber
 public class WorldFallListener
@@ -16,26 +18,54 @@ public class WorldFallListener
 	{
 		final Entity entity = event.getEntity();
 
-		// Checks whether or not an entity is in the Aether's void
-		if (entity.world.provider.getDimensionType() == DimensionsAether.AETHER && entity.posY < -10)
+		if (!entity.world.isRemote)
 		{
-			if (!entity.world.isRemote)
+			if (entity.world.provider.getDimensionType() == DimensionsAether.AETHER && entity.posY < -10)
 			{
-				if (entity instanceof EntityPlayer)
+				if (entity.isBeingRidden())
 				{
-					final EntityPlayer player = (EntityPlayer) entity;
-
-					player.attackEntityFrom(DamageSource.OUT_OF_WORLD, 200.0F);
-
-					if (player.getHealth() <= 0 && !player.isDead)
-					{
-						player.isDead = true;
-					}
+					entity.removePassengers();
 				}
-				else
+
+				if (entity.isRiding())
 				{
-					entity.setDead();
+					entity.dismountRidingEntity();
 				}
+
+				entity.attackEntityFrom(DamageSource.OUT_OF_WORLD, 200.0F);
+
+				entity.setDead();
+			}
+		}
+	}
+	@SubscribeEvent
+	public static void onPlayerEntityTick(final TickEvent.PlayerTickEvent event)
+	{
+		final EntityPlayer player = event.player;
+
+		if (!player.world.isRemote)
+		{
+			if (player.world.provider.getDimensionType() == DimensionsAether.AETHER && player.posY < -10)
+			{
+				if (player.isBeingRidden())
+				{
+					player.removePassengers();
+				}
+
+				if (player.isRiding())
+				{
+					player.dismountRidingEntity();
+				}
+
+				player.attackEntityFrom(DamageSource.OUT_OF_WORLD, 200.0F);
+
+				if (player.getHealth() <= 0 && !player.isDead)
+				{
+					player.isDead = true;
+				}
+
+				player.world.playerEntities.remove(player);
+				player.world.onEntityRemoved(player);
 			}
 		}
 	}
