d5fb1a4cccab390cdee52a9ac65cffce039fcf0e
bconlon <bconlon@gildedgames.com>
2020-04-06 12:59:19 -0700

    Skyroot Trapdoors now act like ladders if there is a ladder beneath it.

--- src/main/java/com/gildedgames/aether/common/blocks/construction/BlockSkyrootTrapDoor.java
+++ src/main/java/com/gildedgames/aether/common/blocks/construction/BlockSkyrootTrapDoor.java
@@ -1,8 +1,13 @@
 package com.gildedgames.aether.common.blocks.construction;
 
+import net.minecraft.block.BlockLadder;
 import net.minecraft.block.BlockTrapDoor;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockSkyrootTrapDoor extends BlockTrapDoor
 {
@@ -15,5 +20,18 @@ public class BlockSkyrootTrapDoor extends BlockTrapDoor
 		this.setHardness(3.0f);
 
 		this.disableStats();
+	}
+
+	@Override
+	public boolean isLadder(IBlockState state, IBlockAccess world, BlockPos pos, EntityLivingBase entity)
+	{
+		if (state.getValue(OPEN))
+		{
+			IBlockState down = world.getBlockState(pos.down());
+			if (down.getBlock() instanceof BlockLadder)
+				return down.getValue(BlockLadder.FACING) == state.getValue(FACING);
+		}
+
+		return false;
 	}
 }
