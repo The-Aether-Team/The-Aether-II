cd00e1ed9dce59141441dc9918b687523b91f18c
bconlon <bconlon@gildedgames.com>
2020-05-18 20:05:34 -0700

    Another possible fix.

--- src/main/java/com/gildedgames/aether/common/events/listeners/player/PlayerEatListener.java
+++ src/main/java/com/gildedgames/aether/common/events/listeners/player/PlayerEatListener.java
@@ -12,6 +12,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.lang.reflect.Field;
 import java.util.Objects;
@@ -32,7 +34,7 @@ public class PlayerEatListener
                 {
                     if (event.getItem().getItem() instanceof ItemFood)
                     {
-                        final Field saturationLevel = ObfuscationReflectionHelper.findField(FoodStats.class, "foodSaturationLevel");
+                        final Field saturationLevel = ReflectionHelper.findField(FoodStats.class, "foodSaturationLevel", "field_149335_c");
 
                         ItemStack stack = event.getItem();
                         EntityPlayer player = (EntityPlayer) event.getEntityLiving();
