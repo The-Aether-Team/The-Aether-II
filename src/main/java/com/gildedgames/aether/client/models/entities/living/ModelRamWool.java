7850c61fd6a3729858ac3f27926c11b60907efe6
Hugo <hugo@gildedgames.com>
2020-12-03 21:40:58 +0000

    Slight tweak to Kirrid wool model.

--- src/main/java/com/gildedgames/aether/client/models/entities/living/ModelRamWool.java
+++ src/main/java/com/gildedgames/aether/client/models/entities/living/ModelRamWool.java
@@ -10,8 +10,6 @@ public class ModelRamWool extends ModelBase
 {
 	public ModelRenderer BodyMain;
 	public ModelRenderer BodyBack;
-	public ModelRenderer LegFrontLeft1;
-	public ModelRenderer LegFrontRight1;
 
 	public ModelRamWool()
 	{
@@ -21,21 +19,11 @@ public class ModelRamWool extends ModelBase
 		this.BodyBack.setRotationPoint(0.0F, 0.0F, 0.0F);
 		this.BodyBack.addBox(-6.0F, -1.5F, 4.6F, 12, 5, 10, 0.0F);
 		this.setRotateAngle(BodyBack, -0.17453292519943295F, -0.0F, 0.0F);
-		this.LegFrontLeft1 = new ModelRenderer(this, 28, 46);
-		this.LegFrontLeft1.mirror = true;
-		this.LegFrontLeft1.setRotationPoint(4.0F, 13.0F, -5.0F);
-		this.LegFrontLeft1.addBox(-3.0F, -5.0F, -2.5F, 6, 8, 6, 0.0F);
-		this.setRotateAngle(LegFrontLeft1, 0.08726646259971647F, -0.0F, 0.0F);
-		this.LegFrontRight1 = new ModelRenderer(this, 4, 46);
-		this.LegFrontRight1.setRotationPoint(-4.0F, 13.0F, -5.0F);
-		this.LegFrontRight1.addBox(-3.0F, -5.0F, -2.5F, 6, 8, 6, 0.0F);
-		this.setRotateAngle(LegFrontRight1, 0.08726646259971647F, -0.0F, 0.0F);
 		this.BodyMain = new ModelRenderer(this, 0, 0);
 		this.BodyMain.setRotationPoint(0.0F, 0.0F, 0.0F);
 		this.BodyMain.addBox(-6.5F, -1.0F, -10.0F, 13, 16, 15, 0.0F);
 		this.BodyMain.addChild(this.BodyBack);
-		this.BodyMain.addChild(this.LegFrontLeft1);
-		this.BodyMain.addChild(this.LegFrontRight1);
+
 	}
 
 	@Override
@@ -65,13 +53,5 @@ public class ModelRamWool extends ModelBase
 		modelRenderer.rotateAngleZ = z;
 	}
 
-	@Override
-	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch,
-								  float scaleFactor, Entity entity)
-	{
-		super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
 
-		this.LegFrontLeft1.rotateAngleX = 0.08726646259971647F + (MathHelper.cos(limbSwing * 0.6662F) * 1.0F * limbSwingAmount);
-		this.LegFrontRight1.rotateAngleX = 0.08726646259971647F + (MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.0F * limbSwingAmount);
-	}
 }
