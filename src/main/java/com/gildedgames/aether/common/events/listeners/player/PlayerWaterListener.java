0cebf1e5d3cccc037e34a1ff373057ef7dd5ef6c
bconlon <bconlon@gildedgames.com>
2021-01-15 17:48:12 -0800

    Added radiation system to water.

---
+++ src/main/java/com/gildedgames/aether/common/events/listeners/player/PlayerWaterListener.java
@@ -0,0 +1,61 @@
+package com.gildedgames.aether.common.events.listeners.player;
+
+import com.gildedgames.aether.api.entity.effects.IAetherStatusEffectPool;
+import com.gildedgames.aether.api.entity.effects.IAetherStatusEffects;
+import com.gildedgames.aether.api.registrar.BiomesAether;
+import com.gildedgames.aether.api.registrar.CapabilitiesAether;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+@Mod.EventBusSubscriber
+public class PlayerWaterListener
+{
+    public static int tickRate = 30;
+    public static int amount = 5;
+
+    @SubscribeEvent
+    public static void onPlayerStunned(final TickEvent.PlayerTickEvent event)
+    {
+        if (event.player != null)
+        {
+            EntityPlayer player = event.player;
+
+            if (player.world != null)
+            {
+                World world = player.world;
+
+                if (world.getBiome(player.getPosition()) == BiomesAether.IRRADIATED_FORESTS)
+                {
+                    IAetherStatusEffectPool statusEffectPool = player.getCapability(CapabilitiesAether.STATUS_EFFECT_POOL, null);
+
+                    if (player.isInWater())
+                    {
+                        if (!player.isCreative())
+                        {
+                            boolean tick = world.getTotalWorldTime() % tickRate == 0;
+
+                            if (tick)
+                            {
+                                if (statusEffectPool != null)
+                                {
+                                    if (statusEffectPool.getBuildupFromEffect(IAetherStatusEffects.effectTypes.AMBROSIUM_POISONING) <= 0)
+                                    {
+                                        statusEffectPool.applyStatusEffect(IAetherStatusEffects.effectTypes.AMBROSIUM_POISONING, amount);
+                                    }
+                                    else
+                                    {
+                                        statusEffectPool.modifyActiveEffectBuildup(IAetherStatusEffects.effectTypes.AMBROSIUM_POISONING,
+                                                statusEffectPool.getBuildupFromEffect(IAetherStatusEffects.effectTypes.AMBROSIUM_POISONING) + amount);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
