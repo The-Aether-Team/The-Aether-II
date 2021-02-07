6d0b41859b8ff5787986561eaa5cbd8c674367d2
bconlon <bconlon@gildedgames.com>
2021-02-07 13:22:53 -0800

    I don't know why this class didn't get pushed with the other commit.

--- src/main/java/com/gildedgames/aether/client/renderer/entities/living/RenderKirrid.java
+++ src/main/java/com/gildedgames/aether/client/renderer/entities/living/RenderKirrid.java
@@ -1,7 +1,9 @@
 package com.gildedgames.aether.client.renderer.entities.living;
 
 import com.gildedgames.aether.client.models.entities.living.ModelKirrid;
+import com.gildedgames.aether.client.models.entities.living.ModelKirridBaby;
 import com.gildedgames.aether.client.renderer.EyeUtil;
+import com.gildedgames.aether.client.renderer.ModelBaseAether;
 import com.gildedgames.aether.client.renderer.entities.living.layers.LayerKirridWool;
 import com.gildedgames.aether.common.AetherCore;
 import com.gildedgames.aether.common.entities.animals.EntityKirrid;
@@ -10,27 +12,19 @@ import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderKirrid extends RenderLiving<EntityKirrid>
+public class RenderKirrid extends RenderLivingChild<EntityKirrid>
 {
 	private static final ResourceLocation texture = AetherCore.getResource("textures/entities/kirrid/kirrid.png");
-
-	private static final ResourceLocation PUPIL_LEFT = AetherCore.getResource("textures/entities/kirrid/pupil_left.png");
-
-	private static final ResourceLocation PUPIL_RIGHT = AetherCore.getResource("textures/entities/kirrid/pupil_right.png");
-
 	private static final ResourceLocation EYES_CLOSED = AetherCore.getResource("textures/entities/kirrid/eyes_closed.png");
+
+	private static final ResourceLocation texture_baby = AetherCore.getResource("textures/entities/kirrid/babykirrid.png");
+	private static final ResourceLocation EYES_CLOSED_baby = AetherCore.getResource("textures/entities/kirrid/babykirrid_eyes_closed.png");
 
 	public RenderKirrid(RenderManager renderManager)
 	{
-		super(renderManager, new ModelKirrid(), 0.75f);
+		super(renderManager, new ModelKirrid(), new ModelKirridBaby(), texture, texture_baby, 0.75f);
 
 		this.addLayer(new LayerKirridWool(this));
-	}
-
-	@Override
-	protected ResourceLocation getEntityTexture(EntityKirrid entity)
-	{
-		return texture;
 	}
 
 	@Override
@@ -38,15 +32,17 @@ public class RenderKirrid extends RenderLiving<EntityKirrid>
 	{
 		super.renderModel(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
 
-		ModelKirrid model = (ModelKirrid) this.mainModel;
+		ResourceLocation eyes = !entity.isChild() ? EYES_CLOSED : EYES_CLOSED_baby;
 
 		boolean globalInvisible = !entity.isInvisible() || this.renderOutlines;
 		boolean playerInvisible = !globalInvisible && !entity.isInvisibleToPlayer(Minecraft.getMinecraft().player);
 
+		ModelBaseAether model = (ModelBaseAether) this.mainModel;
+
 		if (globalInvisible || playerInvisible)
 		{
 			EyeUtil.renderEyesBasic(this.renderManager, model, entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale,
-					EYES_CLOSED);
+					eyes);
 		}
 	}
 }
