960c88f643a015e39de9b8a029282526e4f8745b
bconlon <bconlon@gildedgames.com>
2021-02-16 15:15:44 -0800

    New icestone cooler and masonry bench model.

--- src/main/java/com/gildedgames/aether/client/models/entities/tile/ModelIcestoneCooler.java
+++ src/main/java/com/gildedgames/aether/client/models/entities/tile/ModelIcestoneCooler.java
@@ -4,281 +4,71 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelIcestoneCooler extends ModelBase
-{
-	final ModelRenderer LidBase1;
+/**
+ * icestone_cooler - Undefined
+ * Created using Tabula 5.1.0
+ */
+public class ModelIcestoneCooler extends ModelBase {
+    public ModelRenderer Main;
+    public ModelRenderer Base1;
+    public ModelRenderer Base2;
+    public ModelRenderer Lid;
+    public ModelRenderer LidTop;
+    public ModelRenderer LidHandle;
 
-	final ModelRenderer LidBase2;
+    public ModelIcestoneCooler() {
+        this.textureWidth = 64;
+        this.textureHeight = 64;
+        this.LidHandle = new ModelRenderer(this, 34, 13);
+        this.LidHandle.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.LidHandle.addBox(-3.5F, -1.75F, -11.0F, 7, 1, 7, 0.0F);
+        this.Base2 = new ModelRenderer(this, 0, 13);
+        this.Base2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Base2.addBox(-2.0F, 18.1F, -9.0F, 4, 6, 18, 0.0F);
+        this.setRotateAngle(Base2, 0.0F, 0.7853981633974483F, 0.0F);
+        this.LidTop = new ModelRenderer(this, 36, 6);
+        this.LidTop.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.LidTop.addBox(0.2F, -2.0F, -6.2F, 6, 1, 6, 0.0F);
+        this.setRotateAngle(LidTop, 0.0F, 0.7853981633974483F, 0.0F);
+        this.Base1 = new ModelRenderer(this, 0, 13);
+        this.Base1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Base1.addBox(-2.0F, 18.1F, -9.0F, 4, 6, 18, 0.0F);
+        this.setRotateAngle(Base1, 0.0F, -0.7853981633974483F, 0.0F);
+        this.Main = new ModelRenderer(this, 0, 38);
+        this.Main.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Main.addBox(-6.0F, 10.0F, -6.0F, 12, 14, 12, 0.0F);
+        this.Lid = new ModelRenderer(this, 0, 3);
+        this.Lid.setRotationPoint(0.0F, 10.0F, 4.5F);
+        this.Lid.addBox(-4.5F, -1.0F, -9.0F, 9, 1, 9, 0.0F);
+        this.Lid.addChild(this.LidHandle);
+        this.Lid.addChild(this.LidTop);
+    }
 
-	final ModelRenderer LidBase3;
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+        super.render(entity, f, f1, f2, f3, f4, f5);
+        this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        this.render(f5);
+    }
 
-	final ModelRenderer LidBase4;
+    public void render(float f5) {
+        this.Base2.render(f5);
+        this.Base1.render(f5);
+        this.Main.render(f5);
+        this.Lid.render(f5);
+    }
 
-	final ModelRenderer LidHingeRight;
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
+    }
 
-	final ModelRenderer LidHingeLeft;
-
-	final ModelRenderer LidHandleBarRight;
-
-	final ModelRenderer LidHandleBarLeft;
-
-	final ModelRenderer LidHandle;
-
-	final ModelRenderer LidTopCenter;
-
-	final ModelRenderer LidTop1;
-
-	final ModelRenderer LidTop2;
-
-	final ModelRenderer LidTop3;
-
-	final ModelRenderer LidTop4;
-
-	final ModelRenderer ChamberFront;
-
-	final ModelRenderer ChamberFrontLeft;
-
-	final ModelRenderer ChamberFrontRight;
-
-	final ModelRenderer ChamberBack;
-
-	final ModelRenderer ChamberBackLeft;
-
-	final ModelRenderer ChamberBackRight;
-
-	final ModelRenderer ChamberLeft;
-
-	final ModelRenderer ChamberRight;
-
-	final ModelRenderer ChamberBase;
-
-	final ModelRenderer ChamberAccentFront;
-
-	final ModelRenderer ChamberAccentBack;
-
-	final ModelRenderer ChamberAccentLeft;
-
-	final ModelRenderer ChamberAccentRight;
-
-	public ModelIcestoneCooler()
-	{
-		this.textureWidth = 64;
-		this.textureHeight = 32;
-
-		this.LidBase1 = new ModelRenderer(this, 0, 0);
-		this.LidBase1.addBox(-2F, -1F, -10F, 4, 1, 10);
-		this.LidBase1.setRotationPoint(0F, 10F, 5F);
-		this.LidBase1.setTextureSize(64, 32);
-		this.LidBase1.mirror = true;
-		this.setRotation(this.LidBase1, 0F, 0F, 0F);
-		this.LidBase2 = new ModelRenderer(this, 0, 0);
-		this.LidBase2.addBox(3F, -1F, -5F, 4, 1, 10);
-		this.LidBase2.setRotationPoint(0F, 10F, 5F);
-		this.LidBase2.setTextureSize(64, 32);
-		this.LidBase2.mirror = true;
-		this.setRotation(this.LidBase2, 0F, 1.570796F, 0F);
-		this.LidBase3 = new ModelRenderer(this, 0, 0);
-		this.LidBase3.addBox(1.5F, -0.9F, -8.5F, 4, 1, 10);
-		this.LidBase3.setRotationPoint(0F, 10F, 5F);
-		this.LidBase3.setTextureSize(64, 32);
-		this.LidBase3.mirror = true;
-		this.setRotation(this.LidBase3, 0F, 0.7853982F, 0F);
-		this.LidBase4 = new ModelRenderer(this, 0, 0);
-		this.LidBase4.addBox(-5.5F, -0.9F, -8.5F, 4, 1, 10);
-		this.LidBase4.setRotationPoint(0F, 10F, 5F);
-		this.LidBase4.setTextureSize(64, 32);
-		this.LidBase4.mirror = true;
-		this.setRotation(this.LidBase4, 0F, -0.7853982F, 0F);
-		this.LidHingeRight = new ModelRenderer(this, 58, 0);
-		this.LidHingeRight.addBox(-2F, -1F, -1F, 1, 1, 2);
-		this.LidHingeRight.setRotationPoint(0F, 10F, 5F);
-		this.LidHingeRight.setTextureSize(64, 32);
-		this.LidHingeRight.mirror = true;
-		this.setRotation(this.LidHingeRight, -0.6981317F, 0F, 0F);
-		this.LidHingeLeft = new ModelRenderer(this, 58, 0);
-		this.LidHingeLeft.addBox(1F, -1F, -1F, 1, 1, 2);
-		this.LidHingeLeft.setRotationPoint(0F, 10F, 5F);
-		this.LidHingeLeft.setTextureSize(64, 32);
-		this.LidHingeLeft.mirror = true;
-		this.setRotation(this.LidHingeLeft, -0.6981317F, 0F, 0F);
-		this.LidHandleBarRight = new ModelRenderer(this, 50, 3);
-		this.LidHandleBarRight.addBox(-3F, -1.5F, -12F, 1, 1, 6);
-		this.LidHandleBarRight.setRotationPoint(0F, 10F, 5F);
-		this.LidHandleBarRight.setTextureSize(64, 32);
-		this.LidHandleBarRight.mirror = true;
-		this.setRotation(this.LidHandleBarRight, 0F, 0F, 0F);
-		this.LidHandleBarLeft = new ModelRenderer(this, 50, 3);
-		this.LidHandleBarLeft.addBox(2F, -1.5F, -12F, 1, 1, 6);
-		this.LidHandleBarLeft.setRotationPoint(0F, 10F, 5F);
-		this.LidHandleBarLeft.setTextureSize(64, 32);
-		this.LidHandleBarLeft.mirror = true;
-		this.setRotation(this.LidHandleBarLeft, 0F, 0F, 0F);
-		this.LidHandle = new ModelRenderer(this, 54, 10);
-		this.LidHandle.addBox(-2F, -9.3F, -8F, 4, 1, 1);
-		this.LidHandle.setRotationPoint(0F, 10F, 5F);
-		this.LidHandle.setTextureSize(64, 32);
-		this.LidHandle.mirror = true;
-		this.setRotation(this.LidHandle, 0.7853982F, 0F, 0F);
-		this.LidTopCenter = new ModelRenderer(this, 26, 0);
-		this.LidTopCenter.addBox(0.5F, -2F, -6.5F, 6, 1, 6);
-		this.LidTopCenter.setRotationPoint(0F, 10F, 5F);
-		this.LidTopCenter.setTextureSize(64, 32);
-		this.LidTopCenter.mirror = true;
-		this.setRotation(this.LidTopCenter, 0F, 0.7853982F, 0F);
-		this.LidTop1 = new ModelRenderer(this, 31, 7);
-		this.LidTop1.addBox(1F, -5.6F, -5.7F, 5, 1, 2);
-		this.LidTop1.setRotationPoint(0F, 10F, 5F);
-		this.LidTop1.setTextureSize(64, 32);
-		this.LidTop1.mirror = true;
-		this.setRotation(this.LidTop1, 0.6981317F, 0.7853982F, 0F);
-		this.LidTop2 = new ModelRenderer(this, 31, 7);
-		this.LidTop2.addBox(-6F, -5.6F, -5.7F, 5, 1, 2);
-		this.LidTop2.setRotationPoint(0F, 10F, 5F);
-		this.LidTop2.setTextureSize(64, 32);
-		this.LidTop2.mirror = true;
-		this.setRotation(this.LidTop2, 0.6981317F, -0.7853982F, 0F);
-		this.LidTop3 = new ModelRenderer(this, 31, 7);
-		this.LidTop3.addBox(1F, -1.1F, -0.4F, 5, 1, 2);
-		this.LidTop3.setRotationPoint(0F, 10F, 5F);
-		this.LidTop3.setTextureSize(64, 32);
-		this.LidTop3.mirror = true;
-		this.setRotation(this.LidTop3, 0.6981317F, 2.356194F, 0F);
-		this.LidTop4 = new ModelRenderer(this, 31, 7);
-		this.LidTop4.addBox(-6F, -1.1F, -0.4F, 5, 1, 2);
-		this.LidTop4.setRotationPoint(0F, 10F, 5F);
-		this.LidTop4.setTextureSize(64, 32);
-		this.LidTop4.mirror = true;
-		this.setRotation(this.LidTop4, 0.6981317F, -2.356194F, 0F);
-		this.ChamberFront = new ModelRenderer(this, 0, 15);
-		this.ChamberFront.addBox(-3F, 10F, -7.1F, 6, 14, 3);
-		this.ChamberFront.setRotationPoint(0F, 0F, 0F);
-		this.ChamberFront.setTextureSize(64, 32);
-		this.ChamberFront.mirror = true;
-		this.setRotation(this.ChamberFront, 0F, 0F, 0F);
-		this.ChamberFrontLeft = new ModelRenderer(this, 0, 15);
-		this.ChamberFrontLeft.addBox(-3F, 10.1F, -7F, 6, 14, 3);
-		this.ChamberFrontLeft.setRotationPoint(0F, 0F, 0F);
-		this.ChamberFrontLeft.setTextureSize(64, 32);
-		this.ChamberFrontLeft.mirror = true;
-		this.setRotation(this.ChamberFrontLeft, 0F, -0.7853982F, 0F);
-		this.ChamberFrontRight = new ModelRenderer(this, 0, 15);
-		this.ChamberFrontRight.addBox(-3F, 10.1F, -7F, 6, 14, 3);
-		this.ChamberFrontRight.setRotationPoint(0F, 0F, 0F);
-		this.ChamberFrontRight.setTextureSize(64, 32);
-		this.ChamberFrontRight.mirror = true;
-		this.setRotation(this.ChamberFrontRight, 0F, 0.7853982F, 0F);
-		this.ChamberBack = new ModelRenderer(this, 0, 15);
-		this.ChamberBack.addBox(-3F, 10F, -7.1F, 6, 14, 3);
-		this.ChamberBack.setRotationPoint(0F, 0F, 0F);
-		this.ChamberBack.setTextureSize(64, 32);
-		this.ChamberBack.mirror = true;
-		this.setRotation(this.ChamberBack, 0F, 3.141593F, 0F);
-		this.ChamberBackLeft = new ModelRenderer(this, 0, 15);
-		this.ChamberBackLeft.addBox(-3F, 10.1F, -7F, 6, 14, 3);
-		this.ChamberBackLeft.setRotationPoint(0F, 0F, 0F);
-		this.ChamberBackLeft.setTextureSize(64, 32);
-		this.ChamberBackLeft.mirror = true;
-		this.setRotation(this.ChamberBackLeft, 0F, -2.356194F, 0F);
-		this.ChamberBackRight = new ModelRenderer(this, 0, 15);
-		this.ChamberBackRight.addBox(-3F, 10.1F, -7F, 6, 14, 3);
-		this.ChamberBackRight.setRotationPoint(0F, 0F, 0F);
-		this.ChamberBackRight.setTextureSize(64, 32);
-		this.ChamberBackRight.mirror = true;
-		this.setRotation(this.ChamberBackRight, 0F, 2.356194F, 0F);
-		this.ChamberLeft = new ModelRenderer(this, 0, 15);
-		this.ChamberLeft.addBox(-3F, 10F, -7.1F, 6, 14, 3);
-		this.ChamberLeft.setRotationPoint(0F, 0F, 0F);
-		this.ChamberLeft.setTextureSize(64, 32);
-		this.ChamberLeft.mirror = true;
-		this.setRotation(this.ChamberLeft, 0F, -1.570796F, 0F);
-		this.ChamberRight = new ModelRenderer(this, 0, 15);
-		this.ChamberRight.addBox(-3F, 10F, -7.1F, 6, 14, 3);
-		this.ChamberRight.setRotationPoint(0F, 0F, 0F);
-		this.ChamberRight.setTextureSize(64, 32);
-		this.ChamberRight.mirror = true;
-		this.setRotation(this.ChamberRight, 0F, 1.570796F, 0F);
-		this.ChamberBase = new ModelRenderer(this, 18, 16);
-		this.ChamberBase.addBox(-4F, 15.9F, -4F, 8, 8, 8);
-		this.ChamberBase.setRotationPoint(0F, 0F, 0F);
-		this.ChamberBase.setTextureSize(64, 32);
-		this.ChamberBase.mirror = true;
-		this.setRotation(this.ChamberBase, 0F, 0F, 0F);
-		this.ChamberAccentFront = new ModelRenderer(this, 50, 25);
-		this.ChamberAccentFront.addBox(-2F, 18.7F, -5F, 4, 6, 1);
-		this.ChamberAccentFront.setRotationPoint(0F, 0F, 0F);
-		this.ChamberAccentFront.setTextureSize(64, 32);
-		this.ChamberAccentFront.mirror = true;
-		this.setRotation(this.ChamberAccentFront, -0.122173F, 0F, 0F);
-		this.ChamberAccentBack = new ModelRenderer(this, 50, 25);
-		this.ChamberAccentBack.addBox(-2F, 18.7F, -5F, 4, 6, 1);
-		this.ChamberAccentBack.setRotationPoint(0F, 0F, 0F);
-		this.ChamberAccentBack.setTextureSize(64, 32);
-		this.ChamberAccentBack.mirror = true;
-		this.setRotation(this.ChamberAccentBack, -0.122173F, 3.141593F, 0F);
-		this.ChamberAccentLeft = new ModelRenderer(this, 50, 25);
-		this.ChamberAccentLeft.addBox(-2F, 18.7F, -5F, 4, 6, 1);
-		this.ChamberAccentLeft.setRotationPoint(0F, 0F, 0F);
-		this.ChamberAccentLeft.setTextureSize(64, 32);
-		this.ChamberAccentLeft.mirror = true;
-		this.setRotation(this.ChamberAccentLeft, -0.122173F, -1.570796F, 0F);
-		this.ChamberAccentRight = new ModelRenderer(this, 50, 25);
-		this.ChamberAccentRight.addBox(-2F, 18.7F, -5F, 4, 6, 1);
-		this.ChamberAccentRight.setRotationPoint(0F, 0F, 0F);
-		this.ChamberAccentRight.setTextureSize(64, 32);
-		this.ChamberAccentRight.mirror = true;
-		this.setRotation(this.ChamberAccentRight, -0.122173F, 1.570796F, 0F);
-	}
-
-	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-	{
-		super.render(entity, f, f1, f2, f3, f4, f5);
-		this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		this.render(f5);
-	}
-
-	public void render(float f5)
-	{
-		this.LidBase1.render(f5);
-		this.LidBase2.render(f5);
-		this.LidBase3.render(f5);
-		this.LidBase4.render(f5);
-		this.LidHingeRight.render(f5);
-		this.LidHingeLeft.render(f5);
-		this.LidHandleBarRight.render(f5);
-		this.LidHandleBarLeft.render(f5);
-		this.LidHandle.render(f5);
-		this.LidTopCenter.render(f5);
-		this.LidTop1.render(f5);
-		this.LidTop2.render(f5);
-		this.LidTop3.render(f5);
-		this.LidTop4.render(f5);
-		this.ChamberFront.render(f5);
-		this.ChamberFrontLeft.render(f5);
-		this.ChamberFrontRight.render(f5);
-		this.ChamberBack.render(f5);
-		this.ChamberBackLeft.render(f5);
-		this.ChamberBackRight.render(f5);
-		this.ChamberLeft.render(f5);
-		this.ChamberRight.render(f5);
-		this.ChamberBase.render(f5);
-		this.ChamberAccentFront.render(f5);
-		this.ChamberAccentBack.render(f5);
-		this.ChamberAccentLeft.render(f5);
-		this.ChamberAccentRight.render(f5);
-	}
-
-	private void setRotation(ModelRenderer model, float x, float y, float z)
-	{
-		model.rotateAngleX = x;
-		model.rotateAngleY = y;
-		model.rotateAngleZ = z;
-	}
-
-	@Override
-	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-	{
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-	}
-
+    public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
+    {
+        super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+    }
 }
--- src/main/java/com/gildedgames/aether/client/models/entities/tile/ModelMasonryBench.java
+++ src/main/java/com/gildedgames/aether/client/models/entities/tile/ModelMasonryBench.java
@@ -4,183 +4,95 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelMasonryBench extends ModelBase
-{
-	//fields
-	final ModelRenderer MainBottom;
+/**
+ * masonry_bench.tcn - TechneToTabulaImporter
+ * Created using Tabula 5.1.0
+ */
+public class ModelMasonryBench extends ModelBase {
+    public ModelRenderer MainLeft;
+    public ModelRenderer TopRight;
+    public ModelRenderer TopBack;
+    public ModelRenderer TopLeft;
+    public ModelRenderer Workslab;
+    public ModelRenderer SawRotor;
+    public ModelRenderer SawRight;
+    public ModelRenderer SawLeft;
+    public ModelRenderer RimRight;
+    public ModelRenderer RimBack;
+    public ModelRenderer RimLeft;
 
-	final ModelRenderer Pedal;
+    public ModelMasonryBench() {
+        this.textureWidth = 128;
+        this.textureHeight = 128;
+        this.SawRotor = new ModelRenderer(this, 40, 29);
+        this.SawRotor.setRotationPoint(2.0F, 11.0F, 0.0F);
+        this.SawRotor.addBox(0.0F, -0.5F, -0.5F, 3, 1, 1, 0.0F);
+        this.setRotateAngle(SawRotor, 0.7853981852531433F, -0.0F, 0.0F);
+        this.RimBack = new ModelRenderer(this, 32, 14);
+        this.RimBack.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.RimBack.addBox(-8.0F, 8.0F, 7.0F, 16, 2, 1, 0.0F);
+        this.RimLeft = new ModelRenderer(this, 66, 0);
+        this.RimLeft.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.RimLeft.addBox(7.0F, 8.0F, -8.0F, 1, 2, 15, 0.0F);
+        this.RimRight = new ModelRenderer(this, 0, 0);
+        this.RimRight.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.RimRight.addBox(-8.0F, 8.0F, -8.0F, 1, 2, 15, 0.0F);
+        this.Workslab = new ModelRenderer(this, 0, 17);
+        this.Workslab.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Workslab.addBox(-6.0F, 9.5F, -7.0F, 7, 1, 13, 0.0F);
+        this.SawRight = new ModelRenderer(this, 48, 21);
+        this.SawRight.setRotationPoint(2.0F, 11.0F, 0.0F);
+        this.SawRight.addBox(0.8999999761581421F, -2.5F, -2.5F, 1, 5, 5, 0.0F);
+        this.TopBack = new ModelRenderer(this, 52, 44);
+        this.TopBack.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.TopBack.addBox(2.0F, 10.0F, 5.0F, 3, 2, 3, 0.0F);
+        this.TopRight = new ModelRenderer(this, 0, 31);
+        this.TopRight.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.TopRight.addBox(-8.0F, 10.0F, -8.0F, 10, 2, 16, 0.0F);
+        this.TopLeft = new ModelRenderer(this, 64, 31);
+        this.TopLeft.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.TopLeft.addBox(5.0F, 10.0F, -8.0F, 3, 2, 16, 0.0F);
+        this.SawLeft = new ModelRenderer(this, 60, 21);
+        this.SawLeft.setRotationPoint(2.0F, 11.0F, 0.0F);
+        this.SawLeft.addBox(1.100000023841858F, -2.5F, -2.5F, 1, 5, 5, 0.0F);
+        this.setRotateAngle(SawLeft, 0.7853981852531433F, -0.0F, 0.0F);
+        this.MainLeft = new ModelRenderer(this, 0, 49);
+        this.MainLeft.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.MainLeft.addBox(-8.0F, 12.0F, -8.0F, 16, 12, 16, 0.0F);
+    }
 
-	final ModelRenderer MainLeft;
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+        super.render(entity, f, f1, f2, f3, f4, f5);
+        this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        this.render(f5);
+    }
 
-	final ModelRenderer MainBack;
+    public void render(float f5) {
+        this.SawRotor.render(f5);
+        this.RimBack.render(f5);
+        this.RimLeft.render(f5);
+        this.RimRight.render(f5);
+        this.Workslab.render(f5);
+        this.SawRight.render(f5);
+        this.TopBack.render(f5);
+        this.TopRight.render(f5);
+        this.TopLeft.render(f5);
+        this.SawLeft.render(f5);
+        this.MainLeft.render(f5);
+    }
 
-	final ModelRenderer MainRight;
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
+    }
 
-	final ModelRenderer MainTop;
-
-	final ModelRenderer TopRight;
-
-	final ModelRenderer TopBack;
-
-	final ModelRenderer TopLeft;
-
-	final ModelRenderer Workslab;
-
-	final ModelRenderer SawRotor;
-
-	final ModelRenderer SawRight;
-
-	final ModelRenderer SawLeft;
-
-	final ModelRenderer RimRight;
-
-	final ModelRenderer RimBack;
-
-	final ModelRenderer RimLeft;
-
-	public ModelMasonryBench()
-	{
-		this.textureWidth = 128;
-		this.textureHeight = 256;
-
-		this.MainBottom = new ModelRenderer(this, 33, 91);
-		this.MainBottom.addBox(-8F, 22F, -8F, 16, 2, 16);
-		this.MainBottom.setRotationPoint(0F, 0F, 0F);
-		this.MainBottom.setTextureSize(128, 256);
-		this.MainBottom.mirror = true;
-		this.setRotation(this.MainBottom, 0F, 0F, 0F);
-		this.Pedal = new ModelRenderer(this, 97, 91);
-		this.Pedal.addBox(-2F, 0F, -5F, 4, 2, 5);
-		this.Pedal.setRotationPoint(3.5F, 19F, -2F);
-		this.Pedal.setTextureSize(128, 256);
-		this.Pedal.mirror = true;
-		this.setRotation(this.Pedal, 0.1745329F, 0F, 0F);
-		this.MainLeft = new ModelRenderer(this, 0, 67);
-		this.MainLeft.addBox(-8F, 14F, -8F, 9, 8, 16);
-		this.MainLeft.setRotationPoint(0F, 0F, 0F);
-		this.MainLeft.setTextureSize(128, 256);
-		this.MainLeft.mirror = true;
-		this.setRotation(this.MainLeft, 0F, 0F, 0F);
-		this.MainBack = new ModelRenderer(this, 50, 73);
-		this.MainBack.addBox(1F, 14F, -2F, 5, 8, 10);
-		this.MainBack.setRotationPoint(0F, 0F, 0F);
-		this.MainBack.setTextureSize(128, 256);
-		this.MainBack.mirror = true;
-		this.setRotation(this.MainBack, 0F, 0F, 0F);
-		this.MainRight = new ModelRenderer(this, 80, 67);
-		this.MainRight.addBox(6F, 14F, -8F, 2, 8, 16);
-		this.MainRight.setRotationPoint(0F, 0F, 0F);
-		this.MainRight.setTextureSize(128, 256);
-		this.MainRight.mirror = true;
-		this.setRotation(this.MainRight, 0F, 0F, 0F);
-		this.MainTop = new ModelRenderer(this, 33, 49);
-		this.MainTop.addBox(-8F, 12F, -8F, 16, 2, 16);
-		this.MainTop.setRotationPoint(0F, 0F, 0F);
-		this.MainTop.setTextureSize(128, 256);
-		this.MainTop.mirror = true;
-		this.setRotation(this.MainTop, 0F, 0F, 0F);
-		this.TopRight = new ModelRenderer(this, 0, 31);
-		this.TopRight.addBox(-8F, 10F, -8F, 10, 2, 16);
-		this.TopRight.setRotationPoint(0F, 0F, 0F);
-		this.TopRight.setTextureSize(128, 256);
-		this.TopRight.mirror = true;
-		this.setRotation(this.TopRight, 0F, 0F, 0F);
-		this.TopBack = new ModelRenderer(this, 52, 44);
-		this.TopBack.addBox(2F, 10F, 5F, 3, 2, 3);
-		this.TopBack.setRotationPoint(0F, 0F, 0F);
-		this.TopBack.setTextureSize(128, 256);
-		this.TopBack.mirror = true;
-		this.setRotation(this.TopBack, 0F, 0F, 0F);
-		this.TopLeft = new ModelRenderer(this, 64, 31);
-		this.TopLeft.addBox(5F, 10F, -8F, 3, 2, 16);
-		this.TopLeft.setRotationPoint(0F, 0F, 0F);
-		this.TopLeft.setTextureSize(128, 256);
-		this.TopLeft.mirror = true;
-		this.setRotation(this.TopLeft, 0F, 0F, 0F);
-		this.Workslab = new ModelRenderer(this, 0, 17);
-		this.Workslab.addBox(-6F, 9.5F, -7F, 7, 1, 13);
-		this.Workslab.setRotationPoint(0F, 0F, 0F);
-		this.Workslab.setTextureSize(128, 256);
-		this.Workslab.mirror = true;
-		this.setRotation(this.Workslab, 0F, 0F, 0F);
-		this.SawRotor = new ModelRenderer(this, 40, 29);
-		this.SawRotor.addBox(0F, -0.5F, -0.5F, 3, 1, 1);
-		this.SawRotor.setRotationPoint(2F, 11F, 0F);
-		this.SawRotor.setTextureSize(128, 256);
-		this.SawRotor.mirror = true;
-		this.setRotation(this.SawRotor, 0.7853982F, 0F, 0F);
-		this.SawRight = new ModelRenderer(this, 48, 21);
-		this.SawRight.addBox(0.9F, -2.5F, -2.5F, 1, 5, 5);
-		this.SawRight.setRotationPoint(2F, 11F, 0F);
-		this.SawRight.setTextureSize(128, 256);
-		this.SawRight.mirror = true;
-		this.setRotation(this.SawRight, 0F, 0F, 0F);
-		this.SawLeft = new ModelRenderer(this, 60, 21);
-		this.SawLeft.addBox(1.1F, -2.5F, -2.5F, 1, 5, 5);
-		this.SawLeft.setRotationPoint(2F, 11F, 0F);
-		this.SawLeft.setTextureSize(128, 256);
-		this.SawLeft.mirror = true;
-		this.setRotation(this.SawLeft, 0.7853982F, 0F, 0F);
-		this.RimRight = new ModelRenderer(this, 0, 0);
-		this.RimRight.addBox(-8F, 8F, -8F, 1, 2, 15);
-		this.RimRight.setRotationPoint(0F, 0F, 0F);
-		this.RimRight.setTextureSize(128, 256);
-		this.RimRight.mirror = true;
-		this.setRotation(this.RimRight, 0F, 0F, 0F);
-		this.RimBack = new ModelRenderer(this, 32, 14);
-		this.RimBack.addBox(-8F, 8F, 7F, 16, 2, 1);
-		this.RimBack.setRotationPoint(0F, 0F, 0F);
-		this.RimBack.setTextureSize(128, 256);
-		this.RimBack.mirror = true;
-		this.setRotation(this.RimBack, 0F, 0F, 0F);
-		this.RimLeft = new ModelRenderer(this, 66, 0);
-		this.RimLeft.addBox(7F, 8F, -8F, 1, 2, 15);
-		this.RimLeft.setRotationPoint(0F, 0F, 0F);
-		this.RimLeft.setTextureSize(128, 256);
-		this.RimLeft.mirror = true;
-		this.setRotation(this.RimLeft, 0F, 0F, 0F);
-	}
-
-	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-	{
-		super.render(entity, f, f1, f2, f3, f4, f5);
-		this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		this.render(f5);
-	}
-
-	public void render(float f5)
-	{
-		this.MainBottom.render(f5);
-		this.Pedal.render(f5);
-		this.MainLeft.render(f5);
-		this.MainBack.render(f5);
-		this.MainRight.render(f5);
-		this.MainTop.render(f5);
-		this.TopRight.render(f5);
-		this.TopBack.render(f5);
-		this.TopLeft.render(f5);
-		this.Workslab.render(f5);
-		this.SawRotor.render(f5);
-		this.SawRight.render(f5);
-		this.SawLeft.render(f5);
-		this.RimRight.render(f5);
-		this.RimBack.render(f5);
-		this.RimLeft.render(f5);
-	}
-
-	private void setRotation(ModelRenderer model, float x, float y, float z)
-	{
-		model.rotateAngleX = x;
-		model.rotateAngleY = y;
-		model.rotateAngleZ = z;
-	}
-
-	@Override
-	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-	{
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-	}
-
+    public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
+    {
+        super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+    }
 }
