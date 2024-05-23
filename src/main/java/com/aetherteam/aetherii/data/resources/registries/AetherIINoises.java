6666493b56a684251b8e5326751cb26cad41cf80
Builderdog841 <timon.kempter@gmail.com>
2024-05-23 21:56:02 +0200

    Added Erosion Noise, Tweaked Noise Scale

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIINoises.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIINoises.java
@@ -10,14 +10,16 @@ import net.minecraft.world.level.levelgen.synth.NormalNoise;
 public class AetherIINoises {
     public static final ResourceKey<NormalNoise.NoiseParameters> TEMPERATURE = createKey("temperature");
     public static final ResourceKey<NormalNoise.NoiseParameters> VEGETATION = createKey("vegetation");
+    public static final ResourceKey<NormalNoise.NoiseParameters> EROSION = createKey("erosion");
 
     private static ResourceKey<NormalNoise.NoiseParameters> createKey(String name) {
         return ResourceKey.create(Registries.NOISE, new ResourceLocation(AetherII.MODID, name));
     }
 
     public static void bootstrap(BootstapContext<NormalNoise.NoiseParameters> ctx) {
-        register(ctx, TEMPERATURE, -8, 1.5D, 0.0D, 1.0D, 0.0D, 0.0D, 0.0D);
-        register(ctx, VEGETATION, -7, 1.0D, 1.0D, 0.0D, 0.0D, 0.0D, 0.0D);
+        register(ctx, TEMPERATURE, -9, 1.5D, 0.0D, 1.0D, 0.0D, 0.0D, 0.0D);
+        register(ctx, VEGETATION, -8, 1.0D, 1.0D, 0.0D, 0.0D, 0.0D, 0.0D);
+        register(ctx, EROSION, -9, 1.5, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
     }
 
     public static void register(BootstapContext<NormalNoise.NoiseParameters> ctx, ResourceKey<NormalNoise.NoiseParameters> key, int firstOctave, double firstAmplitude, double... amplitudes) {
