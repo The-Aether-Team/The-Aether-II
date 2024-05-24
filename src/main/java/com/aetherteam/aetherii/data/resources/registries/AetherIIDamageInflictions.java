44d04502a38fd99f5aba5637846fad0f4b9e67cc
bconlon <bconlon.gml@gmail.com>
2024-05-24 12:05:30 -0700

    improv: getter methods

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDamageInflictions.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDamageInflictions.java
@@ -3,10 +3,14 @@ package com.aetherteam.aetherii.data.resources.registries;
 import com.aetherteam.aetherii.AetherII;
 import com.aetherteam.aetherii.api.damage.DamageInfliction;
 import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+
+import java.util.Map;
 
 public class AetherIIDamageInflictions {
     public static final ResourceKey<Registry<DamageInfliction>> DAMAGE_INFLICTION_REGISTRY_KEY = ResourceKey.createRegistryKey(new ResourceLocation(AetherII.MODID, "damage_infliction"));
@@ -19,5 +23,32 @@ public class AetherIIDamageInflictions {
 
     public static void bootstrap(BootstapContext<DamageInfliction> context) {
         context.register(EXAMPLE, new DamageInfliction(Items.APPLE, 100, 100, 100));
+    }
+
+    public static int getSlashDamage(RegistryAccess registryAccess, ItemStack item) {
+        for (Map.Entry<ResourceKey<DamageInfliction>, DamageInfliction> entry : registryAccess.registryOrThrow(DAMAGE_INFLICTION_REGISTRY_KEY).entrySet()) {
+            if (item.is(entry.getValue().item())) {
+                return entry.getValue().slashValue();
+            }
+        }
+        return 0;
+    }
+
+    public static int getImpactDamage(RegistryAccess registryAccess, ItemStack item) {
+        for (Map.Entry<ResourceKey<DamageInfliction>, DamageInfliction> entry : registryAccess.registryOrThrow(DAMAGE_INFLICTION_REGISTRY_KEY).entrySet()) {
+            if (item.is(entry.getValue().item())) {
+                return entry.getValue().impactValue();
+            }
+        }
+        return 0;
+    }
+
+    public static int getPierceDamage(RegistryAccess registryAccess, ItemStack item) {
+        for (Map.Entry<ResourceKey<DamageInfliction>, DamageInfliction> entry : registryAccess.registryOrThrow(DAMAGE_INFLICTION_REGISTRY_KEY).entrySet()) {
+            if (item.is(entry.getValue().item())) {
+                return entry.getValue().pierceValue();
+            }
+        }
+        return 0;
     }
 }
--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDamageResistances.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDamageResistances.java
@@ -3,10 +3,14 @@ package com.aetherteam.aetherii.data.resources.registries;
 import com.aetherteam.aetherii.AetherII;
 import com.aetherteam.aetherii.api.damage.DamageResistance;
 import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+
+import java.util.Map;
 
 public class AetherIIDamageResistances {
     public static final ResourceKey<Registry<DamageResistance>> DAMAGE_RESISTANCE_REGISTRY_KEY = ResourceKey.createRegistryKey(new ResourceLocation(AetherII.MODID, "damage_resistance"));
@@ -19,5 +23,32 @@ public class AetherIIDamageResistances {
 
     public static void bootstrap(BootstapContext<DamageResistance> context) {
         context.register(EXAMPLE, new DamageResistance(EntityType.PIG, 100, 100, 100));
+    }
+
+    public static int getSlashDefense(RegistryAccess registryAccess, Entity entity) {
+        for (Map.Entry<ResourceKey<DamageResistance>, DamageResistance> entry : registryAccess.registryOrThrow(DAMAGE_RESISTANCE_REGISTRY_KEY).entrySet()) {
+            if (entity.getType() == entry.getValue().entityType()) {
+                return entry.getValue().slashValue();
+            }
+        }
+        return 0;
+    }
+
+    public static int getImpactDefense(RegistryAccess registryAccess, Entity entity) {
+        for (Map.Entry<ResourceKey<DamageResistance>, DamageResistance> entry : registryAccess.registryOrThrow(DAMAGE_RESISTANCE_REGISTRY_KEY).entrySet()) {
+            if (entity.getType() == entry.getValue().entityType()) {
+                return entry.getValue().impactValue();
+            }
+        }
+        return 0;
+    }
+
+    public static int getPierceDefense(RegistryAccess registryAccess, Entity entity) {
+        for (Map.Entry<ResourceKey<DamageResistance>, DamageResistance> entry : registryAccess.registryOrThrow(DAMAGE_RESISTANCE_REGISTRY_KEY).entrySet()) {
+            if (entity.getType() == entry.getValue().entityType()) {
+                return entry.getValue().pierceValue();
+            }
+        }
+        return 0;
     }
 }
