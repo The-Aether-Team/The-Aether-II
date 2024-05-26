aa02b96cd291a70cd313a69a8e500aeb032564e4
Builderdog841 <timon.kempter@gmail.com>
2024-05-26 21:07:19 +0200

    Attempt on Resolving Merge Issues

---
+++ src/main/java/com/aetherteam/aetherii/AetherII.java
@@ -0,0 +1,96 @@
+package com.aetherteam.aetherii;
+
+import com.aetherteam.aetherii.attachment.AetherIIDataAttachments;
+import com.aetherteam.aetherii.block.AetherIIBlocks;
+import com.aetherteam.aetherii.blockentity.AetherIIBlockEntityTypes;
+import com.aetherteam.aetherii.client.AetherIIClient;
+import com.aetherteam.aetherii.client.AetherIISoundEvents;
+import com.aetherteam.aetherii.client.particle.AetherIIParticleTypes;
+import com.aetherteam.aetherii.data.AetherIIData;
+import com.aetherteam.aetherii.entity.AetherIIEntityTypes;
+import com.aetherteam.aetherii.event.listeners.WorldInteractionListener;
+import com.aetherteam.aetherii.event.listeners.PortalTeleportationListener;
+import com.aetherteam.aetherii.inventory.menu.AetherIIMenuTypes;
+import com.aetherteam.aetherii.item.AetherIICreativeTabs;
+import com.aetherteam.aetherii.item.AetherIIItems;
+import com.aetherteam.aetherii.network.packet.PortalTeleportationSyncPacket;
+import com.aetherteam.aetherii.network.packet.clientbound.PortalTravelSoundPacket;
+import com.aetherteam.aetherii.world.AetherIIPoi;
+import com.aetherteam.aetherii.world.feature.AetherIIFeatures;
+import com.aetherteam.aetherii.world.structure.AetherIIStructureTypes;
+import com.mojang.logging.LogUtils;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
+import net.neoforged.neoforge.network.registration.IPayloadRegistrar;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
+
+@Mod(AetherII.MODID)
+public class AetherII {
+    public static final String MODID = "aether_ii";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public AetherII(IEventBus bus, Dist dist) {
+        bus.addListener(AetherII::commonSetup);
+        bus.addListener(AetherIIData::dataSetup);
+        bus.addListener(this::registerPackets);
+
+        DeferredRegister<?>[] registers = {
+                AetherIIBlocks.BLOCKS,
+                AetherIIItems.ITEMS,
+                AetherIIEntityTypes.ENTITY_TYPES,
+                AetherIIBlockEntityTypes.BLOCK_ENTITY_TYPES,
+                AetherIICreativeTabs.CREATIVE_MODE_TABS,
+                AetherIIFeatures.FEATURES,
+                AetherIIStructureTypes.STRUCTURE_TYPES,
+                AetherIIParticleTypes.PARTICLES,
+                AetherIISoundEvents.SOUNDS,
+                AetherIIMenuTypes.MENU_TYPES,
+                AetherIIPoi.POI,
+                AetherIIDataAttachments.ATTACHMENTS
+        };
+
+        for (DeferredRegister<?> register : registers) {
+            register.register(bus);
+        }
+
+        AetherII.eventSetup(bus);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, AetherIIConfig.SERVER_SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, AetherIIConfig.COMMON_SPEC);
+
+        if (dist == Dist.CLIENT) {
+            AetherIIClient.clientInit(bus);
+        }
+    }
+
+    public static void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    public static void eventSetup(IEventBus neoBus) {
+        IEventBus bus = NeoForge.EVENT_BUS;
+
+        PortalTeleportationListener.listen(bus);
+        WorldInteractionListener.listen(bus);
+
+        neoBus.addListener(AetherIIEntityTypes::registerSpawnPlacements);
+        neoBus.addListener(AetherIIEntityTypes::registerEntityAttributes);
+    }
+
+    public void registerPackets(RegisterPayloadHandlerEvent event) {
+        IPayloadRegistrar registrar = event.registrar(MODID).versioned("1.0.0").optional();
+
+        // CLIENTBOUND
+        registrar.play(PortalTravelSoundPacket.ID, PortalTravelSoundPacket::decode, payload -> payload.client(PortalTravelSoundPacket::handle));
+
+        // BOTH
+        registrar.play(PortalTeleportationSyncPacket.ID, PortalTeleportationSyncPacket::decode, PortalTeleportationSyncPacket::handle);
+    }
+}
