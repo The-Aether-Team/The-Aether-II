8dac3e89b638f0ffd392b3211b5eaf9ef0caae9f
Builderdog841 <timon.kempter@gmail.com>
2024-05-26 20:33:42 +0200

    Attempt on Resolving Merge Issues

--- src/main/java/com/aetherteam/aetherii/AetherII.java
+++ src/main/java/com/aetherteam/aetherii/AetherII.java
@@ -2,7 +2,6 @@ package com.aetherteam.aetherii;
 
 import com.aetherteam.aetherii.attachment.AetherIIDataAttachments;
 import com.aetherteam.aetherii.block.AetherIIBlocks;
-import com.aetherteam.aetherii.blockentity.AetherIIBlockEntityTypes;
 import com.aetherteam.aetherii.client.AetherIIClient;
 import com.aetherteam.aetherii.client.AetherIISoundEvents;
 import com.aetherteam.aetherii.client.particle.AetherIIParticleTypes;
@@ -10,8 +9,6 @@ import com.aetherteam.aetherii.data.AetherIIData;
 import com.aetherteam.aetherii.entity.AetherIIEntityTypes;
 import com.aetherteam.aetherii.event.listeners.WorldInteractionListener;
 import com.aetherteam.aetherii.event.listeners.PortalTeleportationListener;
-import com.aetherteam.aetherii.event.listeners.abilities.ToolAbilityListener;
-import com.aetherteam.aetherii.inventory.menu.AetherIIMenuTypes;
 import com.aetherteam.aetherii.item.AetherIICreativeTabs;
 import com.aetherteam.aetherii.item.AetherIIItems;
 import com.aetherteam.aetherii.network.packet.PortalTeleportationSyncPacket;
@@ -79,8 +76,8 @@ public class AetherII {
         IEventBus bus = NeoForge.EVENT_BUS;
 
         ToolAbilityListener.listen(bus);
-        AetherIIPlayerListener.listen(bus);
-        DimensionListener.listen(bus);
+        PortalTeleportationListener.listen(bus);
+        WorldInteractionListener.listen(bus);
 
         neoBus.addListener(AetherIIEntityTypes::registerSpawnPlacements);
         neoBus.addListener(AetherIIEntityTypes::registerEntityAttributes);
@@ -93,6 +90,6 @@ public class AetherII {
         registrar.play(PortalTravelSoundPacket.ID, PortalTravelSoundPacket::decode, payload -> payload.client(PortalTravelSoundPacket::handle));
 
         // BOTH
-        registrar.play(AetherIIPlayerSyncPacket.ID, AetherIIPlayerSyncPacket::decode, AetherIIPlayerSyncPacket::handle);
+        registrar.play(PortalTeleportationSyncPacket.ID, PortalTeleportationSyncPacket::decode, PortalTeleportationSyncPacket::handle);
     }
 }
