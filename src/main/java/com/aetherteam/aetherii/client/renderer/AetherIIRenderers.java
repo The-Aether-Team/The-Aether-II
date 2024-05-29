43e49646ffa9ab52940e2c7d4d12ec7e3098eac2
Builderdog841 <timon.kempter@gmail.com>
2024-05-29 14:46:12 +0200

    Client Class Fixes

--- src/main/java/com/aetherteam/aetherii/client/renderer/AetherIIRenderers.java
+++ src/main/java/com/aetherteam/aetherii/client/renderer/AetherIIRenderers.java
@@ -2,11 +2,19 @@ package com.aetherteam.aetherii.client.renderer;
 
 import com.aetherteam.aetherii.blockentity.AetherIIBlockEntityTypes;
 import com.aetherteam.aetherii.client.renderer.blockentity.SkyrootChestRenderer;
+import com.aetherteam.aetherii.client.renderer.model.AerbunnyModel;
+import com.aetherteam.aetherii.entity.AetherIIEntityTypes;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
 public class AetherIIRenderers {
 
     public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(AetherIIBlockEntityTypes.SKYROOT_CHEST.get(), SkyrootChestRenderer::new);
+        event.registerEntityRenderer(AetherIIEntityTypes.AERBUNNY.get(), AerbunnyRenderer::new);
+    }
+
+    public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(AetherModelLayers.AERBUNNY, AerbunnyModel::createBodyLayer);
+        event.registerLayerDefinition(AetherModelLayers.AERBUNNY_COLLAR, AerbunnyModel::createBodyLayer);
     }
 }
