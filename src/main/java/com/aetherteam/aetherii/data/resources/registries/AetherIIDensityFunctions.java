847c0aab1444bf97788b256fc055fe87ff72e20e
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-05-31 17:38:56 -0400

    fix: maybe fix cloud

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDensityFunctions.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDensityFunctions.java
@@ -40,8 +40,8 @@ public class AetherIIDensityFunctions {
                 8.0D // smear scale multiplier, capped at 8
         ));
 
-        context.register(CLOUDBED_BASE, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.00375D, noise.getOrThrow(AetherIINoises.CLOUDBED_BASE)));
-        context.register(CLOUDBED_Y_OFFSET, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.0025D, noise.getOrThrow(AetherIINoises.CLOUDBED_Y_OFFSET)));
+        context.register(CLOUDBED_BASE, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.375D, noise.getOrThrow(AetherIINoises.CLOUDBED_BASE)));
+        context.register(CLOUDBED_Y_OFFSET, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.25D, noise.getOrThrow(AetherIINoises.CLOUDBED_Y_OFFSET)));
     }
 
     public static DensityFunction getFunction(HolderGetter<DensityFunction> densityFunctions, ResourceKey<DensityFunction> key) {
