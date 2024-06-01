d126a1c832278198bc2c8f3ccf57569b2ce396f6
Builderdog841 <timon.kempter@gmail.com>
2024-06-01 14:15:22 +0200

    Irradiated Holystone Generation

--- src/main/java/com/aetherteam/aetherii/data/resources/builders/highlands/HighlandsSurfaceBuilders.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/builders/highlands/HighlandsSurfaceBuilders.java
@@ -1,7 +1,6 @@
 package com.aetherteam.aetherii.data.resources.builders.highlands;
 
 import com.aetherteam.aetherii.block.AetherIIBlocks;
-import com.aetherteam.aetherii.data.resources.builders.AetherIIDensityFunctionBuilders;
 import com.aetherteam.aetherii.data.resources.registries.AetherIIBiomes;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -15,16 +14,16 @@ public class HighlandsSurfaceBuilders {
         SurfaceRules.RuleSource surface = SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.waterBlockCheck(-1, 0), AETHER_GRASS_BLOCK), AETHER_DIRT);
         return SurfaceRules.sequence(
 
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.CONTAMINATED_JUNGLE, AetherIIBiomes.BATTLEGROUND_WASTES),
+                        SurfaceRules.ifTrue(SurfaceRules.steep(),
+                                SurfaceRules.ifTrue(SurfaceRules.not(
+                                        SurfaceRules.verticalGradient("irradiated_holystone", VerticalAnchor.belowTop(296), VerticalAnchor.belowTop(288))), IRRADIATED_HOLYSTONE))),
+
                 SurfaceRules.ifTrue(SurfaceRules.not(
                         SurfaceRules.verticalGradient("aether_grass_block", VerticalAnchor.belowTop(276), VerticalAnchor.belowTop(272))), SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, surface)),
 
                 SurfaceRules.ifTrue(SurfaceRules.not(
                         SurfaceRules.verticalGradient("aether_dirt", VerticalAnchor.belowTop(272), VerticalAnchor.belowTop(272))), SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, AETHER_DIRT)),
-
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.CONTAMINATED_JUNGLE, AetherIIBiomes.BATTLEGROUND_WASTES),
-                        SurfaceRules.ifTrue(SurfaceRules.steep(),
-                                SurfaceRules.ifTrue(SurfaceRules.not(
-                                        SurfaceRules.verticalGradient("irradiated_holystone", VerticalAnchor.belowTop(296), VerticalAnchor.belowTop(288))), SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, IRRADIATED_HOLYSTONE)))),
 
                 SurfaceRules.ifTrue(SurfaceRules.verticalGradient("undershale", VerticalAnchor.absolute(64), VerticalAnchor.absolute(72)), SurfaceRules.state(AetherIIBlocks.UNDERSHALE.get().defaultBlockState())));
     }
