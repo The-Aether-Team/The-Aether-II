092270739cb26c13c724680702c60f95571b6d5f
Builderdog <81527321+Builderdog841@users.noreply.github.com>
2024-06-01 20:10:27 +0200

    feat: Worldgen Improvements (#254)
    ## Worldgen Improvements
    - Reduced Island Smear Scale for more balanced Island Sizes
    - Added  a basic Depth Parameter
    - Frigid Sierras will now spawn above a certain y-level in arctic biomes
    (their spawning isn't final yet)
    - Fixed Ore Spawning
    - Improved Structure Spawning Checks
    - Refactored Density Function Code
    
    ---
    
    I agree to the Contributor License Agreement (CLA).

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDensityFunctions.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIDensityFunctions.java
@@ -1,39 +1,15 @@
 package com.aetherteam.aetherii.data.resources.registries;
 
-import com.aetherteam.aetherii.AetherII;
+import com.aetherteam.aetherii.data.resources.builders.AetherIIDensityFunctionBuilders;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.DensityFunction;
 import net.minecraft.world.level.levelgen.DensityFunctions;
 import net.minecraft.world.level.levelgen.synth.BlendedNoise;
 import net.minecraft.world.level.levelgen.synth.NormalNoise;
 
-public class AetherIIDensityFunctions {
-    public static final ResourceKey<DensityFunction> TEMPERATURE = createKey("highlands/temperature");
-    public static final ResourceKey<DensityFunction> EROSION = createKey("highlands/erosion");
-    public static final ResourceKey<DensityFunction> ELEVATION = createKey("highlands/elevation");
-    public static final ResourceKey<DensityFunction> FACTOR = createKey("highlands/factor"); //TODO: Add to Datagen
-    public static final ResourceKey<DensityFunction> BOTTOM_SLIDE = createKey("highlands/bottom_slide"); //TODO: Add to Datagen
-    public static final ResourceKey<DensityFunction> TOP_SLIDE = createKey("highlands/top_slide"); //TODO: Add to Datagen
-    public static final ResourceKey<DensityFunction> SLOPER = createKey("highlands/sloper"); //TODO: Add to Datagen
-    public static final ResourceKey<DensityFunction> BASE_3D_NOISE = createKey("highlands/base_3d_noise");
-    public static final ResourceKey<DensityFunction> AMPLIFICATION = createKey("highlands/amplification"); //TODO: Add to Datagen
-    public static final ResourceKey<DensityFunction> TERRAIN_SHAPER = createKey("highlands/terrain_shaper"); //TODO: Add to Datagen
-
-    private static ResourceKey<DensityFunction> createKey(String name) {
-        return ResourceKey.create(Registries.DENSITY_FUNCTION, new ResourceLocation(AetherII.MODID, name));
-    }
-
-    public static final ResourceKey<DensityFunction> SHIFT_X = createVanillaKey("shift_x");
-    public static final ResourceKey<DensityFunction> SHIFT_Z = createVanillaKey("shift_z");
-    public static final ResourceKey<DensityFunction> Y = createVanillaKey("y");
-
-    private static ResourceKey<DensityFunction> createVanillaKey(String name) {
-        return ResourceKey.create(Registries.DENSITY_FUNCTION, new ResourceLocation(name));
-    }
+public class AetherIIDensityFunctions extends AetherIIDensityFunctionBuilders {
 
     public static void bootstrap(BootstapContext<DensityFunction> context) {
         HolderGetter<DensityFunction> function = context.lookup(Registries.DENSITY_FUNCTION);
@@ -43,6 +19,7 @@ public class AetherIIDensityFunctions {
 
         context.register(TEMPERATURE, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.5D, noise.getOrThrow(AetherIINoises.TEMPERATURE)));
         context.register(EROSION, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.5D, noise.getOrThrow(AetherIINoises.EROSION)).abs());
+        context.register(DEPTH, DensityFunctions.yClampedGradient(0, 384, -1.5, 1.5));
         context.register(ELEVATION, DensityFunctions.shiftedNoise2d(shiftX, shiftZ, 0.75D, noise.getOrThrow(AetherIINoises.ELEVATION)).abs());
 
         context.register(BASE_3D_NOISE, BlendedNoise.createUnseeded(
@@ -50,20 +27,9 @@ public class AetherIIDensityFunctions {
                 0.02D, // y scale
                 80D, // xz factor
                 160D, // y factor
-                8.0D // smear scale multiplier, capped at 8
+                1.0D // smear scale multiplier, capped at 8
         ));
 
         context.register(TERRAIN_SHAPER, makeTerrainShaper(function));
-    }
-
-    public static DensityFunction makeTerrainShaper(HolderGetter<DensityFunction> function) {
-        DensityFunction density = getFunction(function, AMPLIFICATION);
-        density = DensityFunctions.add(density, DensityFunctions.yClampedGradient(96, 128, 0.75, 0.35));
-        density = DensityFunctions.mul(density, getFunction(function, SLOPER));
-        return density.clamp(0, 1);
-    }
-
-    private static DensityFunction getFunction(HolderGetter<DensityFunction> densityFunctions, ResourceKey<DensityFunction> key) {
-        return new DensityFunctions.HolderHolder(densityFunctions.getOrThrow(key));
     }
 }
