ebc934865c699185336aa11dca1120a944d686eb
Builderdog841 <timon.kempter@gmail.com>
2024-06-01 10:01:26 +0200

    Mossy Wisproot Logs now spawn naturally at the bottom of Wisproot Trees

---
+++ src/main/java/com/aetherteam/aetherii/world/tree/decorator/AetherIITreeDecoratorTypes.java
@@ -0,0 +1,13 @@
+package com.aetherteam.aetherii.world.tree.decorator;
+
+import com.aetherteam.aetherii.AetherII;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorType;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class AetherIITreeDecoratorTypes {
+    public static final DeferredRegister<TreeDecoratorType<?>> TREE_DECORATORS = DeferredRegister.create(BuiltInRegistries.TREE_DECORATOR_TYPE, AetherII.MODID);
+
+    public static final DeferredHolder<TreeDecoratorType<?>, TreeDecoratorType<WisprootTreeDecorator>> WISPROOT_DECORATOR = TREE_DECORATORS.register("wisproot_decorator", () -> new TreeDecoratorType<>(WisprootTreeDecorator.CODEC));
+}
