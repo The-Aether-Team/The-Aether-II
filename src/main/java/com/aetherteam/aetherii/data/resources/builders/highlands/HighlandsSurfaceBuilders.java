725380247d67e0cfd1035d1bc9d34fbc8641a8fe
Builderdog841 <timon.kempter@gmail.com>
2024-06-01 14:30:04 +0200

    Removed Irradiated Holystone Generation

--- src/main/java/com/aetherteam/aetherii/data/resources/builders/highlands/HighlandsSurfaceBuilders.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/builders/highlands/HighlandsSurfaceBuilders.java
@@ -8,16 +8,10 @@ import net.minecraft.world.level.levelgen.VerticalAnchor;
 public class HighlandsSurfaceBuilders {
     private static final SurfaceRules.RuleSource AETHER_GRASS_BLOCK = SurfaceRules.state(AetherIIBlocks.AETHER_GRASS_BLOCK.get().defaultBlockState());
     private static final SurfaceRules.RuleSource AETHER_DIRT = SurfaceRules.state(AetherIIBlocks.AETHER_DIRT.get().defaultBlockState());
-    private static final SurfaceRules.RuleSource IRRADIATED_HOLYSTONE = SurfaceRules.state(AetherIIBlocks.IRRADIATED_HOLYSTONE.get().defaultBlockState());
 
     public static SurfaceRules.RuleSource surfaceRules() {
         SurfaceRules.RuleSource surface = SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.waterBlockCheck(-1, 0), AETHER_GRASS_BLOCK), AETHER_DIRT);
         return SurfaceRules.sequence(
-
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.CONTAMINATED_JUNGLE, AetherIIBiomes.BATTLEGROUND_WASTES),
-                        SurfaceRules.ifTrue(SurfaceRules.steep(),
-                                SurfaceRules.ifTrue(SurfaceRules.not(
-                                        SurfaceRules.verticalGradient("irradiated_holystone", VerticalAnchor.belowTop(296), VerticalAnchor.belowTop(288))), IRRADIATED_HOLYSTONE))),
 
                 SurfaceRules.ifTrue(SurfaceRules.not(
                         SurfaceRules.verticalGradient("aether_grass_block", VerticalAnchor.belowTop(276), VerticalAnchor.belowTop(272))), SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, surface)),
