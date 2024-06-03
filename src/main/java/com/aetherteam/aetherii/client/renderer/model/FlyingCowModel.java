6205ff3abc28bd0281fc072ae9a275a685a17255
baguchan <88538210+baguchan@users.noreply.github.com>
2024-06-03 09:18:24 +0900

    feat: Phyg and FlyingCow Model update (#262)
    `(TITLE)`: type(scope): Phyg and FlyingCow Model update
    
    Updated Phyg and FlyingCow Model!
    ![2024-06-03_07 21
    23](https://github.com/The-Aether-Team/The-Aether-II/assets/88538210/7f430005-d5d5-43b9-8471-8ae60edd33fb)
    
    
    ---
    
    I agree to the Contributor License Agreement (CLA).
    
    ---------
    
    Co-authored-by: bconlon <67203206+bconlon1@users.noreply.github.com>

---
+++ src/main/java/com/aetherteam/aetherii/client/renderer/model/FlyingCowModel.java
@@ -0,0 +1,133 @@
+package com.aetherteam.aetherii.client.renderer.model;// Made with Blockbench 4.10.0
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+import com.aetherteam.aetherii.client.renderer.model.animation.FlyingCowAnimation;
+import com.aetherteam.aetherii.client.renderer.model.animation.WingAnimation;
+import com.aetherteam.aetherii.entity.passive.FlyingCow;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.util.Mth;
+
+public class FlyingCowModel<T extends FlyingCow> extends HierarchicalModel<T> {
+    private final ModelPart root;
+    private final ModelPart body;
+    private final ModelPart wing_left;
+    private final ModelPart wing_right;
+    private final ModelPart tail;
+    private final ModelPart head;
+    private final ModelPart ear_left;
+    private final ModelPart ear_right;
+    private final ModelPart leg_front_right;
+    private final ModelPart leg_front_left;
+    private final ModelPart leg_back_left;
+    private final ModelPart leg_back_right;
+
+    public FlyingCowModel(ModelPart root) {
+        this.root = root;
+        this.body = root.getChild("body");
+        this.head = this.body.getChild("head");
+        this.ear_left = this.head.getChild("ear_left");
+        this.ear_right = this.head.getChild("ear_right");
+        this.wing_left = this.body.getChild("wing_left");
+        this.wing_right = this.body.getChild("wing_right");
+        this.leg_front_left = this.body.getChild("leg_front_left");
+        this.leg_front_right = this.body.getChild("leg_front_right");
+        this.leg_back_left = this.body.getChild("leg_back_left");
+        this.leg_back_right = this.body.getChild("leg_back_right");
+        this.tail = this.body.getChild("tail");
+    }
+
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 21).addBox(-5.0F, -1.0F, 0.0F, 10.0F, 10.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 7.0F, -6.0F));
+
+        PartDefinition wing_left = body.addOrReplaceChild("wing_left", CubeListBuilder.create().texOffs(26, 47).addBox(-1.0F, -5.0F, -5.5F, 2.0F, 18.0F, 11.0F, new CubeDeformation(0.0F))
+                .texOffs(32, 72).mirror().addBox(0.0F, 12.0F, -2.0F, 0.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(5.0F, -1.0F, 7.5F, 0.0F, 0.0F, -0.3054F));
+
+        PartDefinition feather_left_5_r1 = wing_left.addOrReplaceChild("feather_left_5_r1", CubeListBuilder.create().texOffs(40, 76).mirror().addBox(0.0F, -4.0F, -4.0F, 0.0F, 7.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 7.0F, 7.75F, 0.6109F, 0.0F, 0.0F));
+
+        PartDefinition feather_left_4_r1 = wing_left.addOrReplaceChild("feather_left_4_r1", CubeListBuilder.create().texOffs(24, 76).mirror().addBox(0.0F, -4.0F, 0.0F, 0.0F, 7.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 7.0F, -7.75F, -0.6109F, 0.0F, 0.0F));
+
+        PartDefinition feather_left_3_r1 = wing_left.addOrReplaceChild("feather_left_3_r1", CubeListBuilder.create().texOffs(24, 72).mirror().addBox(0.0F, -1.0F, -2.0F, 0.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 12.0F, -3.75F, -0.3491F, 0.0F, 0.0F));
+
+        PartDefinition feather_left_2_r1 = wing_left.addOrReplaceChild("feather_left_2_r1", CubeListBuilder.create().texOffs(40, 72).mirror().addBox(0.0F, -1.0F, 0.75F, 0.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 13.0F, 1.25F, 0.3491F, 0.0F, 0.0F));
+
+        PartDefinition wing_right = body.addOrReplaceChild("wing_right", CubeListBuilder.create().texOffs(0, 47).addBox(-1.0F, -5.0F, -5.5F, 2.0F, 18.0F, 11.0F, new CubeDeformation(0.0F))
+                .texOffs(8, 72).addBox(0.0F, 12.0F, -2.0F, 0.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-5.0F, -1.0F, 7.5F, 0.0F, 0.0F, 0.3054F));
+
+        PartDefinition feather_right_5_r1 = wing_right.addOrReplaceChild("feather_right_5_r1", CubeListBuilder.create().texOffs(0, 76).addBox(0.0F, -4.0F, -4.0F, 0.0F, 7.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 7.0F, 7.75F, 0.6109F, 0.0F, 0.0F));
+
+        PartDefinition feather_right_4_r1 = wing_right.addOrReplaceChild("feather_right_4_r1", CubeListBuilder.create().texOffs(16, 76).addBox(0.0F, -4.0F, 0.0F, 0.0F, 7.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 7.0F, -7.75F, -0.6109F, 0.0F, 0.0F));
+
+        PartDefinition feather_right_3_r1 = wing_right.addOrReplaceChild("feather_right_3_r1", CubeListBuilder.create().texOffs(16, 72).addBox(0.0F, -1.0F, -2.0F, 0.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 12.0F, -3.75F, -0.3491F, 0.0F, 0.0F));
+
+        PartDefinition feather_right_2_r1 = wing_right.addOrReplaceChild("feather_right_2_r1", CubeListBuilder.create().texOffs(0, 72).addBox(0.0F, -1.0F, 0.75F, 0.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 13.0F, 1.25F, 0.3491F, 0.0F, 0.0F));
+
+        PartDefinition tail = body.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 16.0F));
+
+        PartDefinition tail_r1 = tail.addOrReplaceChild("tail_r1", CubeListBuilder.create().texOffs(36, 26).addBox(-2.0F, 0.0F, 0.0F, 4.0F, 11.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.2182F, 0.0F, 0.0F));
+
+        PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 8).addBox(-4.0F, -5.0F, -5.0F, 8.0F, 8.0F, 5.0F, new CubeDeformation(0.0F))
+                .texOffs(26, 16).addBox(-3.0F, 0.0F, -7.0F, 6.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-4.0F, -11.0F, -5.0F, 8.0F, 6.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(34, 0).addBox(2.0F, -9.5F, -1.0F, 7.0F, 6.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(20, 0).addBox(-9.0F, -9.5F, -1.0F, 7.0F, 6.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 2.0F, 0.0F));
+
+        PartDefinition ear_left = head.addOrReplaceChild("ear_left", CubeListBuilder.create(), PartPose.offset(4.0F, -1.0F, -3.0F));
+
+        PartDefinition ear_left_r1 = ear_left.addOrReplaceChild("ear_left_r1", CubeListBuilder.create().texOffs(29, 6).addBox(0.0F, -3.0F, 0.0F, 4.0F, 3.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.1325F, -0.6429F, -0.2187F));
+
+        PartDefinition ear_right = head.addOrReplaceChild("ear_right", CubeListBuilder.create(), PartPose.offset(-4.0F, -1.0F, -3.0F));
+
+        PartDefinition ear_right_r1 = ear_right.addOrReplaceChild("ear_right_r1", CubeListBuilder.create().texOffs(21, 6).addBox(-4.0F, -3.0F, 0.0F, 4.0F, 3.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.1325F, 0.6429F, 0.2187F));
+
+        PartDefinition leg_front_right = body.addOrReplaceChild("leg_front_right", CubeListBuilder.create().texOffs(0, 87).addBox(-1.5F, -1.0F, -1.5F, 3.0F, 9.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 9.0F, 2.0F));
+
+        PartDefinition leg_front_left = body.addOrReplaceChild("leg_front_left", CubeListBuilder.create().texOffs(12, 87).addBox(-1.5F, -1.0F, -1.5F, 3.0F, 9.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(3.0F, 9.0F, 2.0F));
+
+        PartDefinition leg_back_left = body.addOrReplaceChild("leg_back_left", CubeListBuilder.create().texOffs(12, 99).addBox(-1.5F, -1.0F, -1.5F, 3.0F, 9.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(3.0F, 9.0F, 15.0F));
+
+        PartDefinition leg_back_right = body.addOrReplaceChild("leg_back_right", CubeListBuilder.create().texOffs(0, 99).addBox(-1.5F, -1.0F, -1.5F, 3.0F, 9.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 9.0F, 15.0F));
+
+        return LayerDefinition.create(meshdefinition, 64, 128);
+    }
+
+    @Override
+    public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+        this.root().getAllParts().forEach(ModelPart::resetPose);
+        this.head.xRot = headPitch * Mth.DEG_TO_RAD;
+        this.head.yRot = netHeadYaw * Mth.DEG_TO_RAD;
+        this.leg_back_right.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.leg_back_left.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
+        this.leg_front_right.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
+        this.leg_front_left.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+
+
+        if (!Minecraft.getInstance().isPaused()) {
+            float aimingForFold;
+            if (entity.isEntityOnGround()) {
+                aimingForFold = 0.0F;
+            } else {
+                aimingForFold = 1.0F;
+            }
+            entity.setWingFold(entity.getWingFold() + ((aimingForFold - entity.getWingFold()) / 37.5F));
+        }
+        this.animateWalk(WingAnimation.wing_open, 0.0F, entity.getWingFold(), 1.0F, 1.0F);
+
+        if (entity.isBaby()) {
+            this.applyStatic(FlyingCowAnimation.BABY);
+        }
+    }
+
+    @Override
+    public ModelPart root() {
+        return this.root;
+    }
+}
---
+++ src/main/java/com/aetherteam/aetherii/client/renderer/model/animation/FlyingCowAnimation.java
@@ -0,0 +1,20 @@
+package com.aetherteam.aetherii.client.renderer.model.animation;// Save this class in your mod and generate all required imports
+
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+
+public class FlyingCowAnimation {
+    public static final AnimationDefinition BABY = AnimationDefinition.Builder.withLength(0.0F)
+            .addAnimation("body", new AnimationChannel(AnimationChannel.Targets.POSITION,
+                    new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, -7.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+            ))
+            .addAnimation("body", new AnimationChannel(AnimationChannel.Targets.SCALE,
+                    new Keyframe(0.0F, KeyframeAnimations.scaleVec(0.6F, 0.6F, 0.6F), AnimationChannel.Interpolations.LINEAR)
+            ))
+            .addAnimation("head", new AnimationChannel(AnimationChannel.Targets.SCALE,
+                    new Keyframe(0.0F, KeyframeAnimations.scaleVec(1.5F, 1.5F, 1.5F), AnimationChannel.Interpolations.LINEAR)
+            ))
+            .build();
+}
