e75edfa59506a0893c8cc98b126515a5aaeb34f1
Builderdog841 <timon.kempter@gmail.com>
2024-06-05 10:03:32 +0200

    Fixed Spawn Egg Tab ordering

--- src/main/java/com/aetherteam/aetherii/item/AetherIICreativeTabs.java
+++ src/main/java/com/aetherteam/aetherii/item/AetherIICreativeTabs.java
@@ -373,9 +373,9 @@ public class AetherIICreativeTabs {
             .displayItems((features, output) -> {
                 output.accept(AetherIIItems.AERBUNNY_SPAWN_EGG.get());
                 output.accept(AetherIIItems.FLYING_COW_SPAWN_EGG.get());
-                output.accept(AetherIIItems.SHEEPUFF_SPAWN_EGG.get());
-                output.accept(AetherIIItems.PHYG_SPAWN_EGG.get());
                 output.accept(AetherIIItems.KIRRID_SPAWN_EGG.get());
+                output.accept(AetherIIItems.PHYG_SPAWN_EGG.get());
+                output.accept(AetherIIItems.SHEEPUFF_SPAWN_EGG.get());
                 output.accept(AetherIIItems.ZEPHYR_SPAWN_EGG.get());
             }).build());
 }
