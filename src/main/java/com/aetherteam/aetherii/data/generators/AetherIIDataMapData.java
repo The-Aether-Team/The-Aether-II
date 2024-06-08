40abee91b8e111d73a2ef1529bef296695aaf68d
bconlon <bconlon.gml@gmail.com>
2024-06-07 18:44:21 -0700

    feat: compostables

--- src/main/java/com/aetherteam/aetherii/data/generators/AetherIIDataMapData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/AetherIIDataMapData.java
@@ -1,5 +1,7 @@
 package com.aetherteam.aetherii.data.generators;
 
+import com.aetherteam.aetherii.block.AetherIIBlocks;
+import com.aetherteam.aetherii.item.AetherIIItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Item;
@@ -17,22 +19,48 @@ public class AetherIIDataMapData extends DataMapProvider {
 
     @Override
     protected void gather() {
-        var compostables = this.builder(NeoForgeDataMaps.COMPOSTABLES); //todo
-//        this.addCompost(compostables, AetherBlocks.SKYROOT_LEAVES, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.SKYROOT_SAPLING, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.GOLDEN_OAK_LEAVES, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.GOLDEN_OAK_SAPLING, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.CRYSTAL_LEAVES, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.CRYSTAL_FRUIT_LEAVES, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.HOLIDAY_LEAVES, 0.3F);
-//        this.addCompost(compostables, AetherBlocks.DECORATED_HOLIDAY_LEAVES, 0.3F);
-//        this.addCompost(compostables, AetherItems.BLUE_BERRY, 0.3F);
-//        this.addCompost(compostables, AetherItems.ENCHANTED_BERRY, 0.5F);
-//        this.addCompost(compostables, AetherBlocks.BERRY_BUSH, 0.5F);
-//        this.addCompost(compostables, AetherBlocks.BERRY_BUSH_STEM, 0.5F);
-//        this.addCompost(compostables, AetherBlocks.WHITE_FLOWER, 0.65F);
-//        this.addCompost(compostables, AetherBlocks.PURPLE_FLOWER, 0.65F);
-//        this.addCompost(compostables, AetherItems.WHITE_APPLE, 0.65F);
+        var compostables = this.builder(NeoForgeDataMaps.COMPOSTABLES);
+        this.addCompost(compostables, AetherIIBlocks.SKYROOT_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.SKYPLANE_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.SKYBIRCH_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.SKYPINE_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.WISPROOT_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.WISPTOP_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.GREATROOT_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.GREATOAK_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.GREATBOA_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.AMBEROOT_LEAF_PILE, 0.018F);
+        this.addCompost(compostables, AetherIIBlocks.SKYROOT_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.SKYPLANE_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.SKYBIRCH_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.SKYPINE_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.WISPROOT_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.WISPTOP_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.GREATROOT_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.GREATOAK_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.GREATBOA_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.AMBEROOT_LEAVES, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.SKYROOT_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.WISPROOT_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.WISPTOP_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.GREATROOT_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.GREATOAK_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.GREATBOA_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.AMBEROOT_SAPLING, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.AETHER_SHORT_GRASS, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.AETHER_MEDIUM_GRASS, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.AETHER_LONG_GRASS, 0.3F);
+        this.addCompost(compostables, AetherIIBlocks.HESPEROSE, 0.65F);
+        this.addCompost(compostables, AetherIIBlocks.TARABLOOM, 0.65F);
+        this.addCompost(compostables, AetherIIBlocks.HIGHLANDS_BUSH, 0.5F);
+        this.addCompost(compostables, AetherIIBlocks.BLUEBERRY_BUSH, 0.5F);
+        this.addCompost(compostables, AetherIIBlocks.BLUEBERRY_BUSH_STEM, 0.5F);
+        this.addCompost(compostables, AetherIIBlocks.ORANGE_TREE, 0.5F);
+        this.addCompost(compostables, AetherIIItems.BLUEBERRY, 0.3F);
+        this.addCompost(compostables, AetherIIItems.ENCHANTED_BLUEBERRY, 0.5F);
+        this.addCompost(compostables, AetherIIItems.WYNDBERRY, 0.3F);
+        this.addCompost(compostables, AetherIIItems.ENCHANTED_WYNDBERRY, 0.5F);
+        this.addCompost(compostables, AetherIIItems.ORANGE, 0.3F);
     }
 
     private void addCompost(DataMapProvider.Builder<Compostable, Item> map, ItemLike item, float chance) {
