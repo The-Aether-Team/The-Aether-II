5858560c439baeae2d60e6ada1e90fb2716a24f3
Builderdog841 <timon.kempter@gmail.com>
2024-06-11 21:57:43 +0200

    Slightly tweaked Shrouded Forest Gen

--- src/main/java/com/aetherteam/aetherii/data/resources/builders/AetherIIDensityFunctionBuilders.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/builders/AetherIIDensityFunctionBuilders.java
@@ -63,7 +63,7 @@ public class AetherIIDensityFunctionBuilders {
     public static DensityFunction makeVegetationRarityMapper(HolderGetter<DensityFunction> function) {
         DensityFunction vegetation = getFunction(function, VEGETATION);
         DensityFunction density = vegetation;
-        density = DensityFunctions.rangeChoice(getFunction(function, VEGETATION_RARE), 0, 0.4, density, DensityFunctions.constant(2.0));
+        density = DensityFunctions.rangeChoice(getFunction(function, VEGETATION_RARE), 0, 0.35, density, DensityFunctions.constant(2.0));
         density = DensityFunctions.rangeChoice(getFunction(function, TEMPERATURE), -0.4, 0.3, density, vegetation);
         density = DensityFunctions.rangeChoice(getFunction(function, EROSION), 0.0, 0.55, density, vegetation);
         return density;
