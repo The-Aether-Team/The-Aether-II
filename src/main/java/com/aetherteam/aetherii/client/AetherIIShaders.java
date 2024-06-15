12ff302274ee607151d14581b70b313d6681c4a0
bconlon <67203206+bconlon1@users.noreply.github.com>
2024-06-15 14:22:36 -0700

    feat: visual improvements (#309)
    - waterfall gradient
    - stormy weather

--- src/main/java/com/aetherteam/aetherii/client/AetherIIShaders.java
+++ src/main/java/com/aetherteam/aetherii/client/AetherIIShaders.java
@@ -10,18 +10,18 @@ import net.neoforged.neoforge.client.event.RegisterShadersEvent;
 import java.io.IOException;
 
 public class AetherIIShaders {
-    private static ShaderInstance positionColorCloudCoverShader;
+    private static ShaderInstance cloudCoverShader;
 
     public static void registerShaders(RegisterShadersEvent event) {
         ResourceProvider resourceProvider = event.getResourceProvider();
         try {
-            event.registerShader(new ShaderInstance(resourceProvider, new ResourceLocation(AetherII.MODID, "position_color_cloud_cover"), DefaultVertexFormat.POSITION_COLOR), instance -> positionColorCloudCoverShader = instance);
+            event.registerShader(new ShaderInstance(resourceProvider, new ResourceLocation(AetherII.MODID, "cloud_cover"), DefaultVertexFormat.POSITION_COLOR), instance -> cloudCoverShader = instance);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static ShaderInstance getPositionColorCloudCoverShader() {
-        return positionColorCloudCoverShader;
+    public static ShaderInstance getCloudCoverShader() {
+        return cloudCoverShader;
     }
 }
