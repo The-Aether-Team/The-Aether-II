6001aeb45e5be324ad6b2762a8be2085f929708b
bconlon <bconlon.gml@gmail.com>
2024-07-06 13:44:12 -0700

    fix: sleeping

--- src/main/java/com/aetherteam/aetherii/event/listeners/BlockInteractionListener.java
+++ src/main/java/com/aetherteam/aetherii/event/listeners/BlockInteractionListener.java
@@ -6,12 +6,15 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.level.SleepFinishedTimeEvent;
 
 public class BlockInteractionListener {
     public static void listen(IEventBus bus) {
         bus.addListener(BlockInteractionListener::rightClickBlock);
+        bus.addListener(BlockInteractionListener::finishSleeping);
     }
 
     public static void rightClickBlock(PlayerInteractEvent.RightClickBlock event) {
@@ -23,5 +26,10 @@ public class BlockInteractionListener {
         if (BlockInteractionHooks.snowlog(player, level, pos, itemStack, hand)) {
             event.setCanceled(true);
         }
+    }
+
+    public static void finishSleeping(SleepFinishedTimeEvent event) {
+        LevelAccessor level = event.getLevel();
+        BlockInteractionHooks.finishSleep(level, event.getNewTime());
     }
 }
