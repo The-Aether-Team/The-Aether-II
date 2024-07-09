510e39730b3032c511571c9726f1e7c2ff648fc3
Builderdog841 <timon.kempter@gmail.com>
2024-07-09 12:18:55 +0200

    WIP Brettl Grow Code

--- src/main/java/com/aetherteam/aetherii/block/natural/BrettlPlantBlock.java
+++ src/main/java/com/aetherteam/aetherii/block/natural/BrettlPlantBlock.java
@@ -4,6 +4,8 @@ import com.aetherteam.aetherii.block.AetherIIBlockStateProperties;
 import com.aetherteam.aetherii.block.AetherIIBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
@@ -11,7 +13,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.neoforged.neoforge.common.CommonHooks;
 import net.neoforged.neoforge.common.util.TriState;
+import org.jetbrains.annotations.NotNull;
 
 public class BrettlPlantBlock extends Block {
     public static final EnumProperty<BrettlPlantBlock.BrettlType> TYPE = EnumProperty.create("brettl_type", BrettlPlantBlock.BrettlType.class);
@@ -29,6 +34,29 @@ public class BrettlPlantBlock extends Block {
         TriState soilDecision = stateBelow.canSustainPlant(level, pos.below(), Direction.UP, stateBelow);
         if (!soilDecision.isDefault()) return soilDecision.isTrue();
         return stateBelow.is(AetherIIBlocks.QUICKSOIL) && (stateBelow.getValue(TYPE) == BrettlType.BASE) && (stateBelow.getValue(TYPE) == BrettlType.MIDDLE);
+    }
+
+    public void randomTick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+        if (state.getValue(TYPE) == BrettlType.TIP) {
+            if (level.getRawBrightness(pos.above(), 0) >= 9 && CommonHooks.canCropGrow(level, pos, state, random.nextInt(5) == 0)) {
+                BlockState brettlMiddle = state.setValue(TYPE, BrettlType.MIDDLE);
+                BlockState brettlTip = state.setValue(TYPE, BrettlType.TIP);
+                BlockState brettlFlower = AetherIIBlocks.BRETTL_FLOWER.get().defaultBlockState();
+
+                if (level.getBlockState(pos.below()).getValue(TYPE) == BrettlType.BASE) {
+                    if (level.getBlockState(pos.above()).isAir()) {
+                        level.setBlock(pos, brettlMiddle, 2);
+                        level.setBlock(pos.above(), brettlTip, 2);
+                        level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(brettlMiddle));
+                        CommonHooks.fireCropGrowPost(level, pos, state);
+                    }
+                } else {
+                    level.setBlock(pos, brettlFlower, 2);
+                    level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(brettlFlower));
+                    CommonHooks.fireCropGrowPost(level, pos, state);
+                }
+            }
+        }
     }
 
     @Override
