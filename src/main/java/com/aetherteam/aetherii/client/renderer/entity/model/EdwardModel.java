358bf6b003c16f8c0ca874c031f1cf03ca8fa57c
bconlon <bconlon.gml@gmail.com>
2024-07-11 20:58:34 -0700

    improv: basic sit handling; slightly buggy

--- src/main/java/com/aetherteam/aetherii/client/renderer/entity/model/EdwardModel.java
+++ src/main/java/com/aetherteam/aetherii/client/renderer/entity/model/EdwardModel.java
@@ -34,15 +34,13 @@ public class EdwardModel extends HierarchicalModel<Edward> implements ArmedModel
 	private final ModelPart legright1;
 	private final ModelPart legright2;
 	private final ModelPart book;
-	public HumanoidModel.ArmPose leftArmPose = HumanoidModel.ArmPose.EMPTY;
-	public HumanoidModel.ArmPose rightArmPose = HumanoidModel.ArmPose.EMPTY;
 
 	public EdwardModel(ModelPart root) {
 		this.root = root.getChild("root");
 		this.waist = this.root.getChild("waist");
 		this.abdomen = this.waist.getChild("abdomen");
 		this.chest = this.abdomen.getChild("chest");
-		this.head = this.chest.getChild("head");
+		this.head = this.root.getChild("head");
 		this.earleft = this.head.getChild("earleft");
 		this.earright = this.head.getChild("earright");
 		this.hatlayer = this.head.getChild("hatlayer");
@@ -73,7 +71,7 @@ public class EdwardModel extends HierarchicalModel<Edward> implements ArmedModel
 
 		PartDefinition chest = abdomen.addOrReplaceChild("chest", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-4.5F, -7.0F, -2.5F, 9.0F, 7.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, -4.0F, 0.0F));
 
-		PartDefinition head = chest.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -7.0F, 0.0F));
+		PartDefinition head = root.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -7.0F, 0.0F));
 
 		PartDefinition earleft = head.addOrReplaceChild("earleft", CubeListBuilder.create().texOffs(24, 2).mirror().addBox(0.0F, -2.0F, 0.0F, 1.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-4.0F, -3.0F, -2.0F, 0.0F, -0.2618F, -0.0873F));
 
@@ -124,55 +122,88 @@ public class EdwardModel extends HierarchicalModel<Edward> implements ArmedModel
 	public void setupAnim(Edward entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.root().getAllParts().forEach(ModelPart::resetPose);
 
-		this.head.yRot = netHeadYaw * (float) (Math.PI / 180.0);
 		this.head.xRot = headPitch * (float) (Math.PI / 180.0);
+		this.head.yRot = netHeadYaw * (float) (Math.PI / 180.0);
 
-		this.waist.yRot = 0.0F;
-//		this.armright1.z = 0.0F;
-//		this.armright1.x = -5.0F;
-//		this.armleft1.z = 0.0F;
-//		this.armleft1.x = 5.0F;
-		
-		float f = 1.0F;
-		
-		this.armright1.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 2.0F * limbSwingAmount * 0.5F / f;
-		this.armleft1.xRot = Mth.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F / f;
-		this.armright1.zRot = 0.0F;
-		this.armleft1.zRot = 0.0F;
-		this.legright1.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount / f;
-		this.legleft1.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount / f;
-		this.legright1.yRot = 0.005F;
-		this.legleft1.yRot = -0.005F;
-		this.legright1.zRot = 0.005F;
-		this.legleft1.zRot = -0.005F;
-		if (this.riding) {
-			this.armright1.xRot += (float) (-Math.PI / 5);
-			this.armleft1.xRot += (float) (-Math.PI / 5);
-			this.legright1.xRot = -1.4137167F;
-			this.legright1.yRot = (float) (Math.PI / 10);
-			this.legright1.zRot = 0.07853982F;
-			this.legleft1.xRot = -1.4137167F;
-			this.legleft1.yRot = (float) (-Math.PI / 10);
-			this.legleft1.zRot = -0.07853982F;
+		if (entity.isSitting()) {
+			this.head.y = 5.25F;
+			this.head.z = -1.0F;
+
+			this.waist.y = 5.0F;
+			this.waist.z = 2.0F;
+
+			this.abdomen.xRot = 0.0873F;
+
+			this.chest.xRot = 0.2618F;
+
+			this.armleft1.yRot = -0.5009F;
+			this.armleft1.zRot = 0.2731F;
+
+			this.armleft2.xRot = -1.309F;
+
+			this.armright1.xRot = 0.3643F;
+			this.armright1.yRot = -0.3187F;
+			this.armright1.zRot = -0.0456F;
+
+			this.armright2.xRot = -0.8196F;
+
+			this.legleft1.xRot = -1.2217F;
+			this.legleft1.yRot = 0.3142F;
+
+			this.legleft2.xRot = 0.3491F;
+
+			this.legright1.xRot = -1.1839F;
+			this.legright2.yRot = -0.2731F;
+
+			this.legright2.xRot = 0.1745F;
+
+			this.backpack.x = -4.0F;
+			this.backpack.y = 12.0F;
+			this.backpack.z = -2.0F;
+			this.backpack.xRot = 1.5708F;
+			this.backpack.yRot = 1.5708F;
+			this.backpack.zRot = 0.0F;
+		} else {
+			this.head.y = 0.0F;
+
+			this.waist.yRot = 0.0F;
+
+			float f = 1.0F;
+
+			this.armright1.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 2.0F * limbSwingAmount * 0.5F / f;
+			this.armleft1.xRot = Mth.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F / f;
+			this.armright1.zRot = 0.0F;
+			this.armleft1.zRot = 0.0F;
+			this.legright1.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount / f;
+			this.legleft1.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount / f;
+			this.legright1.yRot = 0.005F;
+			this.legleft1.yRot = -0.005F;
+			this.legright1.zRot = 0.005F;
+			this.legleft1.zRot = -0.005F;
+			if (this.riding) {
+				this.armright1.xRot += (float) (-Math.PI / 5);
+				this.armleft1.xRot += (float) (-Math.PI / 5);
+				this.legright1.xRot = -1.4137167F;
+				this.legright1.yRot = (float) (Math.PI / 10);
+				this.legright1.zRot = 0.07853982F;
+				this.legleft1.xRot = -1.4137167F;
+				this.legleft1.yRot = (float) (-Math.PI / 10);
+				this.legleft1.zRot = -0.07853982F;
+			}
+
+			this.armright1.yRot = 0.0F;
+			this.armleft1.yRot = 0.0F;
+
+			this.waist.xRot = 0.0F;
+
+			AnimationUtils.bobModelPart(this.armright1, ageInTicks, -1.0F);
+			AnimationUtils.bobModelPart(this.armleft1, ageInTicks, 1.0F);
 		}
+	}
 
-		this.armright1.yRot = 0.0F;
-		this.armleft1.yRot = 0.0F;
-
-		this.waist.xRot = 0.0F;
-//		this.legright1.z = 0.0F;
-//		this.legleft1.z = 0.0F;
-//		this.legright1.y = 12.0F;
-//		this.legleft1.y = 12.0F;
-//		this.head.y = 0.0F;
-//		this.waist.y = 4.0F;
-//		this.armleft1.y = 2.0F;
-//		this.armright1.y = 2.0F;
-
-		AnimationUtils.bobModelPart(this.armright1, ageInTicks, -1.0F);
-		AnimationUtils.bobModelPart(this.armleft1, ageInTicks, 1.0F);
-
-//		this.hatlayer.copyFrom(this.head);
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
+		super.renderToBuffer(poseStack, buffer, packedLight, packedOverlay, color);
 	}
 
 	@Override
