732122148419556c298508e5cf461bd272ebb650
bconlon <bconlon.gml@gmail.com>
2024-07-13 16:32:42 -0700

    docs: remove todos

--- src/main/java/com/aetherteam/aetherii/client/gui/screen/inventory/ArkeniumForgeScreen.java
+++ src/main/java/com/aetherteam/aetherii/client/gui/screen/inventory/ArkeniumForgeScreen.java
@@ -14,11 +14,9 @@ import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ServerboundRenameItemPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -31,11 +29,8 @@ import net.neoforged.neoforge.network.PacketDistributor;
 import java.util.List;
 
 //todo
-//  get naming to work; probably shouldnt take cost.
-//  forging button will have to consume materials and update equipment stack; may need packets to the server for this but not sure.
-//  need to render selection outline for tier icons dependent on input material count. also i forget if theyre meant to act as buttons as well.
+//  i forget tier icons are meant to act as buttons as well.
 //  need to render the charm displays and "slots" around the item when inputted.
-//  forging button tooltip
 
 public class ArkeniumForgeScreen extends AbstractContainerScreen<ArkeniumForgeMenu> implements ContainerListener {
     private static final ResourceLocation TEXT_FIELD_SPRITE = ResourceLocation.withDefaultNamespace("container/anvil/text_field");
@@ -106,7 +101,7 @@ public class ArkeniumForgeScreen extends AbstractContainerScreen<ArkeniumForgeMe
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        if ((!this.menu.getInput().isEmpty() && !this.name.getValue().equals(this.menu.getInput().getHoverName().getString())) || this.menu.getTierForMaterials() > 0) { //todo more checks
+        if ((!this.menu.getInput().isEmpty() && !this.name.getValue().equals(this.menu.getInput().getHoverName().getString())) || this.menu.getTierForMaterials() > 0) {
             if (!this.forgeButton.active) {
                 this.forgeButton.active = true;
             }
