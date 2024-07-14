e017b57a5abdfc561e9941ba85d4065a1ea597c7
bconlon <bconlon.gml@gmail.com>
2024-07-13 17:46:16 -0700

    refactor: arkenium forge hitbox

--- src/main/java/com/aetherteam/aetherii/block/utility/ArkeniumForgeBlock.java
+++ src/main/java/com/aetherteam/aetherii/block/utility/ArkeniumForgeBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,10 +19,30 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ArkeniumForgeBlock extends Block {
     private static final Component CONTAINER_TITLE = Component.translatable("menu.aether_ii.arkenium_forge");
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    protected static final VoxelShape CORNER_1 = Block.box(0.0, 0.0, 0.0, 3.0, 6.0, 3.0);
+    protected static final VoxelShape CORNER_2 = Block.box(13.0, 0.0, 13.0, 16.0, 6.0, 16.0);
+    protected static final VoxelShape CORNER_3 = Block.box(13.0, 0.0, 0.0, 16.0, 6.0, 3.0);
+    protected static final VoxelShape CORNER_4 = Block.box(0.0, 0.0, 13.0, 3.0, 6.0, 16.0);
+    protected static final VoxelShape BASE_1 = Block.box(1.0, 0.0, 1.0, 15.0, 4.0, 15.0);
+    protected static final VoxelShape RIM_1 = Block.box(2.0, 4.0, 2.0, 4.0, 8.0, 14.0);
+    protected static final VoxelShape RIM_2 = Block.box(4.0, 4.0, 2.0, 12.0, 8.0, 4.0);
+    protected static final VoxelShape RIM_3 = Block.box(12.0, 4.0, 2.0, 14.0, 8.0, 14.0);
+    protected static final VoxelShape RIM_4 = Block.box(4.0, 4.0, 12.0, 12.0, 8.0, 14.0);
+    protected static final VoxelShape CENTER_Z = Block.box(5.5, 4.0, 6.0, 10.5, 11.0, 10.0);
+    protected static final VoxelShape TOP_1_Z = Block.box(5.5, 11.0, 3.0, 10.5, 16.0, 13.0);
+    protected static final VoxelShape TOP_2_Z = Block.box(1.5, 11.0, 5.0, 14.5, 16.0, 11.0);
+    protected static final VoxelShape CENTER_X = Block.box(6.0, 4.0, 5.5, 10.0, 11.0, 10.5);
+    protected static final VoxelShape TOP_1_X = Block.box(3.0, 11.0, 5.5, 13.0, 16.0, 10.5);
+    protected static final VoxelShape TOP_2_X = Block.box(5.0, 11.0, 1.5, 11.0, 16.0, 14.5);
+    protected static final VoxelShape SHAPE_Z = Shapes.or(BASE_1, RIM_1, RIM_2, RIM_3, RIM_4, CENTER_Z, CORNER_1, CORNER_2, CORNER_3, CORNER_4, TOP_1_Z, TOP_2_Z);
+    protected static final VoxelShape SHAPE_X = Shapes.or(BASE_1, RIM_1, RIM_2, RIM_3, RIM_4, CENTER_X, CORNER_1, CORNER_2, CORNER_3, CORNER_4, TOP_1_X, TOP_2_X);
 
     public ArkeniumForgeBlock(Properties properties) {
         super(properties);
@@ -47,6 +68,15 @@ public class ArkeniumForgeBlock extends Block {
     @Override
     protected MenuProvider getMenuProvider(BlockState state, Level level, BlockPos pos) {
         return new SimpleMenuProvider((id, inventory, player) -> new ArkeniumForgeMenu(id, inventory, ContainerLevelAccess.create(level, pos)), CONTAINER_TITLE);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        if (state.getValue(FACING).getAxis() == Direction.Axis.Z) {
+            return SHAPE_Z;
+        } else {
+            return SHAPE_X;
+        }
     }
 
     @Override
