430912815467a1f51e64ffff9605aa32bdacc2d6
Builderdog841 <timon.kempter@gmail.com>
2024-07-16 15:44:54 +0200

    Another Attempt

--- src/main/java/com/aetherteam/aetherii/client/AetherIIMusicManager.java
+++ src/main/java/com/aetherteam/aetherii/client/AetherIIMusicManager.java
@@ -4,19 +4,12 @@ import com.aetherteam.aetherii.AetherIITags;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.WinScreen;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.resources.sounds.SimpleSoundInstance;
-import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.MusicManager;
-import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.core.Holder;
 import net.minecraft.sounds.Music;
 import net.minecraft.sounds.Musics;
 import net.minecraft.tags.BiomeTags;
-import net.minecraft.util.Mth;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.biome.Biome;
-
-import javax.annotation.Nullable;
 
 public class AetherIIMusicManager {
     public static final Music AETHER_NIGHT = Musics.createGameMusic(AetherIISoundEvents.MUSIC_AETHER_NIGHT);
@@ -24,65 +17,9 @@ public class AetherIIMusicManager {
     public static final Music AETHER_SUNSET = Musics.createGameMusic(AetherIISoundEvents.MUSIC_AETHER_SUNSET);
     public static final Music AETHER_CAVES = Musics.createGameMusic(AetherIISoundEvents.MUSIC_AETHER_AMBIENCE);
 
-    private static final RandomSource random = RandomSource.create();
     private static final Minecraft minecraft = Minecraft.getInstance();
     private static final MusicManager musicManager = Minecraft.getInstance().getMusicManager();
-    @Nullable
-    private static SoundInstance currentMusic;
-    private static int nextSongDelay = 100;
 
-    public static void tick() {
-        Music music = getSituationalMusic();
-        if (music != null) {
-            if (currentMusic != null) {
-                if (!music.getEvent().value().getLocation().equals(currentMusic.getLocation()) && music.replaceCurrentMusic()) {
-                    minecraft.getSoundManager().stop(currentMusic);
-                    nextSongDelay = Mth.nextInt(random, 0, music.getMinDelay() / 2);
-                }
-
-                if (!minecraft.getSoundManager().isActive(currentMusic)) {
-                    currentMusic = null;
-                    nextSongDelay = Math.min(nextSongDelay, Mth.nextInt(random, music.getMinDelay(), music.getMaxDelay()));
-                }
-            }
-
-            nextSongDelay = Math.min(nextSongDelay, music.getMaxDelay());
-            if (currentMusic == null && nextSongDelay-- <= 0) {
-                startPlaying(music);
-            }
-        } else {
-            currentMusic = null;
-            if (nextSongDelay-- <= 0) {
-                nextSongDelay = Math.min(Integer.MAX_VALUE, Mth.nextInt(random, 3000, 6000));
-            }
-        }
-    }
-
-    public static void startPlaying(Music music) {
-        musicManager.stopPlaying();
-        currentMusic = SimpleSoundInstance.forMusic(music.getEvent().value());
-        if (currentMusic.getSound() != SoundManager.EMPTY_SOUND) {
-            minecraft.getSoundManager().play(currentMusic);
-        }
-        nextSongDelay = Integer.MAX_VALUE;
-    }
-
-    public static void stopPlaying() {
-        if (currentMusic != null) {
-            minecraft.getSoundManager().stop(currentMusic);
-            currentMusic = null;
-        }
-        nextSongDelay += 100;
-    }
-
-    @Nullable
-    public static SoundInstance getCurrentMusic() {
-        return currentMusic;
-    }
-
-    /**
-     * @return Regular {@link Music}, or nighttime {@link Music}, when it is nighttime.
-     */
     public static Music getSituationalMusic() {
         if (!(minecraft.screen instanceof WinScreen)) {
             if (minecraft.player != null) {
@@ -111,7 +48,8 @@ public class AetherIIMusicManager {
         return null;
     }
 
+    @SuppressWarnings("deprecation")
     public static boolean isCreative(Holder<Biome> holder, LocalPlayer player) {
-        return holder.is(AetherIITags.Biomes.AETHER_MUSIC) && !musicManager.isPlayingMusic(Musics.UNDER_WATER) && (!player.isUnderWater() || !holder.is(BiomeTags.PLAYS_UNDERWATER_MUSIC)) && player.getAbilities().instabuild && player.getAbilities().mayfly;
+        return !holder.is(AetherIITags.Biomes.AETHER_MUSIC) && !musicManager.isPlayingMusic(Musics.UNDER_WATER) && (!player.isUnderWater() || !holder.is(BiomeTags.PLAYS_UNDERWATER_MUSIC)) && player.getAbilities().instabuild && player.getAbilities().mayfly;
     }
 }
--- src/main/java/com/aetherteam/aetherii/client/event/listeners/AudioListener.java
+++ src/main/java/com/aetherteam/aetherii/client/event/listeners/AudioListener.java
@@ -1,32 +1,16 @@
 package com.aetherteam.aetherii.client.event.listeners;
 
-import com.aetherteam.aetherii.client.event.hooks.AudioHooks;
-import net.minecraft.client.resources.sounds.SoundInstance;
+import com.aetherteam.aetherii.client.AetherIIMusicManager;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
-import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.SelectMusicEvent;
 
 public class AudioListener {
 
     public static void listen(IEventBus bus) {
         bus.addListener(AudioListener::onMusicPlayed);
-        bus.addListener(AudioListener::onClientTick);
-        bus.addListener(AudioListener::onPlayerRespawn);
     }
 
     public static void onMusicPlayed(SelectMusicEvent event) {
-        SoundInstance sound = event.getPlayingMusic();
-        if (sound != null) {
-            AudioHooks.playAetherMusic(sound);
-        }
-    }
-
-    public static void onClientTick(ClientTickEvent.Post event) {
-        AudioHooks.tick();
-    }
-
-    public static void onPlayerRespawn(ClientPlayerNetworkEvent.Clone event) {
-        AudioHooks.stop();
+        event.setMusic(AetherIIMusicManager.getSituationalMusic());
     }
 }
