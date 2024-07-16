99f72612dd030fcc79fdc053594dc1f4cbaa74ca
Builderdog841 <timon.kempter@gmail.com>
2024-07-16 10:26:37 +0200

    More Event Progress

--- src/main/java/com/aetherteam/aetherii/client/event/hooks/AudioHooks.java
+++ src/main/java/com/aetherteam/aetherii/client/event/hooks/AudioHooks.java
@@ -1,4 +1,10 @@
 package com.aetherteam.aetherii.client.event.hooks;
 
+import net.neoforged.neoforge.client.event.SelectMusicEvent;
+
 public class AudioHooks {
+
+    public static void playAetherMusic(SelectMusicEvent event) {
+
+    }
 }
--- src/main/java/com/aetherteam/aetherii/client/event/listeners/AudioListener.java
+++ src/main/java/com/aetherteam/aetherii/client/event/listeners/AudioListener.java
@@ -1,16 +1,16 @@
 package com.aetherteam.aetherii.client.event.listeners;
 
-import com.aetherteam.aetherii.client.event.listeners.attachment.DimensionRenderEffectListeners;
+import com.aetherteam.aetherii.client.event.hooks.AudioHooks;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.client.event.SelectMusicEvent;
 
 public class AudioListener {
 
     public static void listen(IEventBus bus) {
-        bus.addListener(AudioListener::playMusic);
+        bus.addListener(AudioListener::onMusicPlayed);
     }
 
-    public static void playMusic(SelectMusicEvent event) {
-
+    public static void onMusicPlayed(SelectMusicEvent event) {
+        AudioHooks.playAetherMusic(event);
     }
 }
