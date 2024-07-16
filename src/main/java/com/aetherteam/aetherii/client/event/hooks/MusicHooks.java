221c7296d3daad5f16429ef56219cb112a9863d0
Builderdog841 <timon.kempter@gmail.com>
2024-07-16 20:14:24 +0200

    Fixed Music

--- src/main/java/com/aetherteam/aetherii/client/event/hooks/MusicHooks.java
+++ src/main/java/com/aetherteam/aetherii/client/event/hooks/MusicHooks.java
@@ -18,42 +18,31 @@ public class MusicHooks {
     public static final Music AETHER_SUNSET = Musics.createGameMusic(AetherIISoundEvents.MUSIC_AETHER_SUNSET);
     public static final Music AETHER_CAVES = Musics.createGameMusic(AetherIISoundEvents.MUSIC_AETHER_AMBIENCE);
 
-    private static final Minecraft minecraft = Minecraft.getInstance();
-    private static final MusicManager musicManager = Minecraft.getInstance().getMusicManager();
+    public static Music getSituationalMusic(Holder<Biome> biome) {
+        if (!(Minecraft.getInstance().screen instanceof WinScreen)) {
+            if (Minecraft.getInstance().player != null) {
 
-    public static Music getSituationalMusic() {
-        if (!(minecraft.screen instanceof WinScreen)) {
-            if (minecraft.player != null) {
-                Holder<Biome> biome = minecraft.player.level().getBiome(minecraft.player.blockPosition());
-                if (biome.is(AetherIITags.Biomes.AETHER_MUSIC)) {
-                    long time = minecraft.player.clientLevel.getLevelData().getDayTime() % 24000L;
-                    boolean day = time >= 0 && time < 12000;
-                    boolean night = time >= 14000 && time < 22000;
-                    boolean sunset = time >= 12000 && time < 14000;
-                    boolean sunrise = time >= 22000;
+                long time = Minecraft.getInstance().player.clientLevel.getLevelData().getDayTime() % 24000L;
+                boolean day = time >= 0 && time < 12000;
+                boolean night = time >= 14000 && time < 22000;
+                boolean sunset = time >= 12000 && time < 14000;
+                boolean sunrise = time >= 22000;
 
-                    if (minecraft.player.position().y <= 80) {
-                        return AETHER_CAVES;
-                    } else {
-                        if (day) {
-                            return biome.value().getBackgroundMusic().get();
-                        } else if (night) {
-                            return AETHER_NIGHT;
-                        } else if (sunset) {
-                            return AETHER_SUNSET;
-                        } else if (sunrise) {
-                            return AETHER_SUNRISE;
-                        }
+                if (Minecraft.getInstance().player.position().y <= 80) {
+                    return AETHER_CAVES;
+                } else {
+                    if (day) {
+                        return biome.value().getBackgroundMusic().get();
+                    } else if (night) {
+                        return AETHER_NIGHT;
+                    } else if (sunset) {
+                        return AETHER_SUNSET;
+                    } else if (sunrise) {
+                        return AETHER_SUNRISE;
                     }
                 }
-                return biome.value().getBackgroundMusic().isPresent() ? biome.value().getBackgroundMusic().get() : null;
             }
         }
         return null;
-    }
-
-    @SuppressWarnings("deprecation")
-    public static boolean isCreative(Holder<Biome> holder, LocalPlayer player) {
-        return !holder.is(AetherIITags.Biomes.AETHER_MUSIC) && !musicManager.isPlayingMusic(Musics.UNDER_WATER) && (!player.isUnderWater() || !holder.is(BiomeTags.PLAYS_UNDERWATER_MUSIC)) && player.getAbilities().instabuild && player.getAbilities().mayfly;
     }
 }
--- src/main/java/com/aetherteam/aetherii/client/event/listeners/MusicListener.java
+++ src/main/java/com/aetherteam/aetherii/client/event/listeners/MusicListener.java
@@ -1,6 +1,12 @@
 package com.aetherteam.aetherii.client.event.listeners;
 
+import com.aetherteam.aetherii.AetherIITags;
 import com.aetherteam.aetherii.client.event.hooks.MusicHooks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
+import net.minecraft.sounds.Music;
+import net.minecraft.sounds.Musics;
+import net.minecraft.world.level.biome.Biome;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.client.event.SelectMusicEvent;
 
@@ -11,8 +17,13 @@ public class MusicListener {
     }
 
     public static void onMusicPlayed(SelectMusicEvent event) {
-        if (event.getMusic() != null) {
-            event.setMusic(MusicHooks.getSituationalMusic());
+        Music music = event.getOriginalMusic();
+
+        if (Minecraft.getInstance().level != null && Minecraft.getInstance().player != null && (music == Musics.CREATIVE || music == Musics.UNDER_WATER)) {
+            Holder<Biome> biome = Minecraft.getInstance().player.level().getBiome(Minecraft.getInstance().player.blockPosition());
+            if (biome.is(AetherIITags.Biomes.AETHER_MUSIC)) {
+                event.setMusic(MusicHooks.getSituationalMusic(biome));
+            }
         }
     }
 }
