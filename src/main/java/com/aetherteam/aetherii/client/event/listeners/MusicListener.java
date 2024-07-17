f978bdbc665d52a270bc86016b3ee32b1494bf81
Builderdog <81527321+Builderdog841@users.noreply.github.com>
2024-07-17 21:31:42 +0200

    fix: Music sometimes not playing at the correct time (#372)

--- src/main/java/com/aetherteam/aetherii/client/event/listeners/MusicListener.java
+++ src/main/java/com/aetherteam/aetherii/client/event/listeners/MusicListener.java
@@ -4,8 +4,6 @@ import com.aetherteam.aetherii.AetherIITags;
 import com.aetherteam.aetherii.client.event.hooks.MusicHooks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
-import net.minecraft.sounds.Music;
-import net.minecraft.sounds.Musics;
 import net.minecraft.world.level.biome.Biome;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.client.event.SelectMusicEvent;
@@ -17,9 +15,7 @@ public class MusicListener {
     }
 
     public static void onMusicPlayed(SelectMusicEvent event) {
-        Music music = event.getOriginalMusic();
-
-        if (Minecraft.getInstance().level != null && Minecraft.getInstance().player != null && (music == Musics.CREATIVE || music == Musics.UNDER_WATER)) {
+        if (Minecraft.getInstance().level != null && Minecraft.getInstance().player != null) {
             Holder<Biome> biome = Minecraft.getInstance().player.level().getBiome(Minecraft.getInstance().player.blockPosition());
             if (biome.is(AetherIITags.Biomes.AETHER_MUSIC)) {
                 event.setMusic(MusicHooks.getSituationalMusic(biome));
