d97ba3477c05ddf4b32eb1280389fb14fccdd3ec
bconlon <bconlon.gml@gmail.com>
2024-07-22 02:06:47 -0700

    improv: 2am moss stone noise blobs

--- src/main/java/com/aetherteam/aetherii/data/resources/builders/highlands/HighlandsSurfaceBuilders.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/builders/highlands/HighlandsSurfaceBuilders.java
@@ -4,12 +4,8 @@ import com.aetherteam.aetherii.block.AetherIIBlocks;
 import com.aetherteam.aetherii.data.resources.registries.AetherIIBiomes;
 import com.aetherteam.aetherii.data.resources.registries.AetherIINoises;
 import com.aetherteam.aetherii.world.surfacerule.NoisePalette3DPlacementRule;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.Noises;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
-
-import java.util.List;
 
 public class HighlandsSurfaceBuilders {
     private static final SurfaceRules.RuleSource AETHER_GRASS_BLOCK = SurfaceRules.state(AetherIIBlocks.AETHER_GRASS_BLOCK.get().defaultBlockState());
@@ -17,7 +13,10 @@ public class HighlandsSurfaceBuilders {
     private static final SurfaceRules.RuleSource AETHER_DIRT = SurfaceRules.state(AetherIIBlocks.AETHER_DIRT.get().defaultBlockState());
     private static final SurfaceRules.RuleSource UNDERSHALE = SurfaceRules.state(AetherIIBlocks.UNDERSHALE.get().defaultBlockState());
     private static final SurfaceRules.RuleSource ARCTIC_SNOW_BLOCK = SurfaceRules.state(AetherIIBlocks.ARCTIC_SNOW_BLOCK.get().defaultBlockState());
-    private static final SurfaceRules.RuleSource MOSSY_HOLYSTONE = new NoisePalette3DPlacementRule(List.of(Blocks.REDSTONE_BLOCK.defaultBlockState(), Blocks.EMERALD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState()), Noises.SURFACE, 0.5);
+    private static final SurfaceRules.RuleSource MOSSY_HOLYSTONE = new NoisePalette3DPlacementRule(AetherIIBlocks.MOSSY_HOLYSTONE.get().defaultBlockState(), AetherIIBlocks.HOLYSTONE.get().defaultBlockState(), 2, 5, 0.045);
+    private static final SurfaceRules.RuleSource PACKED_ICE = new NoisePalette3DPlacementRule(AetherIIBlocks.ARCTIC_PACKED_ICE.get().defaultBlockState(), AetherIIBlocks.HOLYSTONE.get().defaultBlockState(), 2, 5, 0.075);
+    private static final SurfaceRules.RuleSource FERROSITE = new NoisePalette3DPlacementRule(AetherIIBlocks.FERROSITE.get().defaultBlockState(), AetherIIBlocks.HOLYSTONE.get().defaultBlockState(), 3, 5, 0.05);
+//    private static final SurfaceRules.RuleSource RUSTED_FERROSITE = new NoisePalette3DPlacementRule(AetherIIBlocks.RUSTED_FERROSITE.get().defaultBlockState(), AetherIIBlocks.HOLYSTONE.get().defaultBlockState(), 1, 9, 0.03);
 
     public static SurfaceRules.RuleSource surfaceRules() {
         SurfaceRules.RuleSource surface = SurfaceRules.sequence(
@@ -46,7 +45,25 @@ public class HighlandsSurfaceBuilders {
 
                 SurfaceRules.ifTrue(SurfaceRules.verticalGradient("undershale", VerticalAnchor.absolute(80), VerticalAnchor.absolute(88)), UNDERSHALE),
 
-                MOSSY_HOLYSTONE
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.FLOURISHING_FIELD), MOSSY_HOLYSTONE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.VERDANT_WOODS), MOSSY_HOLYSTONE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.SHROUDED_FOREST), MOSSY_HOLYSTONE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.SHIMMERING_BASIN), MOSSY_HOLYSTONE),
+
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.FRIGID_SIERRA), PACKED_ICE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.ENDURING_WOODLAND), PACKED_ICE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.FROZEN_LAKES), PACKED_ICE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.SHEER_TUNDRA), PACKED_ICE),
+
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.MAGNETIC_SCAR), FERROSITE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.TURQUOISE_FOREST), FERROSITE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.GLISTENING_SWAMP), FERROSITE),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.VIOLET_HIGHWOODS), FERROSITE)
+
+//                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.MAGNETIC_SCAR), RUSTED_FERROSITE),
+//                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.TURQUOISE_FOREST), RUSTED_FERROSITE),
+//                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.GLISTENING_SWAMP), RUSTED_FERROSITE),
+//                SurfaceRules.ifTrue(SurfaceRules.isBiome(AetherIIBiomes.VIOLET_HIGHWOODS), RUSTED_FERROSITE) //todo
         );
     }
 }
--- src/main/java/com/aetherteam/aetherii/world/surfacerule/NoisePalette3DPlacementRule.java
+++ src/main/java/com/aetherteam/aetherii/world/surfacerule/NoisePalette3DPlacementRule.java
@@ -1,38 +1,39 @@
 package com.aetherteam.aetherii.world.surfacerule;
 
-import com.aetherteam.aetherii.AetherII;
-import com.aetherteam.aetherii.block.AetherIIBlocks;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.KeyDispatchDataCodec;
 import net.minecraft.util.Mth;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.XoroshiroRandomSource;
 import net.minecraft.world.level.levelgen.synth.ImprovedNoise;
-import net.minecraft.world.level.levelgen.synth.NormalNoise;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public record NoisePalette3DPlacementRule(List<BlockState> blockStates, ResourceKey<NormalNoise.NoiseParameters> noise, double noiseFreq) implements SurfaceRules.RuleSource {
+public record NoisePalette3DPlacementRule(BlockState spot, BlockState backing, int spotRatio, int backingRatio, double noiseFreq) implements SurfaceRules.RuleSource {
 	public static final KeyDispatchDataCodec<NoisePalette3DPlacementRule> KEY_CODEC = KeyDispatchDataCodec.of(RecordCodecBuilder.mapCodec(inst -> inst.group(
-			BlockState.CODEC.listOf().fieldOf("blockstates").forGetter(NoisePalette3DPlacementRule::blockStates),
-			ResourceKey.codec(Registries.NOISE).fieldOf("noise").forGetter(NoisePalette3DPlacementRule::noise),
+			BlockState.CODEC.fieldOf("spot_blockstate").forGetter(NoisePalette3DPlacementRule::spot),
+			BlockState.CODEC.fieldOf("backing_blockstate").forGetter(NoisePalette3DPlacementRule::backing),
+			Codec.INT.fieldOf("spot_ratio").forGetter(NoisePalette3DPlacementRule::spotRatio),
+			Codec.INT.fieldOf("backing_ratio").forGetter(NoisePalette3DPlacementRule::backingRatio),
 			Codec.DOUBLE.fieldOf("noise_frequency").forGetter(NoisePalette3DPlacementRule::noiseFreq)
 	).apply(inst, NoisePalette3DPlacementRule::new)));
 
 	@Override
 	public SurfaceRules.SurfaceRule apply(SurfaceRules.Context context) {
 		return (x, y, z) -> {
-			List<BlockState> blockStates = List.of(Blocks.EMERALD_BLOCK.defaultBlockState(), AetherIIBlocks.HOLYSTONE.get().defaultBlockState());
-//			AetherII.LOGGER.info(blockStates.toString());
-			ImprovedNoise noise = new ImprovedNoise(new XoroshiroRandomSource(0)) ;
-//			double noiseValue = noise.noise(x * this.noiseFreq, y * this.noiseFreq, z * this.noiseFreq);
-			double noiseValue = noise.noise(x * 0.05, y * 0.05, z * 0.05);
-			float normalizedNoise = (float) noiseValue * 0.5f + 0.5f; // Limits noise within 0.0 - 1.0 range.
+			List<BlockState> blockStates = new ArrayList<>();
+            for (int i = 0; i < this.spotRatio; i++) {
+				blockStates.add(this.spot);
+			}
+			for (int i = 0; i < this.backingRatio; i++) {
+				blockStates.add(this.backing);
+			}
+			ImprovedNoise noise = new ImprovedNoise(new XoroshiroRandomSource(0)); //todo replace with future ResourceKey<NormalNoise.NoiseParameters> noise parameter usage and context.randomState.getOrCreateNoise(this.noise);
+			double noiseValue = noise.noise(x * this.noiseFreq, y * this.noiseFreq, z * this.noiseFreq);
+			float normalizedNoise = (float) noiseValue * 0.5F + 0.5F;
 			double clamped = Mth.clampedLerp(0, blockStates.size(), normalizedNoise);
 			double finalClamp = Mth.clamp(clamped, 0, blockStates.size() - 1);
 			return blockStates.get((int) finalClamp);
