9ef7c9dc49f21cc92e5aa6129bce3b7243f727d7
bconlon <bconlon.gml@gmail.com>
2024-08-09 08:17:58 -0700

    refactor: wip first iteration aercloud reworking

--- src/main/java/com/aetherteam/aetherii/world/feature/AercloudFeature.java
+++ src/main/java/com/aetherteam/aetherii/world/feature/AercloudFeature.java
@@ -31,17 +31,17 @@ public class AercloudFeature extends Feature<AercloudConfiguration> {
         BlockState blockState = config.block().getState(random, blockPos);
 
         int width = 3;
-        int height = 2;
+        int height = 1;
 
         for (int lengthCount = 0; lengthCount < config.bounds(); ++lengthCount) {
-            blockPos = blockPos.offset(random.nextInt(2), (random.nextBoolean() ? random.nextInt(3) - 1 : 0), random.nextInt(2) * (positiveZAngle ? 1 : -1));
+            blockPos = blockPos.offset(random.nextInt(2), (random.nextInt(5) > 2 ? random.nextInt(3) - 1 : 0), random.nextInt(2) * (positiveZAngle ? 1 : -1));
 
             for (int x = 0; x < random.nextInt(2) + width; ++x) {
                 for (int y = 0; y < random.nextInt(1) + height; ++y) {
                     for (int z = 0; z < random.nextInt(2) + width; ++z) {
                         BlockPos newPosition = blockPos.offset(x, y, z);
                         if (level.isEmptyBlock(newPosition)) {
-                            if (x + y + z < 4 + random.nextInt(2)) {
+                            if (x + y + z < random.nextInt(9)) {
                                 this.setBlock(level, newPosition, blockState);
                             }
                         }
