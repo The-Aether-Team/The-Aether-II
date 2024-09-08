32144226bf8982670f7b34e26a334b5506921fc4
Builderdog841 <timon.kempter@gmail.com>
2024-09-08 19:07:29 +0200

    Fixed Arkenium Trowel Attack Damage

--- src/main/java/com/aetherteam/aetherii/item/equipment/tools/arkenium/ArkeniumTrowelItem.java
+++ src/main/java/com/aetherteam/aetherii/item/equipment/tools/arkenium/ArkeniumTrowelItem.java
@@ -5,6 +5,6 @@ import net.minecraft.world.item.HoeItem;
 
 public class ArkeniumTrowelItem extends HoeItem {
     public ArkeniumTrowelItem() {
-        super(AetherIIItemTiers.ARKENIUM, new Properties().attributes(HoeItem.createAttributes(AetherIIItemTiers.ARKENIUM, -3, 0)));
+        super(AetherIIItemTiers.ARKENIUM, new Properties().attributes(HoeItem.createAttributes(AetherIIItemTiers.ARKENIUM, -2, 0)));
     }
 }
