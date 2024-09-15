a02bc717facaf072757fd8734ca22853ba6fc491
Builderdog841 <timon.kempter@gmail.com>
2024-09-15 22:22:17 +0200

    1 Floor Main Path Gen

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/InfectedGuardianTreePools.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/InfectedGuardianTreePools.java
@@ -16,6 +16,12 @@ import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 public class InfectedGuardianTreePools {
     public static final ResourceKey<StructureTemplatePool> LOBBIES = AetherIIPools.createKey("dungeon/infected_guardian_tree/lobbies");
     public static final ResourceKey<StructureTemplatePool> ENTRANCE = AetherIIPools.createKey("dungeon/infected_guardian_tree/entrance");
+    public static final ResourceKey<StructureTemplatePool> MAIN_PATH_FLOOR_1 = AetherIIPools.createKey("dungeon/infected_guardian_tree/main_path/floor_1");
+    public static final ResourceKey<StructureTemplatePool> MAIN_PATH_FLOOR_2 = AetherIIPools.createKey("dungeon/infected_guardian_tree/main_path/floor_2");
+    public static final ResourceKey<StructureTemplatePool> MAIN_PATH_FLOOR_3 = AetherIIPools.createKey("dungeon/infected_guardian_tree/main_path/floor_3");
+    public static final ResourceKey<StructureTemplatePool> PATHS = AetherIIPools.createKey("dungeon/infected_guardian_tree/path");
+    public static final ResourceKey<StructureTemplatePool> ROOMS = AetherIIPools.createKey("dungeon/infected_guardian_tree/rooms");
+    public static final ResourceKey<StructureTemplatePool> ARENA = AetherIIPools.createKey("dungeon/infected_guardian_tree/arena");
 
     public static void bootstrap(BootstrapContext<StructureTemplatePool> context) {
         HolderGetter<StructureTemplatePool> templatePools = context.lookup(Registries.TEMPLATE_POOL);
@@ -36,6 +42,23 @@ public class InfectedGuardianTreePools {
                 fallback,
                 ImmutableList.of(
                         Pair.of(StructurePoolElement.feature(placement.getOrThrow(HighlandsPlacedFeatures.INFECTED_GUARDIAN_TREE_ENTRANCE)), 1)
+                ),
+                StructureTemplatePool.Projection.RIGID)
+        );
+
+        context.register(MAIN_PATH_FLOOR_1, new StructureTemplatePool(
+                fallback,
+                ImmutableList.of(
+                        Pair.of(AetherIIPools.aetherPool("dungeon/infected_guardian_tree/main_path/floor_1/main_path_01"), 1),
+                        Pair.of(AetherIIPools.aetherPool("dungeon/infected_guardian_tree/main_path/floor_1/main_path_02"), 1)
+                ),
+                StructureTemplatePool.Projection.RIGID)
+        );
+
+        context.register(ARENA, new StructureTemplatePool(
+                fallback,
+                ImmutableList.of(
+                        Pair.of(AetherIIPools.aetherPool("dungeon/infected_guardian_tree/boss/arena"), 1)
                 ),
                 StructureTemplatePool.Projection.RIGID)
         );
