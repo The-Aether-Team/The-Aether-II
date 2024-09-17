61b51f499bd4129fbb8177d592500096cb08dcc5
bconlon <bconlon.gml@gmail.com>
2024-09-17 13:27:25 -0700

    fix: holpupea snow state placement

--- src/main/java/com/aetherteam/aetherii/world/feature/AetherFlowerFeature.java
+++ src/main/java/com/aetherteam/aetherii/world/feature/AetherFlowerFeature.java
@@ -1,13 +1,14 @@
 package com.aetherteam.aetherii.world.feature;
 
 import com.aetherteam.aetherii.block.AetherIIBlocks;
-import com.aetherteam.aetherii.block.natural.AetherFlowerBlock;
+import com.aetherteam.aetherii.block.natural.Snowable;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.GrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
@@ -25,8 +26,9 @@ public class AetherFlowerFeature extends Feature<SimpleBlockConfiguration> {
         BlockState blockstate = simpleblockconfiguration.toPlace().getState(context.random(), blockpos);
         BlockState atstate = worldgenlevel.getBlockState(blockpos);
         BlockState belowstate = worldgenlevel.getBlockState(blockpos.below());
-        if (blockstate.getBlock() instanceof AetherFlowerBlock && (atstate.getBlock() == AetherIIBlocks.ARCTIC_SNOW.get() || (belowstate.getBlock() == AetherIIBlocks.AETHER_GRASS_BLOCK.get() && belowstate.getValue(GrassBlock.SNOWY)))) {
-            blockstate = blockstate.setValue(AetherFlowerBlock.SNOWY, true);
+        if (blockstate.getBlock() instanceof Snowable && blockstate.hasProperty(BlockStateProperties.SNOWY)
+                && (atstate.getBlock() == AetherIIBlocks.ARCTIC_SNOW.get() || (belowstate.getBlock() == AetherIIBlocks.AETHER_GRASS_BLOCK.get() && belowstate.getValue(GrassBlock.SNOWY)))) {
+            blockstate = blockstate.setValue(BlockStateProperties.SNOWY, true);
         }
         if (blockstate.canSurvive(worldgenlevel, blockpos)) {
             if (blockstate.getBlock() instanceof DoublePlantBlock) {
