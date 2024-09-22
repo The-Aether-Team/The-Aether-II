0e48d7e9fd3f03e9154c95d08feb84fa09402470
Builderdog841 <timon.kempter@gmail.com>
2024-09-22 16:24:55 +0200

    Mushrooms can now sustain on Ferrosite Mud

--- src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIBlockTagData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIBlockTagData.java
@@ -881,7 +881,10 @@ public class AetherIIBlockTagData extends BlockTagsProvider {
                 AetherIIBlocks.BRYALINN_MOSS_VINES.get(),
                 AetherIIBlocks.SHAYELINN_MOSS_VINES.get(),
                 AetherIIBlocks.AMBRELINN_MOSS_VINES.get());
-        this.tag(BlockTags.MUSHROOM_GROW_BLOCK).add(AetherIIBlocks.FERROSITE_SAND.get());
+        this.tag(BlockTags.MUSHROOM_GROW_BLOCK).add(
+                AetherIIBlocks.FERROSITE_SAND.get(),
+                AetherIIBlocks.FERROSITE_MUD.get()
+        );
 
 
         // Forge
