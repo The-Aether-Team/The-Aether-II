d0c2e5676776cc9c9283de53fbde318fe5b3d595
Builderdog841 <timon.kempter@gmail.com>
2024-09-26 22:59:14 +0200

    Fixed Skyroot Lizards taking Fall Damage

--- src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIEntityTagData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIEntityTagData.java
@@ -80,7 +80,19 @@ public class AetherIIEntityTagData extends EntityTypeTagsProvider {
         );
 
         // Vanilla
-        this.tag(EntityTypeTags.IMPACT_PROJECTILES).add(AetherIIEntityTypes.HOLYSTONE_ROCK.get(), AetherIIEntityTypes.ARCTIC_SNOWBALL.get(), AetherIIEntityTypes.SKYROOT_PINECONE.get(), AetherIIEntityTypes.SCATTERGLASS_BOLT.get());
-        this.tag(EntityTypeTags.FALL_DAMAGE_IMMUNE).addTag(AetherIITags.Entities.KIRRID).add(AetherIIEntityTypes.AERBUNNY.get()).add(AetherIIEntityTypes.PHYG.get()).add(AetherIIEntityTypes.ZEPHYR.get()).add(AetherIIEntityTypes.FLYING_COW.get()).add(AetherIIEntityTypes.SKYROOT_LIZARD.get());
+        this.tag(EntityTypeTags.IMPACT_PROJECTILES).add(
+                AetherIIEntityTypes.HOLYSTONE_ROCK.get(),
+                AetherIIEntityTypes.ARCTIC_SNOWBALL.get(),
+                AetherIIEntityTypes.SKYROOT_PINECONE.get(),
+                AetherIIEntityTypes.SCATTERGLASS_BOLT.get());
+        this.tag(EntityTypeTags.FALL_DAMAGE_IMMUNE).addTag(
+                AetherIITags.Entities.KIRRID
+        ).add(
+                AetherIIEntityTypes.PHYG.get(),
+                AetherIIEntityTypes.FLYING_COW.get(),
+                AetherIIEntityTypes.AERBUNNY.get(),
+                AetherIIEntityTypes.SKYROOT_LIZARD.get(),
+                AetherIIEntityTypes.ZEPHYR.get()
+        );
     }
 }
