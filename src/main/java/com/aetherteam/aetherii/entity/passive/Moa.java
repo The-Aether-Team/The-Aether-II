a2a614b3bd74abf13b38c6997d6f038173e22c94
Builderdog841 <timon.kempter@gmail.com>
2024-09-30 19:26:21 +0200

    Moas can now be fed

--- src/main/java/com/aetherteam/aetherii/entity/passive/Moa.java
+++ src/main/java/com/aetherteam/aetherii/entity/passive/Moa.java
@@ -453,7 +453,7 @@ public class Moa extends MountableAnimal {
             this.setSitting(!this.isSitting());
 
             return InteractionResult.sidedSuccess(this.level().isClientSide());
-        } else if (!this.level().isClientSide() && this.isPlayerGrown() && this.isBaby() && this.isHungry() && this.getAmountFed() < 3 && itemStack.is(AetherIITags.Items.MOA_FOOD)) { // Feeds a hungry baby Moa.
+        } else if (!this.level().isClientSide() /*&& this.isPlayerGrown() TODO*/ && this.isBaby() && this.isHungry() && this.getAmountFed() < 3 && itemStack.is(AetherIITags.Items.MOA_FOOD)) { // Feeds a hungry baby Moa.
             if (!player.getAbilities().instabuild) {
                 itemStack.shrink(1);
             }
