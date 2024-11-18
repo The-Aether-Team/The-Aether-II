3f2fef7d2dad60d7a8ae684452d72e423489dff6
bconlon <bconlon.gml@gmail.com>
2024-11-17 18:54:59 -0800

    improv: add glider tooltips

--- src/main/java/com/aetherteam/aetherii/data/generators/AetherIILanguageData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/AetherIILanguageData.java
@@ -829,6 +829,13 @@ public class AetherIILanguageData extends AetherIILanguageProvider {
         this.addPerItemAbilityTooltip(AetherIIItems.GRAVITITE_GLOVES.get(), 1, "§9Ability:§r Double Jump");
         this.addPerItemAbilityTooltip(AetherIIItems.GRAVITITE_GLOVES.get(), 2, "§9Set Pieces:§r %s");
 
+        this.addPerItemAbilityTooltip(AetherIIItems.COLD_AERCLOUD_GLIDER.get(), 1, "§3Use:§r Hold-Use");
+        this.addPerItemAbilityTooltip(AetherIIItems.GOLDEN_AERCLOUD_GLIDER.get(), 1, "§3Use:§r Hold-Use");
+        this.addPerItemAbilityTooltip(AetherIIItems.BLUE_AERCLOUD_GLIDER.get(), 1, "§9Ability:§r Upwards Boost");
+        this.addPerItemAbilityTooltip(AetherIIItems.BLUE_AERCLOUD_GLIDER.get(), 2, "§3Use:§r Hold-Use");
+        this.addPerItemAbilityTooltip(AetherIIItems.PURPLE_AERCLOUD_GLIDER.get(), 1, "§9Ability:§r Forwards Boost");
+        this.addPerItemAbilityTooltip(AetherIIItems.PURPLE_AERCLOUD_GLIDER.get(), 2, "§3Use:§r Hold-Use");
+
 
         // Miscellaneous Item Tooltips
         this.addItemTooltip("treasure.description", "Treasure Item");
