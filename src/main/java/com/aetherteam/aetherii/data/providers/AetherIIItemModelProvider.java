baeee7451b409cabc72e5224c6ec6f099310a928
bconlon <bconlon.gml@gmail.com>
2024-11-20 17:08:49 -0800

    improv: better glider model generation and layout

--- src/main/java/com/aetherteam/aetherii/data/providers/AetherIIItemModelProvider.java
+++ src/main/java/com/aetherteam/aetherii/data/providers/AetherIIItemModelProvider.java
@@ -110,66 +110,67 @@ public abstract class AetherIIItemModelProvider extends NitrogenItemModelProvide
     }
 
     public void gliderItem(Item item, boolean hasAbility) {
-        ItemModelBuilder inventory = this.nested().parent(this.getExistingFile(this.mcLoc("item/generated")))
-                .texture("layer0", this.modLoc("item/miscellaneous/" + this.itemName(item)));
-        ItemModelBuilder gliderClosed = this.nested().parent(this.getExistingFile(this.modLoc("item/aercloud_glider_closed")))
-                .texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
+        ItemModelBuilder normalInventorySprite = this.nested().parent(this.getExistingFile(this.mcLoc("item/generated"))).texture("layer0", this.modLoc("item/miscellaneous/" + this.itemName(item)));
+
+        ItemModelBuilder closedGliderModelBase = this.nested().parent(this.getExistingFile(this.modLoc("item/aercloud_glider_closed"))).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
                 .transforms()
                 .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND).rotation(-90.0F, 0.0F, -80.0F).translation(5.75F, -2.0F, 4.15F).scale(0.75F).end()
                 .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).rotation(0.0F, 0.0F, -90.0F).translation(7.5F, 0.0F, 4.0F).end()
                 .end();
-        ItemModelBuilder gliderOpen = this.nested().parent(this.getExistingFile(this.modLoc("item/aercloud_glider_open")))
-                .texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
+        ItemModelBuilder openGliderModelBase = this.nested().parent(this.getExistingFile(this.modLoc("item/aercloud_glider_open"))).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
                 .transforms()
                 .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND).rotation(-90.0F, 0.0F, -35.0F).translation(11.55F, -2.0F, -0.1F).end()
                 .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).rotation(0.0F, 0.0F, 0.0F).translation(0.0F, 9.0F, -6.0F).end()
                 .end();
 
-        ItemModelBuilder normalOverride = this.withExistingParent(this.itemName(item) + "_model", this.modLoc("item/aercloud_glider_open")).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
-                .texture("particle", this.modLoc("item/miscellaneous/" + this.itemName(item)))
+        ItemModelBuilder openPredicate = this.withExistingParent(this.itemName(item) + "_open", this.modLoc("item/aercloud_glider_open")).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
                 .customLoader((itemModelBuilder, existingFileHelper) ->
                         SeparateTransformsModelBuilder.begin(itemModelBuilder, existingFileHelper)
-                                .base(gliderOpen)
-                                .perspective(ItemDisplayContext.GUI, inventory)
-                                .perspective(ItemDisplayContext.GROUND, inventory)
-                                .perspective(ItemDisplayContext.FIXED, inventory)
+                                .base(openGliderModelBase)
+                                .perspective(ItemDisplayContext.GUI, normalInventorySprite)
+                                .perspective(ItemDisplayContext.GROUND, normalInventorySprite)
+                                .perspective(ItemDisplayContext.FIXED, normalInventorySprite)
                 ).end();
+        ItemModelBuilder closedPredicate = this.withExistingParent(this.itemName(item) + "_closed", this.modLoc("item/aercloud_glider_closed")).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
+                .customLoader((itemModelBuilder, existingFileHelper) ->
+                        SeparateTransformsModelBuilder.begin(itemModelBuilder, existingFileHelper)
+                                .base(closedGliderModelBase)
+                                .perspective(ItemDisplayContext.GUI, normalInventorySprite)
+                                .perspective(ItemDisplayContext.GROUND, normalInventorySprite)
+                                .perspective(ItemDisplayContext.FIXED, normalInventorySprite)
+                ).end();
+
         if (!hasAbility) {
             this.withExistingParent(this.itemName(item), this.mcLoc("item/generated"))
                     .texture("particle", this.modLoc("item/miscellaneous/" + this.itemName(item)))
-                    .customLoader((itemModelBuilder, existingFileHelper) ->
-                            SeparateTransformsModelBuilder.begin(itemModelBuilder, existingFileHelper)
-                                    .base(gliderClosed)
-                                    .perspective(ItemDisplayContext.GUI, inventory)
-                                    .perspective(ItemDisplayContext.GROUND, inventory)
-                                    .perspective(ItemDisplayContext.FIXED, inventory)
-                    ).end()
-                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 1.0F).model(normalOverride).end();
+                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 0.0F).model(closedPredicate).end()
+                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 1.0F).model(openPredicate).end();
         } else {
-            ItemModelBuilder inventoryDull = this.nested().parent(this.getExistingFile(this.mcLoc("item/generated")))
-                    .texture("layer0", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_dull"));
-            ItemModelBuilder dullOverride = this.withExistingParent(this.itemName(item) + "_model", this.modLoc("item/aercloud_glider_open")).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
-                    .texture("particle", this.modLoc("item/miscellaneous/" + this.itemName(item)))
+            ItemModelBuilder dullInventorySprite = this.nested().parent(this.getExistingFile(this.mcLoc("item/generated"))).texture("layer0", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_dull"));
+
+            ItemModelBuilder dullOpenPredicate = this.withExistingParent(this.itemName(item) + "_dull_open", this.modLoc("item/aercloud_glider_open")).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
                     .customLoader((itemModelBuilder, existingFileHelper) ->
                             SeparateTransformsModelBuilder.begin(itemModelBuilder, existingFileHelper)
-                                    .base(gliderOpen)
-                                    .perspective(ItemDisplayContext.GUI, inventoryDull)
-                                    .perspective(ItemDisplayContext.GROUND, inventoryDull)
-                                    .perspective(ItemDisplayContext.FIXED, inventoryDull)
+                                    .base(openGliderModelBase)
+                                    .perspective(ItemDisplayContext.GUI, dullInventorySprite)
+                                    .perspective(ItemDisplayContext.GROUND, dullInventorySprite)
+                                    .perspective(ItemDisplayContext.FIXED, dullInventorySprite)
+                    ).end();
+            ItemModelBuilder dullClosedPredicate = this.withExistingParent(this.itemName(item) + "_dull_closed", this.modLoc("item/aercloud_glider_closed")).texture("glider", this.modLoc("item/miscellaneous/" + this.itemName(item) + "_model"))
+                    .customLoader((itemModelBuilder, existingFileHelper) ->
+                            SeparateTransformsModelBuilder.begin(itemModelBuilder, existingFileHelper)
+                                    .base(closedGliderModelBase)
+                                    .perspective(ItemDisplayContext.GUI, dullInventorySprite)
+                                    .perspective(ItemDisplayContext.GROUND, dullInventorySprite)
+                                    .perspective(ItemDisplayContext.FIXED, dullInventorySprite)
                     ).end();
 
             this.withExistingParent(this.itemName(item), this.mcLoc("item/generated"))
                     .texture("particle", this.modLoc("item/miscellaneous/" + this.itemName(item)))
-                    .customLoader((itemModelBuilder, existingFileHelper) ->
-                            SeparateTransformsModelBuilder.begin(itemModelBuilder, existingFileHelper)
-                                    .base(gliderClosed)
-                                    .perspective(ItemDisplayContext.GUI, inventory)
-                                    .perspective(ItemDisplayContext.GROUND, inventory)
-                                    .perspective(ItemDisplayContext.FIXED, inventory)
-                    ).end()
-                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 1.0F).predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "dull"), 0.0F).model(normalOverride).end()
-                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "dull"), 1.0F).model(dullOverride).end();
-
+                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 0.0F).predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "dull"), 0.0F).model(closedPredicate).end()
+                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 1.0F).predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "dull"), 0.0F).model(openPredicate).end()
+                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 0.0F).predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "dull"), 1.0F).model(dullClosedPredicate).end()
+                    .override().predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "parachuting"), 1.0F).predicate(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "dull"), 1.0F).model(dullOpenPredicate).end();
         }
     }
 
