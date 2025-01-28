1fbcde749af7fac80f180c6f69316796d1c608fa
Builderdog <timon.kempter@gmail.com>
2025-01-28 18:12:26 +0100

    fix: Fixed Merge Issues

--- src/main/java/com/aetherteam/aetherii/data/resources/builders/worldgen/highlands/HighlandsBiomeBuilders.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/builders/worldgen/highlands/HighlandsBiomeBuilders.java
@@ -170,18 +170,20 @@ public class HighlandsBiomeBuilders {
                 downfall,
                 HIGHFIELDS_EFFECTS,
                 spawnSettingsBuilder
-                        .addMobCharge(AetherIIEntityTypes.AECHOR_PLANT.get(), 0.4, 0.11)
+                        .addMobCharge(AetherIIEntityTypes.AECHOR_PLANT.get(), 0.3, 0.11)
+                        .addMobCharge(AetherIIEntityTypes.CARRION_SPROUT.get(), 0.3, 0.11)
                         .addMobCharge(AetherIIEntityTypes.SWET.get(), 0.25, 0.1)
                         .addMobCharge(AetherIIEntityTypes.ZEPHYR.get(), 0.3, 0.16)
                         .addMobCharge(AetherIIEntityTypes.TEMPEST.get(), 0.6, 0.16)
                         .addMobCharge(AetherIIEntityTypes.COCKATRICE.get(), 0.4, 0.15)
                         .addMobCharge(AetherIIEntityTypes.SKEPHID.get(), 0.5, 0.15)
+                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.AECHOR_PLANT.get(), 1, 1, 1))
+                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.CARRION_SPROUT.get(), 1, 1, 1))
+                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.SWET.get(), 3, 1, 3))
                         .addSpawn(AetherIIMobCategory.AETHER_SKY_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.ZEPHYR.get(), 3, 1, 2))
                         .addSpawn(AetherIIMobCategory.AETHER_SKY_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.TEMPEST.get(), 2, 1, 1))
                         .addSpawn(AetherIIMobCategory.AETHER_DARKNESS_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.COCKATRICE.get(), 2, 1, 1))
                         .addSpawn(AetherIIMobCategory.AETHER_DARKNESS_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.SKEPHID.get(), 1, 1, 2))
-                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.AECHOR_PLANT.get(), 1, 1, 1))
-                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.SWET.get(), 3, 1, 3))
                         .build(),
                 builder
                         .addFeature(GenerationStep.Decoration.RAW_GENERATION, HighlandsPlacedFeatures.NOISE_LAKE)
@@ -332,13 +334,15 @@ public class HighlandsBiomeBuilders {
                 downfall,
                 MAGNETIC_EFFECTS,
                 spawnSettingsBuilder
-                        .addMobCharge(AetherIIEntityTypes.AECHOR_PLANT.get(), 0.4, 0.11)
+                        .addMobCharge(AetherIIEntityTypes.AECHOR_PLANT.get(), 0.3, 0.11)
+                        .addMobCharge(AetherIIEntityTypes.CARRION_SPROUT.get(), 0.3, 0.11)
                         .addMobCharge(AetherIIEntityTypes.SWET.get(), 0.25, 0.1)
                         .addMobCharge(AetherIIEntityTypes.ZEPHYR.get(), 0.3, 0.16)
                         .addMobCharge(AetherIIEntityTypes.TEMPEST.get(), 0.6, 0.16)
                         .addMobCharge(AetherIIEntityTypes.COCKATRICE.get(), 0.4, 0.15)
                         .addMobCharge(AetherIIEntityTypes.SKEPHID.get(), 0.5, 0.15)
                         .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.AECHOR_PLANT.get(), 1, 1, 1))
+                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.CARRION_SPROUT.get(), 1, 1, 1))
                         .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.SWET.get(), 3, 1, 3))
                         .addSpawn(AetherIIMobCategory.AETHER_SKY_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.ZEPHYR.get(), 3, 1, 2))
                         .addSpawn(AetherIIMobCategory.AETHER_SKY_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.TEMPEST.get(), 2, 1, 1))
@@ -481,13 +485,15 @@ public class HighlandsBiomeBuilders {
                 downfall,
                 ARCTIC_EFFECTS,
                 spawnSettingsBuilder
-                        .addMobCharge(AetherIIEntityTypes.AECHOR_PLANT.get(), 0.4, 0.11)
+                        .addMobCharge(AetherIIEntityTypes.AECHOR_PLANT.get(), 0.3, 0.11)
+                        .addMobCharge(AetherIIEntityTypes.CARRION_SPROUT.get(), 0.3, 0.11)
                         .addMobCharge(AetherIIEntityTypes.SWET.get(), 0.25, 0.1)
                         .addMobCharge(AetherIIEntityTypes.ZEPHYR.get(), 0.3, 0.16)
                         .addMobCharge(AetherIIEntityTypes.TEMPEST.get(), 0.6, 0.16)
                         .addMobCharge(AetherIIEntityTypes.COCKATRICE.get(), 0.4, 0.15)
                         .addMobCharge(AetherIIEntityTypes.SKEPHID.get(), 0.5, 0.15)
                         .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.AECHOR_PLANT.get(), 1, 1, 1))
+                        .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.CARRION_SPROUT.get(), 1, 1, 1))
                         .addSpawn(AetherIIMobCategory.AETHER_SURFACE_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.SWET.get(), 3, 1, 3))
                         .addSpawn(AetherIIMobCategory.AETHER_SKY_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.ZEPHYR.get(), 3, 1, 2))
                         .addSpawn(AetherIIMobCategory.AETHER_SKY_MONSTER, new MobSpawnSettings.SpawnerData(AetherIIEntityTypes.TEMPEST.get(), 2, 1, 1))
