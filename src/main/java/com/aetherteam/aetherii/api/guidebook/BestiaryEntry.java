fc2b1e229d1a1805ba5c7de34f4116568ddad280
bconlon <bconlon.gml@gmail.com>
2025-01-28 10:42:35 -0800

    improv: finish reworking BestiaryEntry

--- src/main/java/com/aetherteam/aetherii/api/guidebook/BestiaryEntry.java
+++ src/main/java/com/aetherteam/aetherii/api/guidebook/BestiaryEntry.java
@@ -1,6 +1,7 @@
 package com.aetherteam.aetherii.api.guidebook;
 
 import com.aetherteam.aetherii.data.resources.registries.AetherIIBestiaryEntries;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
@@ -35,33 +36,45 @@ public class BestiaryEntry extends GuidebookEntry {
                     BestiaryEntry.DESCRIPTION_KEY.mapCodec().forGetter((entry) -> entry.getDescriptionKey().getValue()),
                     BestiaryEntry.ENTITY_TYPE.mapCodec().forGetter((entry) -> entry.getEntityType().getValue()),
                     BestiaryEntry.SCALE_MULTIPLIER.mapCodec().forGetter((entry) -> entry.getScaleMultiplier().getValue()),
-                    LootDisplay.DIRECT_CODEC.sizeLimitedListOf(3).fieldOf("loot").forGetter((entry) -> entry.getLoot().value()),
-                    TagKey.codec(Registries.ITEM).optionalFieldOf("food").forGetter((entry) -> entry.getFood().value())
+                    BestiaryEntry.LOOT_1.mapCodec().forGetter((entry) -> entry.getLoot1().getValue()),
+                    BestiaryEntry.LOOT_2.mapCodec().forGetter((entry) -> entry.getLoot2().getValue()),
+                    BestiaryEntry.LOOT_3.mapCodec().forGetter((entry) -> entry.getLoot3().getValue()),
+                    BestiaryEntry.FOOD.mapCodec().forGetter((entry) -> entry.getFood().getValue())
             ).apply(in, BestiaryEntry::new));
     public static final Codec<Holder<BestiaryEntry>> REFERENCE_CODEC = RegistryFileCodec.create(AetherIIBestiaryEntries.BESTIARY_ENTRY_REGISTRY_KEY, DIRECT_CODEC);
     public static final StreamCodec<RegistryFriendlyByteBuf, Holder<BestiaryEntry>> STREAM_CODEC = ByteBufCodecs.holderRegistry(AetherIIBestiaryEntries.BESTIARY_ENTRY_REGISTRY_KEY);
 
     public static final DataTemplate<Holder<EntityType<?>>> ENTITY_TYPE = new DataTemplate<>("entity_type", BuiltInRegistries.ENTITY_TYPE.holderByNameCodec()::fieldOf);
     public static final DataTemplate<Optional<Double>> SCALE_MULTIPLIER = new DataTemplate<>("scale_multiplier", Codec.DOUBLE::optionalFieldOf);
+    public static final DataTemplate<LootDisplay> LOOT_1 = new DataTemplate<>("loot_1", LootDisplay.DIRECT_CODEC::fieldOf);
+    public static final DataTemplate<LootDisplay> LOOT_2 = new DataTemplate<>("loot_2", LootDisplay.DIRECT_CODEC::fieldOf);
+    public static final DataTemplate<LootDisplay> LOOT_3 = new DataTemplate<>("loot_3", LootDisplay.DIRECT_CODEC::fieldOf);
+    public static final DataTemplate<Optional<TagKey<Item>>> FOOD = new DataTemplate<>("food", TagKey.codec(Registries.ITEM)::optionalFieldOf);
 
     private final Info<Holder<EntityType<?>>> entityType;
     private final Info<Optional<Double>> scaleMultiplier;
-    private final Info<List<LootDisplay>> loot; //todo need to make each loot display entry individually toggleable
+    private final Info<LootDisplay> loot1;
+    private final Info<LootDisplay> loot2;
+    private final Info<LootDisplay> loot3;
     private final Info<Optional<TagKey<Item>>> food;
 
-    public BestiaryEntry(ResourceLocation icon, Optional<String> name, Optional<String> slotName, Optional<String> slotSubtitle, String descriptionKey, Holder<EntityType<?>> entityType, Optional<Double> scaleMultiplier, List<LootDisplay> loot, Optional<TagKey<Item>> food) {
+    public BestiaryEntry(ResourceLocation icon, Optional<String> name, Optional<String> slotName, Optional<String> slotSubtitle, String descriptionKey, Holder<EntityType<?>> entityType, Optional<Double> scaleMultiplier, LootDisplay loot1, LootDisplay loot2, LootDisplay loot3, Optional<TagKey<Item>> food) {
         super(icon, name, slotName, slotSubtitle, descriptionKey);
         this.entityType = this.info(ENTITY_TYPE, entityType);
         this.scaleMultiplier = this.info(SCALE_MULTIPLIER, scaleMultiplier);
-        this.loot = this.info(loot);
-        this.food = this.info(food);
+        this.loot1 = this.info(LOOT_1, loot1);
+        this.loot2 = this.info(LOOT_2, loot2);
+        this.loot3 = this.info(LOOT_3, loot3);
+        this.food = this.info(FOOD, food);
     }
 
-    public BestiaryEntry(Info<ResourceLocation> icon, Info<Optional<String>> name, Info<Optional<String>> slotName, Info<Optional<String>> slotSubtitle, Info<String> descriptionKey, Info<Holder<EntityType<?>>> entityType, Info<Optional<Double>> scaleMultiplier, Info<List<LootDisplay>> loot, Info<Optional<TagKey<Item>>> food) {
+    public BestiaryEntry(Info<ResourceLocation> icon, Info<Optional<String>> name, Info<Optional<String>> slotName, Info<Optional<String>> slotSubtitle, Info<String> descriptionKey, Info<Holder<EntityType<?>>> entityType, Info<Optional<Double>> scaleMultiplier, Info<LootDisplay> loot1, Info<LootDisplay> loot2, Info<LootDisplay> loot3, Info<Optional<TagKey<Item>>> food) {
         super(icon, name, slotName, slotSubtitle, descriptionKey);
         this.entityType = entityType;
         this.scaleMultiplier = scaleMultiplier;
-        this.loot = loot;
+        this.loot1 = loot1;
+        this.loot2 = loot2;
+        this.loot3 = loot3;
         this.food = food;
     }
 
@@ -73,8 +86,20 @@ public class BestiaryEntry extends GuidebookEntry {
         return this.scaleMultiplier;
     }
 
-    public Info<List<LootDisplay>> getLoot() {
-        return this.loot;
+    public Info<LootDisplay> getLoot1() {
+        return this.loot1;
+    }
+
+    public Info<LootDisplay> getLoot2() {
+        return this.loot2;
+    }
+
+    public Info<LootDisplay> getLoot3() {
+        return this.loot3;
+    }
+
+    public List<Info<LootDisplay>> getLoot() {
+        return ImmutableList.of(this.getLoot1(), this.getLoot2(), this.getLoot3());
     }
 
     public Info<Optional<TagKey<Item>>> getFood() {
@@ -98,7 +123,7 @@ public class BestiaryEntry extends GuidebookEntry {
             return new LootDisplay(Either.right(item), chance, minCount, maxCount);
         }
 
-        public ItemLike getItemLike() { //todo wait i dont need this. all blocks are registered to the item registry too.
+        public ItemLike getItemLike() {
             if (this.item.right().isPresent()) {
                 return this.item.right().get().value();
             } else if (this.item.left().isPresent()) {
@@ -113,7 +138,7 @@ public class BestiaryEntry extends GuidebookEntry {
         private final Map<String, Info<?>> clientValues = new HashMap<>();
 
         public Client(BestiaryEntry entry) {
-            super(entry.getIcon(), entry.getName(), entry.getSlotName(), entry.getSlotSubtitle(), entry.getDescriptionKey(), entry.getEntityType(), entry.getScaleMultiplier(), entry.getLoot(), entry.getFood());
+            super(entry.getIcon(), entry.getName(), entry.getSlotName(), entry.getSlotSubtitle(), entry.getDescriptionKey(), entry.getEntityType(), entry.getScaleMultiplier(), entry.getLoot1(), entry.getLoot2(), entry.getLoot3(), entry.getFood());
             this.clientValues.putAll(this.getValues());
         }
 
