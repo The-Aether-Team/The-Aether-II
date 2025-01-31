5236a2513c1e610a4119b435c3dc2c419cb328fe
bconlon <bconlon.gml@gmail.com>
2025-01-30 19:20:38 -0800

    fix: serialize slot index

--- src/main/java/com/aetherteam/aetherii/inventory/container/AccessoryContainer.java
+++ src/main/java/com/aetherteam/aetherii/inventory/container/AccessoryContainer.java
@@ -1,22 +1,48 @@
 package com.aetherteam.aetherii.inventory.container;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.util.INBTSerializable;
 
-public class AccessoryContainer extends SimpleContainer implements INBTSerializable<ListTag> { //todo on world load the items get shuffled a bit for some reason.
+public class AccessoryContainer extends SimpleContainer implements INBTSerializable<ListTag> {
     public AccessoryContainer(int size) {
         super(size);
     }
 
     @Override
     public ListTag serializeNBT(HolderLookup.Provider provider) {
-        return this.createTag(provider);
+        ListTag listTag = new ListTag();
+
+        for (int i = 0; i < this.getContainerSize(); ++i) {
+            ItemStack itemstack = this.getItem(i);
+            if (!itemstack.isEmpty()) {
+                CompoundTag tag = new CompoundTag();
+                tag.putByte("Slot", (byte) i);
+                listTag.add(itemstack.save(provider, tag));
+            }
+        }
+
+        return listTag;
     }
 
     @Override
-    public void deserializeNBT(HolderLookup.Provider provider, ListTag tag) {
-        this.fromTag(tag, provider);
+    public void deserializeNBT(HolderLookup.Provider provider, ListTag listTag) {
+        this.clearContent();
+
+        for (int i = 0; i < listTag.size(); ++i) {
+            CompoundTag tag = listTag.getCompound(i);
+            int index = tag.getByte("Slot") & 255;
+            if (index < this.getContainerSize()) {
+                this.setItem(index, ItemStack.parse(provider, tag).orElse(ItemStack.EMPTY));
+            }
+        }
+    }
+
+    public void tick(LivingEntity livingEntity) {
+
     }
 }
