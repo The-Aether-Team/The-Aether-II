60992902eb6874f4ae7cdec45dfce455a18fcfa3
bconlon <bconlon.gml@gmail.com>
2025-02-01 14:29:09 -0800

    improv: EquipmentUtil uses AccessoryUtil

--- src/main/java/com/aetherteam/aetherii/inventory/container/AccessoryContainer.java
+++ src/main/java/com/aetherteam/aetherii/inventory/container/AccessoryContainer.java
@@ -8,9 +8,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.util.INBTSerializable;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AccessoryContainer extends SimpleContainer implements INBTSerializable<ListTag> {
     public AccessoryContainer(int size) {
         super(size);
