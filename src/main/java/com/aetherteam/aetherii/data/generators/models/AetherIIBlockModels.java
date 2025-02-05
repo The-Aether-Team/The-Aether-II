fafc97aebaa03d5bdc7250e95992d8621d3af131
Builderdog <timon.kempter@gmail.com>
2025-02-05 17:16:52 +0100

    improv: Updated Root Transition Block Textures

--- src/main/java/com/aetherteam/aetherii/data/generators/models/AetherIIBlockModels.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/models/AetherIIBlockModels.java
@@ -338,8 +338,8 @@ public class AetherIIBlockModels extends AetherIIBlockModelSubProvider {
         this.createTrivialCube(AetherIIBlocks.DIM_INFECTED_ROOTS.get());
         this.createTrivialCube(AetherIIBlocks.LUCENT_INFECTED_ROOTS.get());
         this.createTrivialCube(AetherIIBlocks.INFECTED_LAMP.get());
-        this.createFacingTopBottomColumnWithHorizontalVariantGeneric(AetherIIBlocks.BASE_ROOTED_INFECTED_WOOD.get(), AetherIIBlocks.INFECTED_LOG.get(), AetherIIBlocks.INFECTED_ROOTS.get());
-        this.createFacingTopBottomColumnWithHorizontalVariantGeneric(AetherIIBlocks.TOP_ROOTED_INFECTED_WOOD.get(), AetherIIBlocks.INFECTED_ROOTS.get(), AetherIIBlocks.INFECTED_LOG.get());
+        this.createFacingTopBottomColumnWithHorizontalVariantGeneric(AetherIIBlocks.BASE_ROOTED_INFECTED_WOOD.get(), AetherIIBlocks.INFECTED_ROOTS.get(), AetherIIBlocks.INFECTED_LOG.get());
+        this.createFacingTopBottomColumnWithHorizontalVariantGeneric(AetherIIBlocks.TOP_ROOTED_INFECTED_WOOD.get(), AetherIIBlocks.INFECTED_LOG.get(), AetherIIBlocks.INFECTED_ROOTS.get());
 
         // Wool
         this.createFullAndCarpetBlocks(AetherIIBlocks.CLOUDWOOL.get(), AetherIIBlocks.CLOUDWOOL_CARPET.get());
