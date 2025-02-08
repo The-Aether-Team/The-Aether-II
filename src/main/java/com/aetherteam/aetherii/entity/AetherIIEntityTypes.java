9dc455fcbff9f03a9aee572306171efe8d0bd6cc
Builderdog <timon.kempter@gmail.com>
2025-02-08 23:32:58 +0100

    fix: Fixed Zephyr Hitbox size

--- src/main/java/com/aetherteam/aetherii/entity/AetherIIEntityTypes.java
+++ src/main/java/com/aetherteam/aetherii/entity/AetherIIEntityTypes.java
@@ -66,7 +66,7 @@ public class AetherIIEntityTypes {
             () -> EntityType.Builder.of(CarrionSprout::new, AetherIIMobCategory.AETHER_SURFACE_MONSTER).sized(1.0F, 1.0F).clientTrackingRange(8).build(AetherIIEntities.CARRION_SPROUT));
 
     public static final DeferredHolder<EntityType<?>, EntityType<Zephyr>> ZEPHYR = ENTITY_TYPES.register("zephyr",
-            () -> EntityType.Builder.of(Zephyr::new, AetherIIMobCategory.AETHER_SKY_MONSTER).sized(4.0F, 1.75F).clientTrackingRange(10).build(AetherIIEntities.ZEPHYR));
+            () -> EntityType.Builder.of(Zephyr::new, AetherIIMobCategory.AETHER_SKY_MONSTER).sized(2.0F, 1.75F).clientTrackingRange(10).build(AetherIIEntities.ZEPHYR));
 
     public static final DeferredHolder<EntityType<?>, EntityType<Tempest>> TEMPEST = ENTITY_TYPES.register("tempest",
             () -> EntityType.Builder.of(Tempest::new, AetherIIMobCategory.AETHER_SKY_MONSTER).sized(1.5F, 1.5F).clientTrackingRange(10).build(AetherIIEntities.TEMPEST));
