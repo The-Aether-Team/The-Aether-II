810512efbea1ba897308d682c0cd280aa4771ebd
bconlon <bconlon.gml@gmail.com>
2025-02-17 09:59:39 -0800

    fix: MutableEntry issue

--- src/main/java/com/aetherteam/aetherii/api/guidebook/MutableEntry.java
+++ src/main/java/com/aetherteam/aetherii/api/guidebook/MutableEntry.java
@@ -5,7 +5,7 @@ import net.minecraft.core.Holder;
 import java.util.Map;
 
 public interface MutableEntry {
-    Holder<BestiaryEntry> getEntry();
+    Holder<? extends GuidebookEntry> getEntry();
 
     Map<String, GuidebookEntry.Info> getClientValues();
 }
