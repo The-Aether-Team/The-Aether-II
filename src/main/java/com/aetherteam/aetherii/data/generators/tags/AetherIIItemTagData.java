11335e2c52f61876956b42c0e7c29547f91bbf6c
Builderdog841 <timon.kempter@gmail.com>
2025-03-17 10:59:57 +0100

    fix: Fixed Aether Hammers, Spears and Crossbows not being enchantable

--- src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIItemTagData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIItemTagData.java
@@ -635,6 +635,52 @@ public class AetherIIItemTagData extends ItemTagsProvider {
                 AetherIIItems.BURRUKAI_PELT_BOOTS.get(),
                 AetherIIItems.BURRUKAI_PELT_GLOVES.get()
         );
+        this.tag(ItemTags.SWORD_ENCHANTABLE).addTags(
+                AetherIITags.Items.TOOLS_SPEARS,
+                AetherIITags.Items.TOOLS_HAMMERS
+        );
+        this.tag(ItemTags.CROSSBOW_ENCHANTABLE).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
+        this.tag(ItemTags.DURABILITY_ENCHANTABLE).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
+        this.tag(ItemTags.VANISHING_ENCHANTABLE).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
+        this.tag(ItemTags.PILLAGER_PREFERRED_WEAPONS).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
+        this.tag(ItemTags.PIGLIN_PREFERRED_WEAPONS).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
+        this.tag(ItemTags.WITHER_SKELETON_DISLIKED_WEAPONS).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
 
         // Forge
         this.tag(Tags.Items.BOOKSHELVES).add(
@@ -713,10 +759,24 @@ public class AetherIIItemTagData extends ItemTagsProvider {
                 AetherIIItems.ARKENIUM_CROSSBOW.get(),
                 AetherIIItems.GRAVITITE_CROSSBOW.get()
         );
+        this.tag(Tags.Items.RANGED_WEAPON_TOOLS).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
+        );
         this.tag(Tags.Items.TOOLS).addTags(
                 AetherIITags.Items.TOOLS_SHORTSWORDS,
                 AetherIITags.Items.TOOLS_HAMMERS,
                 AetherIITags.Items.TOOLS_SPEARS
+        );
+        this.tag(Tags.Items.ENCHANTABLES).add(
+                AetherIIItems.SKYROOT_CROSSBOW.get(),
+                AetherIIItems.HOLYSTONE_CROSSBOW.get(),
+                AetherIIItems.ZANITE_CROSSBOW.get(),
+                AetherIIItems.ARKENIUM_CROSSBOW.get(),
+                AetherIIItems.GRAVITITE_CROSSBOW.get()
         );
         this.tag(ItemTags.HEAD_ARMOR).add(
                 AetherIIItems.TAEGORE_HIDE_HELMET.get(),
