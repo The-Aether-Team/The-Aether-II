fb9d327d2af03871622cb75f2dc1e90d7ac81833
Builderdog <timon.kempter@gmail.com>
2025-03-21 21:49:08 +0100

    feat: Proper Generation Checks for buried structures

--- src/main/java/com/aetherteam/aetherii/world/structure/AetherJigsawStructure.java
+++ src/main/java/com/aetherteam/aetherii/world/structure/AetherJigsawStructure.java
@@ -7,10 +7,14 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.NoiseColumn;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.WorldGenerationContext;
+import net.minecraft.world.level.levelgen.heightproviders.ConstantHeight;
 import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
@@ -37,6 +41,7 @@ public class AetherJigsawStructure extends Structure {
                     Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
                     Codec.intRange(-4096, 4096).fieldOf("discard_below_y").forGetter(structure -> structure.discardBelowY),
                     Codec.intRange(-4096, 4096).fieldOf("discard_above_y").forGetter(structure -> structure.discardAboveY),
+                    Codec.BOOL.fieldOf("buried").forGetter(structure -> structure.buried),
                     Codec.list(PoolAliasBinding.CODEC).optionalFieldOf("pool_aliases", List.of()).forGetter(structure -> structure.poolAliases),
                     DimensionPadding.CODEC.optionalFieldOf("dimension_padding", DimensionPadding.ZERO).forGetter(structure -> structure.dimensionPadding),
                     LiquidSettings.CODEC.optionalFieldOf("liquid_settings", LiquidSettings.APPLY_WATERLOGGING).forGetter(structure -> structure.liquidSettings)
@@ -49,11 +54,12 @@ public class AetherJigsawStructure extends Structure {
     private final int maxDistanceFromCenter;
     private final int discardBelowY;
     private final int discardAboveY;
+    private final boolean buried;
     private final List<PoolAliasBinding> poolAliases;
     private final DimensionPadding dimensionPadding;
     private final LiquidSettings liquidSettings;
 
-    public AetherJigsawStructure(StructureSettings config, Holder<StructureTemplatePool> startPool, Optional<ResourceLocation> startJigsawName, int size, HeightProvider startHeight, Optional<Heightmap.Types> projectStartToHeightmap, int maxDistanceFromCenter, int discardBelowY, int discardAboveY, List<PoolAliasBinding> poolAliases, DimensionPadding dimensionPadding, LiquidSettings liquidSettings) {
+    public AetherJigsawStructure(StructureSettings config, Holder<StructureTemplatePool> startPool, Optional<ResourceLocation> startJigsawName, int size, HeightProvider startHeight, Optional<Heightmap.Types> projectStartToHeightmap, int maxDistanceFromCenter, int discardBelowY, int discardAboveY, boolean buried, List<PoolAliasBinding> poolAliases, DimensionPadding dimensionPadding, LiquidSettings liquidSettings) {
         super(config);
         this.startPool = startPool;
         this.startJigsawName = startJigsawName;
@@ -63,6 +69,7 @@ public class AetherJigsawStructure extends Structure {
         this.maxDistanceFromCenter = maxDistanceFromCenter;
         this.discardBelowY = discardBelowY;
         this.discardAboveY = discardAboveY;
+        this.buried = buried;
         this.poolAliases = poolAliases;
         this.dimensionPadding = dimensionPadding;
         this.liquidSettings = liquidSettings;
@@ -70,12 +77,14 @@ public class AetherJigsawStructure extends Structure {
 
     @Override
     public @NotNull Optional<GenerationStub> findGenerationPoint(@NotNull GenerationContext context) {
-        if (!this.checkHeight(context, discardBelowY, discardAboveY)) {
-            return Optional.empty();
-        }
-        int startY = startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
+        ChunkGenerator generator = context.chunkGenerator();
+        LevelHeightAccessor heightAccessor = context.heightAccessor();
+        int startY = startHeight.sample(context.random(), new WorldGenerationContext(generator, heightAccessor));
         ChunkPos chunkPos = context.chunkPos();
         BlockPos pos = new BlockPos(chunkPos.getMiddleBlockX(), startY, chunkPos.getMiddleBlockZ());
+        if (!this.checkHeight(context, discardBelowY, discardAboveY) || (checkCorners(generator, heightAccessor, chunkPos, context.randomState(), startY) && buried)) {
+            return Optional.empty();
+        }
         return JigsawPlacement.addPieces(
                 context,
                 startPool,
@@ -97,8 +106,23 @@ public class AetherJigsawStructure extends Structure {
         return posTest > minY && posTest < maxY;
     }
 
-    public boolean checkBuried(Structure.GenerationContext context, BlockPos pos) {
-        return context.chunkGenerator().getBaseColumn(pos.getY() - 24, pos.getY(), context.heightAccessor(), context.randomState()).getBlock(pos.getY() - 24).is(Blocks.AIR) ;
+    private static boolean checkCorners(ChunkGenerator generator, LevelHeightAccessor heightAccessor, ChunkPos chunkPos, RandomState random, int startY) {
+        int minX = chunkPos.getMinBlockX() - 1;
+        int minZ = chunkPos.getMinBlockZ() - 1;
+        int maxX = chunkPos.getMaxBlockX() + 1;
+        int maxZ = chunkPos.getMaxBlockZ() + 1;
+        NoiseColumn[] columns = {
+                generator.getBaseColumn(minX, minZ, heightAccessor, random),
+                generator.getBaseColumn(minX, maxZ, heightAccessor, random),
+                generator.getBaseColumn(maxX, minZ, heightAccessor, random),
+                generator.getBaseColumn(maxX, maxZ, heightAccessor, random)
+        };
+        for (NoiseColumn column : columns) {
+            if (!column.getBlock(startY - 20).isAir()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
