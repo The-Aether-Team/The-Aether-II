44a59131a866ade54c92b2e71b835f7b2f1cb76e
Builderdog <timon.kempter@gmail.com>
2025-03-26 20:49:06 +0100

    feat: Basic Entrance Corners

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/InfectedGuardianTreePools.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/InfectedGuardianTreePools.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 public class InfectedGuardianTreePools {
     public static final ResourceKey<StructureTemplatePool> ENTRANCE = AetherIIPools.createKey("infected_guardian_tree/entrance");
     public static final ResourceKey<StructureTemplatePool> ENTRANCE_WALLS = AetherIIPools.createKey("infected_guardian_tree/entrance_walls");
+    public static final ResourceKey<StructureTemplatePool> ENTRANCE_CORNERS = AetherIIPools.createKey("infected_guardian_tree/entrance_corners");
     public static final ResourceKey<StructureTemplatePool> START_ROOM = AetherIIPools.createKey("infected_guardian_tree/start_room");
 
     public static void bootstrap(BootstrapContext<StructureTemplatePool> context) {
@@ -31,6 +32,14 @@ public class InfectedGuardianTreePools {
                 fallback,
                 ImmutableList.of(
                         Pair.of(AetherIIPools.aetherPoolBuried("infected_guardian_tree/entrance/entrance_wall_01"), 1)
+                ),
+                StructureTemplatePool.Projection.RIGID)
+        );
+
+        context.register(ENTRANCE_CORNERS, new StructureTemplatePool(
+                fallback,
+                ImmutableList.of(
+                        Pair.of(AetherIIPools.aetherPoolBuried("infected_guardian_tree/entrance/entrance_corner_01"), 1)
                 ),
                 StructureTemplatePool.Projection.RIGID)
         );
