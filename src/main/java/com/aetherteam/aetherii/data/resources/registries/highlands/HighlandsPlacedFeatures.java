58e8bc58b030ab2697dc459bc12b7e79978d0264
Builderdog841 <timon.kempter@gmail.com>
2025-05-20 11:41:13 +0200

    feat: Basic Rotshroom Patch Placed Feature

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/highlands/HighlandsPlacedFeatures.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/highlands/HighlandsPlacedFeatures.java
@@ -177,6 +177,7 @@ public class HighlandsPlacedFeatures {
     // Dungeon
     public static final ResourceKey<PlacedFeature> LARGE_SHELF_ROTSHROOM = createKey("large_shelf_rotshroom");
     public static final ResourceKey<PlacedFeature> LARGE_SHELF_ROTSHROOM_UNDERGROUND = createKey("large_shelf_rotshroom_underground");
+    public static final ResourceKey<PlacedFeature> ROTSHROOM_PATCH = createKey("rotshroom_patch");
 
 
     // Air
@@ -812,6 +813,7 @@ public class HighlandsPlacedFeatures {
 
         register(context, LARGE_SHELF_ROTSHROOM, configuredFeatures.getOrThrow(HighlandsConfiguredFeatures.LARGE_SHELF_ROTSHROOM));
         register(context, LARGE_SHELF_ROTSHROOM_UNDERGROUND, configuredFeatures.getOrThrow(HighlandsConfiguredFeatures.LARGE_SHELF_ROTSHROOM_UNDERGROUND));
+        register(context, ROTSHROOM_PATCH, configuredFeatures.getOrThrow(HighlandsConfiguredFeatures.ROTSHROOM_PATCH));
     }
 
     public static void bootstrapAir(BootstrapContext<PlacedFeature> context) {
