70c6f6e3fbc0399d71b7fbbdda9abe28ad2e2ada
Builderdog <timon.kempter@gmail.com>
2025-05-29 15:38:52 +0200

    improv: Further Generation Tweaks

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructures.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructures.java
@@ -49,6 +49,6 @@ public class AetherIIStructures {
 
         context.register(INFECTED_GUARDIAN_TREE, new AetherJigsawStructure(
                 AetherIIStructureBuilders.structure(biomes.getOrThrow(AetherIITags.Biomes.HAS_STRUCTURE_INFECTED_GUARDIAN_TREE), GenerationStep.Decoration.SURFACE_STRUCTURES, TerrainAdjustment.NONE),
-                templatePools.getOrThrow(InfectedGuardianTreePools.TRUNK_BASE), Optional.empty(), 20, ConstantHeight.of(VerticalAnchor.absolute(-3)), Optional.of(Heightmap.Types.WORLD_SURFACE_WG), 96, 128, 156, List.of(), new DimensionPadding(64, 0), LiquidSettings.IGNORE_WATERLOGGING));
+                templatePools.getOrThrow(InfectedGuardianTreePools.TRUNK_BASE), Optional.empty(), 20, ConstantHeight.of(VerticalAnchor.absolute(-3)), Optional.of(Heightmap.Types.WORLD_SURFACE_WG), 72, 128, 156, List.of(), new DimensionPadding(96, 0), LiquidSettings.IGNORE_WATERLOGGING));
     }
 }
