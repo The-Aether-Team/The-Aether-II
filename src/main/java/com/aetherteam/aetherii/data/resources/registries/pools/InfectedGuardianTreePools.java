dbcaf34e8885682c24d41b71d76d8843af8ba444
Builderdog <timon.kempter@gmail.com>
2025-10-08 14:08:35 +0200

    feat: Implemented further layout variation

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/InfectedGuardianTreePools.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/InfectedGuardianTreePools.java
@@ -248,6 +248,7 @@ public class InfectedGuardianTreePools {
                 ImmutableList.of(
                         Pair.of(AetherIIPools.aetherPoolUnderground("infected_guardian_tree/layouts/paths/large/2_eyes/path_01_a"), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("infected_guardian_tree/layouts/paths/large/2_eyes/path_01_b"), 1),
+                        Pair.of(AetherIIPools.aetherPoolUnderground("infected_guardian_tree/layouts/paths/large/2_eyes/path_01_c"), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("infected_guardian_tree/layouts/paths/large/2_eyes/path_01_a_m"), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("infected_guardian_tree/layouts/paths/large/2_eyes/path_02_a"), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("infected_guardian_tree/layouts/paths/large/2_eyes/path_02_a_m"), 1)
