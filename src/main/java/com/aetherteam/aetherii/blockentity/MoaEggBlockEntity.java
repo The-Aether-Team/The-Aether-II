4aa7ca0055e28daa32766168e32f37d55f1b5a2f
Raptor <39534148+raptor494@users.noreply.github.com>
2025-10-14 14:51:51 -0500

    improv: Moa death messages (#592)
    Moas send death messages to the person who last rode them or the person
    who placed their egg (if applicable)

--- src/main/java/com/aetherteam/aetherii/blockentity/MoaEggBlockEntity.java
+++ src/main/java/com/aetherteam/aetherii/blockentity/MoaEggBlockEntity.java
@@ -1,19 +1,45 @@
 package com.aetherteam.aetherii.blockentity;
 
+import javax.annotation.Nullable;
+
 import com.aetherteam.aetherii.block.miscellaneous.MoaEggBlock;
 import com.aetherteam.aetherii.item.components.AetherIIDataComponents;
 import com.aetherteam.aetherii.item.components.MoaEggType;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityReference;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 public class MoaEggBlockEntity extends BlockEntity {
+    @Nullable
+    private EntityReference<Player> placedBy;
     public int tickCount;
 
     public MoaEggBlockEntity(BlockPos pos, BlockState state) {
         super(AetherIIBlockEntityTypes.MOA_EGG.get(), pos, state);
+    }
+
+    @Nullable
+    public EntityReference<Player> getPlacedBy() {
+        return placedBy;
+    }
+
+    public void setPlacedBy(@Nullable EntityReference<Player> placedBy) {
+        this.placedBy = placedBy;
+        this.setChanged();
+    }
+
+    public void setPlacedBy(@Nullable Player placedBy) {
+        this.placedBy = placedBy == null? null : new EntityReference<>(placedBy);
+        this.setChanged();
     }
 
     public static void tick(Level level, BlockPos blockPos, BlockState blockState, MoaEggBlockEntity moaEggBlockEntity) {
@@ -23,6 +49,21 @@ public class MoaEggBlockEntity extends BlockEntity {
     @Override
     protected void collectImplicitComponents(DataComponentMap.Builder components) {
         super.collectImplicitComponents(components);
-        components.set(AetherIIDataComponents.MOA_EGG_TYPE, new MoaEggType(this.getBlockState().getValue(MoaEggBlock.KERATIN), this.getBlockState().getValue(MoaEggBlock.EYES), this.getBlockState().getValue(MoaEggBlock.FEATHERS), this.getBlockState().getValue(MoaEggBlock.FEATHER_SHAPE)));
+        var state = this.getBlockState();
+        components.set(AetherIIDataComponents.MOA_EGG_TYPE, new MoaEggType(state.getValue(MoaEggBlock.KERATIN), state.getValue(MoaEggBlock.EYES), state.getValue(MoaEggBlock.FEATHERS), this.getBlockState().getValue(MoaEggBlock.FEATHER_SHAPE)));
+    }
+
+    @Override
+    protected void loadAdditional(ValueInput input) {
+        super.loadAdditional(input);
+        this.placedBy = input.read("PlacedBy", EntityReference.<Player>codec()).orElse(null);
+    }
+
+    @Override
+    protected void saveAdditional(ValueOutput output) {
+        super.saveAdditional(output);
+        if (this.placedBy != null) {
+            this.placedBy.store(output, "PlacedBy");
+        }
     }
 }
