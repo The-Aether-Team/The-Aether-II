ed794ef28c0ef4f26a209c67c14ce3a37a8da864
Builderdog <81527321+Builderdog841@users.noreply.github.com>
2025-10-18 23:15:51 +0200

    feat: implemented 4 new music tracks by sunsette (#598)

--- src/main/java/com/aetherteam/aetherii/data/generators/AetherIISoundData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/AetherIISoundData.java
@@ -1017,13 +1017,21 @@ public class AetherIISoundData extends SoundDefinitionsProvider {
                         sound("aether_ii:music/aether3").stream(),
                         sound("aether_ii:music/aether4").stream(),
                         sound("aether_ii:music/aether5").stream(),
-                        sound("aether_ii:music/aether6").stream()
+                        sound("aether_ii:music/aether6").stream(),
+                        sound("aether_ii:music/polarity").stream(),
+                        sound("aether_ii:music/sunbeam").stream(),
+                        sound("aether_ii:music/thin_air").stream(),
+                        sound("aether_ii:music/walking_dream").stream()
                 )
         );
         this.add(AetherIISoundEvents.MUSIC_AETHER_NIGHT,
                 definition().with(
                         sound("aether_ii:music/aether_night1").stream(),
-                        sound("aether_ii:music/aether_night2").stream()
+                        sound("aether_ii:music/aether_night2").stream(),
+                        sound("aether_ii:music/polarity").stream(),
+                        sound("aether_ii:music/sunbeam").stream(),
+                        sound("aether_ii:music/thin_air").stream(),
+                        sound("aether_ii:music/walking_dream").stream()
                 )
         );
         this.add(AetherIISoundEvents.MUSIC_AETHER_SUNRISE,
