e82a87510aa8a11ad08a34a12fafb4e1e5ade900
bconlon <bconlon.gml@gmail.com>
2025-10-19 20:26:29 -0700

    fix: server crashes

---
+++ src/main/java/com/aetherteam/aetherii/client/AetherIIClientProxy.java
@@ -0,0 +1,30 @@
+package com.aetherteam.aetherii.client;
+
+import com.aetherteam.aetherii.client.renderer.level.HighlandsSpecialEffects;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class AetherIIClientProxy {
+    public static boolean isHighlandsSpecialEffects(Level level) {
+        return level instanceof ClientLevel clientLevel && clientLevel.effects() instanceof HighlandsSpecialEffects;
+    }
+
+    public static Player getClientPlayer() {
+        return Minecraft.getInstance().player;
+    }
+
+    @Nullable
+    public static <T> T getClientPlayerData(Supplier<AttachmentType<T>> holder) {
+        if (Minecraft.getInstance().player != null) {
+            return Minecraft.getInstance().player.getData(holder);
+        } else {
+            return null;
+        }
+    }
+}
