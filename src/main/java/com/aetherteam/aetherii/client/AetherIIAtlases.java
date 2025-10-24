7317ed13b2cbfe7e1289d25d1204a80a619c04d5
Raptor4694 <raptor4694@gmail.com>
2025-10-23 20:19:11 -0500

    Alkahest Purifier texture optimizations
    Alkahest purifier textures are no longer added to the vanilla blocks atlas. They have their own atlas now.

--- src/main/java/com/aetherteam/aetherii/client/AetherIIAtlases.java
+++ src/main/java/com/aetherteam/aetherii/client/AetherIIAtlases.java
@@ -1,8 +1,8 @@
 package com.aetherteam.aetherii.client;
 
 import com.aetherteam.aetherii.AetherII;
-import com.aetherteam.aetherii.client.renderer.entity.MoaRenderer;
-import com.aetherteam.aetherii.item.components.ArmorStyle;
+
+import net.minecraft.client.renderer.MaterialMapper;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
@@ -13,25 +13,19 @@ public class AetherIIAtlases {
     public static final ResourceLocation MOA_EYES_SHEET = ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "textures/atlas/moa_eyes.png");
     public static final ResourceLocation MOA_KERATIN_SHEET = ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "textures/atlas/moa_keratin.png");
     public static final ResourceLocation ARMOR_STYLES_SHEET = ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "textures/atlas/armor_styles.png");
+    public static final ResourceLocation ALKAHEST_PURIFIER_SHEET = ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "textures/atlas/alkahest_purifier.png");
 
-    public static Material SKYROOT_CHEST_MATERIAL;
-    public static Material SKYROOT_CHEST_LEFT_MATERIAL;
-    public static Material SKYROOT_CHEST_RIGHT_MATERIAL;
+    public static final MaterialMapper ALKAHEST_PURIFIER_MAPPER = new MaterialMapper(ALKAHEST_PURIFIER_SHEET, "entity/alkahest_purifier");
 
-    public static void registerSkyrootChestAtlases() {
-        SKYROOT_CHEST_MATERIAL = getChestMaterial("skyroot_chest");
-        SKYROOT_CHEST_LEFT_MATERIAL = getChestMaterial("skyroot_chest_left");
-        SKYROOT_CHEST_RIGHT_MATERIAL = getChestMaterial("skyroot_chest_right");
-    }
-
-    public static Material getChestMaterial(String chestName) {
-        return new Material(Sheets.CHEST_SHEET, ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "entity/chest/" + chestName));
-    }
+    public static final Material SKYROOT_CHEST_MATERIAL = Sheets.CHEST_MAPPER.apply(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "skyroot_chest"));
+    public static final Material SKYROOT_CHEST_LEFT_MATERIAL = Sheets.CHEST_MAPPER.apply(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "skyroot_chest_left"));
+    public static final Material SKYROOT_CHEST_RIGHT_MATERIAL = Sheets.CHEST_MAPPER.apply(ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "skyroot_chest_right"));
 
     public static void registerAtlases(RegisterMaterialAtlasesEvent event) {
         event.register(MOA_FEATHER_SHEET, ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "moa_feather"));
         event.register(MOA_EYES_SHEET, ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "moa_eyes"));
         event.register(MOA_KERATIN_SHEET, ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "moa_keratin"));
         event.register(ARMOR_STYLES_SHEET, ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "armor_styles"));
+        event.register(ALKAHEST_PURIFIER_SHEET, ResourceLocation.fromNamespaceAndPath(AetherII.MODID, "alkahest_purifier"));
     }
 }
