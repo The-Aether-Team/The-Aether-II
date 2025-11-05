c931fc01176003bfb4473da29a7b154bcf3990f6
bconlon <bconlon.gml@gmail.com>
2025-11-05 14:08:56 -0800

    fix: noticeable shield model seam transitions

--- src/main/java/com/aetherteam/aetherii/client/renderer/item/model/ShieldModel.java
+++ src/main/java/com/aetherteam/aetherii/client/renderer/item/model/ShieldModel.java
@@ -70,7 +70,7 @@ public class ShieldModel implements ItemModel {
     public List<BakedQuad> faceElement(TextureAtlasSprite sprite, float xOffset, float yOffset, boolean front) {
         Vector3f scale = front ? new Vector3f(1, 1, 1) : new Vector3f(0.9999F, 0.9999F, 0.9999F);
         return UnbakedElementsHelper.bakeElements(UnbakedElementsHelper.createUnbakedItemElements(0, sprite), $ -> sprite,
-                new ComposedModelState(BlockModelRotation.X0_Y0, new Transformation(new Vector3f(px(xOffset) + px(3.0F), px(yOffset) - px(0.5F), (0.001F * (front ? 1.0F : -1.0F)) + px(3.5F)), new Quaternionf(), scale, new Quaternionf())));
+                new ComposedModelState(BlockModelRotation.X0_Y0, new Transformation(new Vector3f(px(xOffset) + px(3.0F), px(yOffset) - px(0.5F), (0.0001F * (front ? 1.0F : -1.0F)) + px(3.5F)), new Quaternionf(), scale, new Quaternionf())));
     }
 
     public static float px(float offset) {
