a38104033300ac74ce564ce419a85cff9ca15cff
bconlon <bconlon.gml@gmail.com>
2025-11-07 15:25:39 -0800

    refactor: reduce generation rate of ferrosite spikes

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/highlands/HighlandsPlacedFeatures.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/highlands/HighlandsPlacedFeatures.java
@@ -822,7 +822,7 @@ public class HighlandsPlacedFeatures {
         );
 
         register(context, FERROSITE_SPIKE, configuredFeatures.getOrThrow(HighlandsConfiguredFeatures.FERROSITE_SPIKE),
-                CountPlacement.of(18),
+                CountPlacement.of(12),
                 HeightRangePlacement.uniform(VerticalAnchor.absolute(112), VerticalAnchor.absolute(256)),
                 BlockPredicateFilter.forPredicate(BlockPredicate.matchesTag(new BlockPos(0, -1, 0), AetherIITags.Blocks.FERROSITE_SPIKE_GENERATES_ON)),
                 PlacementUtils.HEIGHTMAP_OCEAN_FLOOR,
