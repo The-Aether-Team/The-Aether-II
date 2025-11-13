75867651b66f12fd2661efca8a1b484a6711a209
Builderdog <timon.kempter@gmail.com>
2025-11-13 22:23:15 +0100

    feat: Inert Mineral Blocks

--- src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIBlockTagData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIBlockTagData.java
@@ -713,6 +713,8 @@ public class AetherIIBlockTagData extends BlockTagsProvider {
                 AetherIIBlocks.SCATTERGLASS_PANE.get(),
                 AetherIIBlocks.SKYROOT_FRAMED_SCATTERGLASS_PANE.get(),
                 AetherIIBlocks.ARKENIUM_FRAMED_SCATTERGLASS_PANE.get(),
+                AetherIIBlocks.INERT_ARKENIUM_BLOCK.get(),
+                AetherIIBlocks.INERT_GRAVITITE_BLOCK.get(),
                 AetherIIBlocks.AMBROSIUM_BLOCK.get(),
                 AetherIIBlocks.ZANITE_BLOCK.get(),
                 AetherIIBlocks.GRAVITITE_BLOCK.get(),
@@ -947,6 +949,8 @@ public class AetherIIBlockTagData extends BlockTagsProvider {
                 AetherIIBlocks.UNDERSHALE_GLINT_ORE.get(),
                 AetherIIBlocks.UNDERSHALE_ARKENIUM_ORE.get(),
                 AetherIIBlocks.UNDERSHALE_GRAVITITE_ORE.get(),
+                AetherIIBlocks.INERT_ARKENIUM_BLOCK.get(),
+                AetherIIBlocks.INERT_GRAVITITE_BLOCK.get(),
                 AetherIIBlocks.ARKENIUM_BLOCK.get(),
                 AetherIIBlocks.GRAVITITE_BLOCK.get()
         );
@@ -1065,6 +1069,8 @@ public class AetherIIBlockTagData extends BlockTagsProvider {
         );
         this.tag(Tags.Blocks.STONES).addTag(AetherIITags.Blocks.HOLYSTONE).add(AetherIIBlocks.UNDERSHALE.get());
         this.tag(Tags.Blocks.STORAGE_BLOCKS).add(
+                AetherIIBlocks.INERT_ARKENIUM_BLOCK.get(),
+                AetherIIBlocks.INERT_GRAVITITE_BLOCK.get(),
                 AetherIIBlocks.AMBROSIUM_BLOCK.get(),
                 AetherIIBlocks.ZANITE_BLOCK.get(),
                 AetherIIBlocks.GRAVITITE_BLOCK.get(),
