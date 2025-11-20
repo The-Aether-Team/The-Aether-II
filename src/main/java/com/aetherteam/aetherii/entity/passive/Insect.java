d68f71c3b379e5e34f091a6b01ca2e352c099dd3
baguchan <shutux500@gmail.com>
2025-11-21 08:38:08 +0900

    flying like real life insect

--- src/main/java/com/aetherteam/aetherii/entity/passive/Insect.java
+++ src/main/java/com/aetherteam/aetherii/entity/passive/Insect.java
@@ -1,20 +1,24 @@
 package com.aetherteam.aetherii.entity.passive;
 
-import com.aetherteam.aetherii.entity.AetherIIEntityTypes;
+import com.aetherteam.aetherii.entity.ai.controller.FlyingMoveControl;
+import com.aetherteam.aetherii.entity.ai.goal.FlyingLookGoal;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.MoveControl;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ambient.AmbientCreature;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
+import java.util.EnumSet;
 
 public class Insect extends AmbientCreature {
     @Nullable
@@ -22,43 +26,23 @@ public class Insect extends AmbientCreature {
 
     public Insect(EntityType<? extends Insect> entityType, Level level) {
         super(entityType, level);
+        this.moveControl = new FlyingMoveControl(this);
     }
 
     public static AttributeSupplier.Builder createMobAttributes() {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 6.0F);
     }
 
-    public void tick() {
-        super.tick();
-        this.setDeltaMovement(this.getDeltaMovement().multiply(1.0F, 0.6, 1.0F));
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(5, new RandomFloatAroundGoal(this));
+        this.goalSelector.addGoal(6, new FlyingLookGoal(this));
     }
 
-    protected void customServerAiStep(ServerLevel p_376388_) {
-        super.customServerAiStep(p_376388_);
-        BlockPos blockpos = this.blockPosition();
-        BlockPos blockpos1 = blockpos.above();
-        if (this.targetPosition != null && (!p_376388_.isEmptyBlock(this.targetPosition) || this.targetPosition.getY() <= p_376388_.getMinY())) {
-            this.targetPosition = null;
-        }
-
-        if (this.targetPosition == null || this.random.nextInt(30) == 0 || this.targetPosition.closerToCenterThan(this.position(), (double)2.0F)) {
-            this.targetPosition = BlockPos.containing(this.getX() + (double)this.random.nextInt(7) - (double)this.random.nextInt(7), this.getY() + (double)this.random.nextInt(6) - (double)2.0F, this.getZ() + (double)this.random.nextInt(7) - (double)this.random.nextInt(7));
-        }
-
-        double d2 = (double)this.targetPosition.getX() + (double)0.5F - this.getX();
-        double d0 = (double)this.targetPosition.getY() + 0.1 - this.getY();
-        double d1 = (double)this.targetPosition.getZ() + (double)0.5F - this.getZ();
-        Vec3 vec3 = this.getDeltaMovement();
-        Vec3 vec31 = vec3.add((Math.signum(d2) * (double)0.5F - vec3.x) * (double)0.1F, (Math.signum(d0) * (double)0.7F - vec3.y) * (double)0.1F, (Math.signum(d1) * (double)0.5F - vec3.z) * (double)0.1F);
-        this.setDeltaMovement(vec31);
-        float f = (float)(Mth.atan2(vec31.z, vec31.x) * (double)180.0F / (double)(float)Math.PI) - 90.0F;
-        float f1 = Mth.wrapDegrees(f - this.getYRot());
-        this.zza = 0.5F;
-        this.setYRot(this.getYRot() + f1);
-//        if (this.random.nextInt(100) == 0 && p_376388_.getBlockState(blockpos1).isRedstoneConductor(p_376388_, blockpos1)) {
-//            this.setResting(true);
-//        }
-
+    @Override
+    public void travel(Vec3 p_415638_) {
+        this.travelFlying(p_415638_, 0.02F);
     }
 
     protected Entity.MovementEmission getMovementEmission() {
@@ -80,5 +64,52 @@ public class Insect extends AmbientCreature {
     }
 
     protected void pushEntities() {
+    }
+
+    /**
+     * [CODE COPY] - {@link net.minecraft.world.entity.monster.Ghast.RandomFloatAroundGoal}.
+     */
+    public static class RandomFloatAroundGoal extends Goal {
+        private final Insect insect;
+
+        public RandomFloatAroundGoal(Insect insect) {
+            this.insect = insect;
+            this.setFlags(EnumSet.of(Flag.MOVE));
+        }
+
+        @Override
+        public boolean canUse() {
+            MoveControl moveControl = this.insect.getMoveControl();
+            if (!moveControl.hasWanted()) {
+                return true;
+            } else {
+                double d0 = moveControl.getWantedX() - this.insect.getX();
+                double d1 = moveControl.getWantedY() - this.insect.getY();
+                double d2 = moveControl.getWantedZ() - this.insect.getZ();
+                double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+                return d3 < 1.0 || d3 > 3600.0;
+            }
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            return false;
+        }
+
+        @Override
+        public void start() {
+            LivingEntity target = this.insect.getTarget();
+            RandomSource random = this.insect.getRandom();
+            double d0 = this.insect.getX() + (random.nextFloat() * 2.0F - 1.0F) * 4.0F;
+            double d1 = this.insect.getY() + (random.nextFloat() * 2.0F - 1.0F) * 4.0F;
+            double d2 = this.insect.getZ() + (random.nextFloat() * 2.0F - 1.0F) * 4.0F;
+            this.insect.getMoveControl().setWantedPosition(d0, d1, d2, 1.0);
+
+        }
+
+        @Override
+        public boolean requiresUpdateEveryTick() {
+            return true;
+        }
     }
 }
