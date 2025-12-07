af74b22c5ad187011ba07abf26355f1bff5b29df
Builderdog <timon.kempter@gmail.com>
2025-12-07 18:29:17 +0100

    improv: dimmer sentry blocks

--- src/main/java/com/aetherteam/aetherii/block/AetherIIBlocks.java
+++ src/main/java/com/aetherteam/aetherii/block/AetherIIBlocks.java
@@ -466,14 +466,14 @@ public class AetherIIBlocks extends AetherIIBlockBuilders {
     public static final DeferredBlock<FacingPillarBlock> UNDERSHALE_PILLAR = register("undershale_pillar", FacingPillarBlock::new, () -> Block.Properties.ofFullCopy(UNDERSHALE_BRICKS.get()));
 
     // Sentry Bricks
-    public static final DeferredBlock<Block> SENTRY_BRICKS = register("sentry_bricks", SentryBlock::new, () -> Block.Properties.of().mapColor(MapColor.COLOR_LIGHT_GRAY).instrument(NoteBlockInstrument.BASEDRUM).strength(4.0F, 6.0F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops().isRedstoneConductor(AetherIIBlockBuilders::never).lightLevel(AetherIIBlockBuilders::lightLevel11));
+    public static final DeferredBlock<Block> SENTRY_BRICKS = register("sentry_bricks", SentryBlock::new, () -> Block.Properties.of().mapColor(MapColor.COLOR_LIGHT_GRAY).instrument(NoteBlockInstrument.BASEDRUM).strength(4.0F, 6.0F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops().isRedstoneConductor(AetherIIBlockBuilders::never).lightLevel(AetherIIBlockBuilders::lightLevel6));
     public static final DeferredBlock<StairBlock> SENTRY_BRICK_STAIRS = register("sentry_brick_stairs", (properties) -> new SentryStairBlock(SENTRY_BRICKS.get().defaultBlockState(), properties), () -> Block.Properties.ofFullCopy(AetherIIBlocks.SENTRY_BRICKS.get()));
     public static final DeferredBlock<SlabBlock> SENTRY_BRICK_SLAB = register("sentry_brick_slab", SentrySlabBlock::new, () -> Block.Properties.ofFullCopy(AetherIIBlocks.SENTRY_BRICKS.get()));
     public static final DeferredBlock<WallBlock> SENTRY_BRICK_WALL = register("sentry_brick_wall", SentryWallBlock::new, () -> Block.Properties.ofFullCopy(AetherIIBlocks.SENTRY_BRICKS.get()).forceSolidOn());
     public static final DeferredBlock<ButtonBlock> SENTRY_BUTTON = register("sentry_button", (properties) -> new ButtonBlock(BlockSetType.STONE, 20, properties), () -> Block.Properties.ofFullCopy(Blocks.STONE_BUTTON).sound(SoundType.DEEPSLATE));
 
     // Sentry Decorative Blocks
-    public static final DeferredBlock<Block> SENTRY_LIGHTSTONE = register("sentry_lightstone", SentryBlock::new, () -> Block.Properties.ofFullCopy(SENTRY_BRICKS.get()));
+    public static final DeferredBlock<Block> SENTRY_LIGHTSTONE = register("sentry_lightstone", SentryBlock::new, () -> Block.Properties.ofFullCopy(SENTRY_BRICKS.get()).lightLevel(AetherIIBlockBuilders::lightLevel11));
     public static final DeferredBlock<Block> SENTRY_FLAGSTONES = register("sentry_flagstones", SentryBlock::new, () -> Block.Properties.ofFullCopy(SENTRY_BRICKS.get()));
     public static final DeferredBlock<Block> SENTRY_TILE = register("sentry_tile", SentryBlock::new, () -> Block.Properties.ofFullCopy(SENTRY_BRICKS.get()));
     public static final DeferredBlock<Block> SENTRY_BASE_BRICKS = register("sentry_base_bricks", SentryBlock::new, () -> Block.Properties.ofFullCopy(SENTRY_BRICKS.get()));
