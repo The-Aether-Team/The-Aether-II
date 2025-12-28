2fa3d1d9c4f9dabc61cdc7dfd1364a5da0dd22e5
bconlon <bconlon.gml@gmail.com>
2025-12-28 13:53:57 -0800

    fix: slider not dealing damage

--- src/main/java/com/aetherteam/aetherii/entity/ai/goal/boss/CollideGoal.java
+++ src/main/java/com/aetherteam/aetherii/entity/ai/goal/boss/CollideGoal.java
@@ -36,7 +36,7 @@ public class CollideGoal extends Goal {
         if (this.slider.level() instanceof ServerLevel serverLevel) {
             for (Entity entity : this.slider.level().getEntities(this.slider, collisionBounds)) {
                 DamageSource damageSource = AetherIIDamageTypes.entityDamageSource(this.slider.level(), AetherIIDamageTypes.CRUSH, this.slider);
-                if (entity instanceof LivingEntity livingEntity && livingEntity.isInvulnerableTo(serverLevel, damageSource) && entity.hurtServer(serverLevel, damageSource, 6)) {
+                if (entity instanceof LivingEntity livingEntity && !livingEntity.isInvulnerableTo(serverLevel, damageSource) && entity.hurtServer(serverLevel, damageSource, 6)) {
                     if (livingEntity instanceof Player player && player.getUseItem().is(Items.SHIELD) && player.isBlocking()) { // Disables the player's Shield if one is being used. //TODO CHECK NEW SHIELDS
                         player.getCooldowns().addCooldown(Items.SHIELD.getDefaultInstance(), 100);
                         player.stopUsingItem();
