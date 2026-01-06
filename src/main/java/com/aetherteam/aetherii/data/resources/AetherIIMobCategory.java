3451b7ef25aae796226ccccbd4be1037aedc098f
Builderdog <timon.kempter@gmail.com>
2026-01-06 17:51:52 +0100

    improv: increased glitterwing and shroudwing spawn cap

--- src/main/java/com/aetherteam/aetherii/data/resources/AetherIIMobCategory.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/AetherIIMobCategory.java
@@ -5,5 +5,6 @@ import net.minecraft.world.entity.MobCategory;
 public class AetherIIMobCategory {
     public static final MobCategory AETHER_SURFACE_MONSTER = MobCategory.valueOf("AETHER_II_AETHER_SURFACE_MONSTER");
     public static final MobCategory AETHER_SKY_MONSTER = MobCategory.valueOf("AETHER_II_AETHER_SKY_MONSTER");
+    public static final MobCategory AETHER_AMBIENT = MobCategory.valueOf("AETHER_II_AETHER_AMBIENT");
     public static final MobCategory AETHER_AERWHALE = MobCategory.valueOf("AETHER_II_AETHER_AERWHALE");
 }
