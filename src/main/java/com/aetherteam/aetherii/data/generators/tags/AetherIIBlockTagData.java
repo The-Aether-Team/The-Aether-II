760adca0cbf48f5ee224ae91da842e30567027a9
bconlon <bconlon.gml@gmail.com>
2026-01-19 18:50:31 -0800

    fix: disallow sentry ruins rooms from thinking aercloud is a valid solid

--- src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIBlockTagData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/tags/AetherIIBlockTagData.java
@@ -311,7 +311,7 @@ public class AetherIIBlockTagData extends BlockTagsProvider {
         this.tag(AetherIITags.Blocks.HOVERING_BLOCK_REPLACE_BLACKLIST).addTags(
                 BlockTags.PORTALS
         );
-        this.tag(AetherIITags.Blocks.NON_SENTRY_RUINS_SPAWNABLE).add(
+        this.tag(AetherIITags.Blocks.NON_SENTRY_RUINS_SPAWNABLE).addTag(AetherIITags.Blocks.AERCLOUDS).add(
                 Blocks.WATER);
         this.tag(AetherIITags.Blocks.NON_TUNNEL_REPLACEABLE).add(
                 Blocks.AIR,
