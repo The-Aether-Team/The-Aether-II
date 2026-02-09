70f2845cfccd3bebeaa174b0b8a7d031f457d436
bconlon <bconlon.gml@gmail.com>
2026-02-08 19:38:53 -0800

    chore: clean import

--- src/main/java/com/aetherteam/aetherii/client/gui/screen/inventory/AmberHourglassScreen.java
+++ src/main/java/com/aetherteam/aetherii/client/gui/screen/inventory/AmberHourglassScreen.java
@@ -15,7 +15,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.AbstractFurnaceMenu;
 import net.minecraft.world.item.Items;
 
 import java.util.List;
