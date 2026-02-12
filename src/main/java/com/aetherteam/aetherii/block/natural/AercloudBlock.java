2ac8f5298994729bc57d4d7fe3eab97ce12fe9a7
bconlon <bconlon.gml@gmail.com>
2026-02-12 11:56:50 -0800

    improv: disallow placing blocks on aercloud

--- src/main/java/com/aetherteam/aetherii/block/natural/AercloudBlock.java
+++ src/main/java/com/aetherteam/aetherii/block/natural/AercloudBlock.java
@@ -1,12 +1,16 @@
 package com.aetherteam.aetherii.block.natural;
 
+import com.aetherteam.aetherii.AetherIITags;
+import com.aetherteam.aetherii.block.AetherIIBlocks;
 import com.aetherteam.aetherii.entity.vehicle.CloudSkiff;
+import com.aetherteam.aetherii.item.AetherIIItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.InsideBlockEffectApplier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -133,6 +137,14 @@ public class AercloudBlock extends HalfTransparentBlock implements LiquidBlockCo
     @Override
     public VoxelShape getVisualShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return Shapes.empty();
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        if (context instanceof EntityCollisionContext entityCollisionContext && entityCollisionContext.getEntity() instanceof Player player) {
+            return player.isHolding((stack) -> stack.getItem() instanceof BlockItem) && !player.isHolding((stack) -> stack.is(AetherIITags.Items.CAN_USE_ON_AERCLOUD)) ? Shapes.empty() : Shapes.block();
+        }
+        return Shapes.block();
     }
 
     @Override
