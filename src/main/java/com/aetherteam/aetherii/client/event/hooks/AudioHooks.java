ca044a692474cdd3ede3d4f9929df60f0b93e9fa
bconlon <bconlon.gml@gmail.com>
2026-02-15 09:24:20 -0800

    fix: music not working

--- src/main/java/com/aetherteam/aetherii/client/event/hooks/AudioHooks.java
+++ src/main/java/com/aetherteam/aetherii/client/event/hooks/AudioHooks.java
@@ -3,17 +3,16 @@ package com.aetherteam.aetherii.client.event.hooks;
 import com.aetherteam.aetherii.AetherIITags;
 import com.aetherteam.aetherii.client.sound.AetherIISoundEvents;
 import com.aetherteam.aetherii.client.sound.instance.FadeOutSoundInstance;
+import com.aetherteam.aetherii.client.sound.instance.MusicSoundInstance;
 import com.aetherteam.aetherii.entity.monster.dungeon.boss.AetherBossMob;
 import com.aetherteam.aetherii.mixin.mixins.client.accessor.BossHealthOverlayAccessor;
-import com.aetherteam.aetherii.mixin.mixins.client.accessor.MusicManagerAccessor;
 import com.aetherteam.aetherii.mixin.mixins.client.accessor.SoundEngineAccessor;
+import com.aetherteam.aetherii.mixin.mixins.client.accessor.SoundManagerAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.LerpingBossEvent;
 import net.minecraft.client.gui.screens.WinScreen;
-import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.MusicInfo;
-import net.minecraft.client.sounds.MusicManager;
 import net.minecraft.client.sounds.SoundEngine;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -21,7 +20,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.Music;
 import net.minecraft.sounds.Musics;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -39,6 +37,7 @@ public class AudioHooks { //todo creative music override
     public static final Music AETHER_CAVES = createAetherMusic(AetherIISoundEvents.MUSIC_AETHER_AMBIENCE);
 
     public static <T extends LivingEntity & AetherBossMob<?>> MusicInfo getSituationalMusic() {
+        MusicInfo musicInfo = null;
         if (Minecraft.getInstance().level != null && Minecraft.getInstance().player != null) {
             Holder<Biome> biome = Minecraft.getInstance().player.level().getBiome(Minecraft.getInstance().player.blockPosition());
             float volume = biome.value().getBackgroundMusicVolume();
@@ -49,7 +48,7 @@ public class AudioHooks { //todo creative music override
                         if (boss != null && boss.getHealth() > 0) {
                             Music bossMusic = boss.getBossMusic();
                             if (bossMusic != null) {
-                                return new MusicInfo(boss.getBossMusic(), volume);
+                                musicInfo = new MusicInfo(boss.getBossMusic(), volume);
                             }
                         }
                     } else {
@@ -60,38 +59,45 @@ public class AudioHooks { //todo creative music override
                         boolean sunrise = time >= 22000;
 
                         if (Minecraft.getInstance().player.position().y <= 80) {
-                            return new MusicInfo(AETHER_CAVES, volume);
+                            musicInfo = new MusicInfo(AETHER_CAVES, volume);
                         } else {
                             if (day) {
                                 Optional<WeightedList<Music>> optional = biome.value().getBackgroundMusic();
                                 if (optional.isPresent()) {
                                     Optional<Music> optional1 = optional.get().getRandom(Minecraft.getInstance().level.random);
-                                    return new MusicInfo(optional1.orElse(null), volume);
+                                    musicInfo = new MusicInfo(optional1.orElse(null), volume);
                                 } else {
-                                    return new MusicInfo(Musics.GAME, volume);
+                                    musicInfo = new MusicInfo(Musics.GAME, volume);
                                 }
                             } else if (sunset) {
-                                return new MusicInfo(AETHER_SUNSET, volume);
+                                musicInfo = new MusicInfo(AETHER_SUNSET, volume);
                             } else if (night) {
-                                return new MusicInfo(AETHER_NIGHT, volume);
+                                musicInfo = new MusicInfo(AETHER_NIGHT, volume);
                             } else if (sunrise) {
-                                return new MusicInfo(AETHER_SUNRISE, volume);
+                                musicInfo = new MusicInfo(AETHER_SUNRISE, volume);
                             }
                         }
                     }
                 }
             }
         }
-        return null;
+
+        SoundEngine soundEngine = ((SoundManagerAccessor) Minecraft.getInstance().getSoundManager()).aether_ii$getSoundEngine();
+        Optional<SoundInstance> instance = ((SoundEngineAccessor) soundEngine).aether_ii$getInstanceToChannel().keySet().stream().filter((soundInstance) -> soundInstance instanceof MusicSoundInstance).findFirst();
+        if (instance.isPresent()) {
+            musicInfo = null;
+        }
+
+        return musicInfo;
     }
 
     public static Music createAetherMusic(Holder<SoundEvent> event) {
         return new Music(event, 3600, 10800, false);
     }
 
-    public static boolean isAetherBossMusic(Music music) {
-        if (music.event().getKey() != null) {
-            Holder<SoundEvent> sound = BuiltInRegistries.SOUND_EVENT.get(music.event().getKey()).orElse(null);
+    public static boolean isAetherBossMusic(MusicInfo musicInfo) {
+        if (musicInfo != null && musicInfo.music() != null && musicInfo.music().event().getKey() != null) {
+            Holder<SoundEvent> sound = BuiltInRegistries.SOUND_EVENT.get(musicInfo.music().event().getKey()).orElse(null);
             if (sound != null) {
                 return sound.is(AetherIITags.SoundEvents.BOSS_MUSIC);
             }
@@ -117,18 +123,6 @@ public class AudioHooks { //todo creative music override
             }
         }
         return null;
-    }
-
-    public static boolean shouldCancelMusic(SoundInstance sound) {
-        Holder<SoundEvent> soundEvent = getSoundEvent(sound);
-        if (sound.getSource() == SoundSource.MUSIC && soundEvent != null && !soundEvent.is(AetherIITags.SoundEvents.ACHIEVEMENT_SOUNDS)) {
-            // Check whether there is Aether music and the sound that attempts to play does not match it.
-            MusicManager manager = Minecraft.getInstance().getMusicManager();
-            MusicManagerAccessor accessor = (MusicManagerAccessor) manager;
-            return Minecraft.getInstance().getSituationalMusic().music() != null && !sound.getLocation().equals(SimpleSoundInstance.forMusic(Minecraft.getInstance().getSituationalMusic().music().event().value(), 1.0F).getLocation())
-                    || (accessor.aether_ii$getCurrentMusic() != null && !sound.getLocation().equals(accessor.aether_ii$getCurrentMusic().getLocation()));
-        }
-        return false;
     }
 
     /**
