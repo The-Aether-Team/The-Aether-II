50f90a285fcf3d8634cec73c53d74edafea405d8
bconlon <bconlon.gml@gmail.com>
2026-02-14 18:43:41 -0800

    refactor: reduce plant mob health

--- src/main/java/com/aetherteam/aetherii/AetherIIStats.java
+++ src/main/java/com/aetherteam/aetherii/AetherIIStats.java
@@ -163,14 +163,14 @@ public class AetherIIStats {
             .put(Attributes.MAX_HEALTH, 6.0)
             .build();
     public static final ImmutableMap<Holder<Attribute>, Double> AECHOR_PLANT = ImmutableMap.<Holder<Attribute>, Double>builder() //todo
-            .put(Attributes.MAX_HEALTH, 18.0)
+            .put(Attributes.MAX_HEALTH, 10.0)
             .put(AetherIIAttributes.SLASH_RESISTANCE, -1.0)
             .put(AetherIIAttributes.IMPACT_RESISTANCE, 0.0)
             .put(AetherIIAttributes.PIERCE_RESISTANCE, 0.0)
             .put(AetherIIAttributes.TOXIN_EFFECT_RESISTANCE, 1.0)
             .build();
     public static final ImmutableMap<Holder<Attribute>, Double> CARRION_SPROUT = ImmutableMap.<Holder<Attribute>, Double>builder() //todo
-            .put(Attributes.MAX_HEALTH, 12.0)
+            .put(Attributes.MAX_HEALTH, 8.0)
             .put(AetherIIAttributes.SLASH_RESISTANCE, -1.0)
             .put(AetherIIAttributes.IMPACT_RESISTANCE, 0.0)
             .put(AetherIIAttributes.PIERCE_RESISTANCE, 0.0)
