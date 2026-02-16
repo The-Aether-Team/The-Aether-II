acfee9fb9f9ab8e3e694b57499ac87f73ac2ca3c
bconlon <bconlon.gml@gmail.com>
2026-02-15 21:42:05 -0800

    refactor: unnecessary change

--- src/main/java/com/aetherteam/aetherii/network/packet/clientbound/FreezingParticlePacket.java
+++ src/main/java/com/aetherteam/aetherii/network/packet/clientbound/FreezingParticlePacket.java
@@ -31,7 +31,7 @@ public record FreezingParticlePacket(Block block, BlockPos pos) implements Custo
     }
 
     public static void execute(FreezingParticlePacket payload, IPayloadContext context) {
-        if (context.player() != null && context.player().level() != null) {
+        if (Minecraft.getInstance().player != null && Minecraft.getInstance().level != null) {
             for (Direction direction : Direction.values()) {
                 for (int i = 0; i < 25; i++) {
                     ParticleUtils.spawnParticleOnFace(context.player().level(), payload.pos(), direction, new BlockParticleOption(ParticleTypes.BLOCK, payload.block().defaultBlockState()), Vec3.ZERO, 0.5F);
