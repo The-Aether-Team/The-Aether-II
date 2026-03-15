76234d51fbc8f3fa3d03a23c6b7c26eddf7b3a14
Builderdog <timon.kempter@gmail.com>
2026-03-15 10:27:16 +0100

    improv: reorganized spawn egg tab

--- src/main/java/com/aetherteam/aetherii/item/AetherIICreativeTabs.java
+++ src/main/java/com/aetherteam/aetherii/item/AetherIICreativeTabs.java
@@ -872,37 +872,37 @@ public class AetherIICreativeTabs {
             .icon(() -> new ItemStack(AetherIIItems.AERBUNNY_SPAWN_EGG.get()))
             .title(Component.translatable("itemGroup." + AetherII.MODID + ".spawn_eggs"))
             .displayItems((features, output) -> {
-                output.accept(AetherIIItems.AECHOR_PLANT_SPAWN_EGG.get());
-                output.accept(AetherIIItems.AERBUNNY_SPAWN_EGG.get());
-                output.accept(AetherIIItems.AERWHALE_SPAWN_EGG.get());
-                output.accept(AetherIIItems.ARKENIUM_TALUTON_SPAWN_EGG.get());
-                output.accept(AetherIIItems.DETONATION_SENTRY_SPAWN_EGG.get());
-                output.accept(AetherIIItems.HIGHFIELDS_BURRUKAI_SPAWN_EGG.get());
-                output.accept(AetherIIItems.MAGNETIC_BURRUKAI_SPAWN_EGG.get());
-                output.accept(AetherIIItems.ARCTIC_BURRUKAI_SPAWN_EGG.get());
-                output.accept(AetherIIItems.CARRION_SPROUT_SPAWN_EGG.get());
-                output.accept(AetherIIItems.COCKATRICE_SPAWN_EGG.get());
-                output.accept(AetherIIItems.FLYING_COW_SPAWN_EGG.get());
-                output.accept(AetherIIItems.GLITTERWING_SPAWN_EGG.get());
-                output.accept(AetherIIItems.GRAVITITE_TALUTON_SPAWN_EGG.get());
-                output.accept(AetherIIItems.HIGHFIELDS_KIRRID_SPAWN_EGG.get());
-                output.accept(AetherIIItems.MAGNETIC_KIRRID_SPAWN_EGG.get());
-                output.accept(AetherIIItems.ARCTIC_KIRRID_SPAWN_EGG.get());
-                output.accept(AetherIIItems.MOA_SPAWN_EGG.get());
-                output.accept(AetherIIItems.PHYG_SPAWN_EGG.get());
-                output.accept(AetherIIItems.SHEEPUFF_SPAWN_EGG.get());
                 output.accept(AetherIIItems.HIGHFIELDS_TAEGORE_SPAWN_EGG.get());
                 output.accept(AetherIIItems.MAGNETIC_TAEGORE_SPAWN_EGG.get());
                 output.accept(AetherIIItems.ARCTIC_TAEGORE_SPAWN_EGG.get());
-                output.accept(AetherIIItems.SENTRY_CRATE_MIMIC_SPAWN_EGG.get());
-                output.accept(AetherIIItems.SENTRY_GOLEM_SPAWN_EGG.get());
-                output.accept(AetherIIItems.SHROUDWING_SPAWN_EGG.get());
+                output.accept(AetherIIItems.HIGHFIELDS_KIRRID_SPAWN_EGG.get());
+                output.accept(AetherIIItems.MAGNETIC_KIRRID_SPAWN_EGG.get());
+                output.accept(AetherIIItems.ARCTIC_KIRRID_SPAWN_EGG.get());
+                output.accept(AetherIIItems.HIGHFIELDS_BURRUKAI_SPAWN_EGG.get());
+                output.accept(AetherIIItems.MAGNETIC_BURRUKAI_SPAWN_EGG.get());
+                output.accept(AetherIIItems.ARCTIC_BURRUKAI_SPAWN_EGG.get());
+                output.accept(AetherIIItems.PHYG_SPAWN_EGG.get());
+                output.accept(AetherIIItems.SHEEPUFF_SPAWN_EGG.get());
+                output.accept(AetherIIItems.FLYING_COW_SPAWN_EGG.get());
+                output.accept(AetherIIItems.AERBUNNY_SPAWN_EGG.get());
                 output.accept(AetherIIItems.SKYROOT_LIZARD_SPAWN_EGG.get());
+                output.accept(AetherIIItems.GLITTERWING_SPAWN_EGG.get());
+                output.accept(AetherIIItems.SHROUDWING_SPAWN_EGG.get());
+                output.accept(AetherIIItems.MOA_SPAWN_EGG.get());
+                output.accept(AetherIIItems.AERWHALE_SPAWN_EGG.get());
                 output.accept(AetherIIItems.BLUE_SWET_SPAWN_EGG.get());
                 output.accept(AetherIIItems.GOLDEN_SWET_SPAWN_EGG.get());
+                output.accept(AetherIIItems.AECHOR_PLANT_SPAWN_EGG.get());
+                output.accept(AetherIIItems.CARRION_SPROUT_SPAWN_EGG.get());
                 output.accept(AetherIIItems.SKEPHID_SPAWN_EGG.get());
-                output.accept(AetherIIItems.TEMPEST_SPAWN_EGG.get());
                 output.accept(AetherIIItems.ZEPHYR_SPAWN_EGG.get());
+                output.accept(AetherIIItems.TEMPEST_SPAWN_EGG.get());
+                output.accept(AetherIIItems.COCKATRICE_SPAWN_EGG.get());
+                output.accept(AetherIIItems.ARKENIUM_TALUTON_SPAWN_EGG.get());
+                output.accept(AetherIIItems.GRAVITITE_TALUTON_SPAWN_EGG.get());
+                output.accept(AetherIIItems.SENTRY_CRATE_MIMIC_SPAWN_EGG.get());
+                output.accept(AetherIIItems.DETONATION_SENTRY_SPAWN_EGG.get());
+                output.accept(AetherIIItems.SENTRY_GOLEM_SPAWN_EGG.get());
             }).build());
 
     public static ItemStack getMoaBook() {
