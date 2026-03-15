10a38958cc5c3694a294aea44cdb31aa29a54afc
baguchan <shutux500@gmail.com>
2026-03-15 09:21:34 +0900

    moa egg and altar render fixed

---
+++ src/main/java/com/aetherteam/aetherii/client/renderer/blockentity/state/AlterRenderState.java
@@ -0,0 +1,23 @@
+package com.aetherteam.aetherii.client.renderer.blockentity.state;
+
+import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
+import net.minecraft.client.renderer.entity.state.ItemClusterRenderState;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.core.Direction;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AlterRenderState extends BlockEntityRenderState {
+    public float inputItemRotation;
+    public ItemClusterRenderState displayItem;
+    public float bobOff;
+    public int progress;
+    public float ambSpinningSpeed = 0.025F;
+    public double ambFinalSpeed;
+
+    public List<ItemStackRenderState> fuelItems = Collections.emptyList();
+    public ItemStackRenderState itemInput = new ItemStackRenderState();
+    public Direction facing = Direction.NORTH;
+
+}
