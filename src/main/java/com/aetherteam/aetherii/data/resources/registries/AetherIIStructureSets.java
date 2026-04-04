1cca09f1b3c79d66126689d14917dca61ccbe712
Builderdog <timon.kempter@gmail.com>
2026-04-04 09:32:56 +0200

    improv: made veradexian libraries a bit more common

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructureSets.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructureSets.java
@@ -53,9 +53,9 @@ public class AetherIIStructureSets {
         );
 
         context.register(AETHER_SURFACE_RUINS, new StructureSet(List.of(
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_TEMPERATE), 3),
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_ARCTIC), 3),
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_LIBRARY_TEMPERATE))),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_TEMPERATE), 9),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_ARCTIC), 9),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_LIBRARY_TEMPERATE), 4)),
                 new RandomSpreadStructurePlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.LEGACY_TYPE_2, 0.75F, 2038911, Optional.of(new StructurePlacement.ExclusionZone(outposts, 6)), 14, 8, RandomSpreadType.LINEAR))
         );
 
