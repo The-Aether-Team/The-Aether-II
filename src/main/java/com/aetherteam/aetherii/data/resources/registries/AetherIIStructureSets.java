ad6acf65ba5194a864a557431a8e3b8d7df714d2
Builderdog <timon.kempter@gmail.com>
2026-04-04 18:45:17 +0200

    improv: made libraries a bit more common again

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructureSets.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructureSets.java
@@ -53,10 +53,10 @@ public class AetherIIStructureSets {
         );
 
         context.register(AETHER_SURFACE_RUINS, new StructureSet(List.of(
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_TEMPERATE), 9),
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_ARCTIC), 9),
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_LIBRARY_TEMPERATE), 5),
-                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_LIBRARY_ARCTIC), 5)),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_TEMPERATE), 3),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_RUINS_ARCTIC), 3),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_LIBRARY_TEMPERATE), 2),
+                StructureSet.entry(structures.getOrThrow(AetherIIStructures.VERADEXIAN_LIBRARY_ARCTIC), 2)),
                 new RandomSpreadStructurePlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.LEGACY_TYPE_2, 0.75F, 2038911, Optional.of(new StructurePlacement.ExclusionZone(outposts, 6)), 14, 8, RandomSpreadType.LINEAR))
         );
 
