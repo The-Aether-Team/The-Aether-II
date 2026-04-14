c44cbd3c7cf983b6e7f84966ddafc1793428ba1d
bconlon <bconlon.gml@gmail.com>
2026-04-14 15:47:09 -0700

    improv: allow accessory freezable recipes to have biome checks

--- src/main/java/com/aetherteam/aetherii/recipe/recipes/block/AccessoryFreezableRecipe.java
+++ src/main/java/com/aetherteam/aetherii/recipe/recipes/block/AccessoryFreezableRecipe.java
@@ -2,18 +2,27 @@ package com.aetherteam.aetherii.recipe.recipes.block;
 
 import com.aetherteam.aetherii.recipe.recipes.AetherIIRecipeTypes;
 import com.aetherteam.aetherii.recipe.serializer.AetherIIRecipeSerializers;
+import com.aetherteam.aetherii.recipe.serializer.BiomeParameterRecipeSerializer;
 import com.aetherteam.nitrogen.recipe.BlockPropertyPair;
 import com.aetherteam.nitrogen.recipe.BlockStateIngredient;
 import com.aetherteam.nitrogen.recipe.recipes.AbstractBlockStateRecipe;
 import com.aetherteam.nitrogen.recipe.serializer.BlockStateRecipeSerializer;
+import com.mojang.datafixers.util.Either;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.biome.Biome;
 
 import java.util.Optional;
 
-public class AccessoryFreezableRecipe extends AbstractBlockStateRecipe {
+public class AccessoryFreezableRecipe extends AbstractBiomeParameterRecipe {
+    public AccessoryFreezableRecipe(Optional<Either<ResourceKey<Biome>, TagKey<Biome>>> biome, BlockStateIngredient ingredient, BlockPropertyPair result, Optional<Identifier> function) {
+        super(AetherIIRecipeTypes.ACCESSORY_FREEZABLE.get(), biome, ingredient, result, function);
+    }
+
     public AccessoryFreezableRecipe(BlockStateIngredient ingredient, BlockPropertyPair result, Optional<Identifier> function) {
-        super(AetherIIRecipeTypes.ACCESSORY_FREEZABLE.get(), ingredient, result, function);
+        this(Optional.empty(), ingredient, result, function);
     }
 
     @Override
@@ -21,9 +30,9 @@ public class AccessoryFreezableRecipe extends AbstractBlockStateRecipe {
         return AetherIIRecipeSerializers.ACCESSORY_FREEZABLE.get();
     }
 
-    public static class Serializer extends BlockStateRecipeSerializer<AccessoryFreezableRecipe> {
+    public static class Serializer extends BiomeParameterRecipeSerializer<AccessoryFreezableRecipe> {
         public Serializer() {
-            super(AccessoryFreezableRecipe::new);
+            super(AccessoryFreezableRecipe::new, AccessoryFreezableRecipe::new);
         }
     }
 }
