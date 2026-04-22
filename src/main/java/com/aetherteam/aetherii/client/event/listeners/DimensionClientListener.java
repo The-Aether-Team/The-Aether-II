549be89cc46cda0e3a48bc76ebe419f3c29f3132
bconlon <bconlon.gml@gmail.com>
2026-04-22 16:20:07 -0700

    improv: restore closened fog for certain biomes

--- src/main/java/com/aetherteam/aetherii/client/event/listeners/DimensionClientListener.java
+++ src/main/java/com/aetherteam/aetherii/client/event/listeners/DimensionClientListener.java
@@ -28,47 +28,47 @@ public class DimensionClientListener {
         float nearDistance = event.getNearPlaneDistance();
         float farDistance = event.getFarPlaneDistance();
 
-        if (camera.entity().level() instanceof ClientLevel clientLevel) {
+        if (camera.entity().level() instanceof ClientLevel clientLevel && clientLevel.getBiome(camera.blockPosition()).is(AetherIITags.Biomes.THE_AETHER)) {
             Holder<Biome> biome = clientLevel.getBiome(camera.blockPosition());
             FogType fluidState = camera.getFluidInCamera();
-//            if (fogMode == FogType.ATMOSPHERIC && fluidState == FogType.NONE && (camera.entity().isEyeInFluid() == NeoForgeMod.EMPTY_TYPE.value())) { //todo
-//                if (modifiedNearDistance == null) {
-//                    modifiedNearDistance = nearDistance;
-//                }
-//                if (modifiedFarDistance == null) {
-//                    modifiedFarDistance = farDistance;
-//                }
-//
-//                float nearDistanceGoal = farDistance / 20.0F;
-//                float farDistanceGoal = farDistance;
-//
-//                if (biome.is(AetherIITags.Biomes.ARCTIC)) {
-//                    nearDistanceGoal = farDistance / 20.0F;
-//                    farDistanceGoal = farDistance / 5.0F;
-//                } else if (biome.is(AetherIITags.Biomes.MAGNETIC_FOG)) {
-//                    nearDistanceGoal = farDistance / 80.0F;
-//                    farDistanceGoal = farDistance / 5.0F;
-//                } else if (biome.is(AetherIITags.Biomes.IRRADIATED)) {
-//                    nearDistanceGoal = farDistance / 60.0F;
-//                    farDistanceGoal = farDistance / 7.5F;
-//                }
-//
-//                if (clientLevel.isRaining()) {
-//                    nearDistanceGoal = -15.0F;
-//                }
-//                if (clientLevel.isThundering()) {
-//                    nearDistanceGoal = -30.0F;
-//                }
-//
-//                modifiedNearDistance = Mth.lerp(0.05F, modifiedNearDistance, nearDistanceGoal);
-//                modifiedFarDistance = Mth.lerp(0.05F, modifiedFarDistance, farDistanceGoal);
-//
-//                event.setNearPlaneDistance(modifiedNearDistance);
-//                event.setFarPlaneDistance(modifiedFarDistance);
-//            } else {
+            if (fogMode == FogType.ATMOSPHERIC && fluidState == FogType.NONE) {
+                if (modifiedNearDistance == null) {
+                    modifiedNearDistance = nearDistance;
+                }
+                if (modifiedFarDistance == null) {
+                    modifiedFarDistance = farDistance;
+                }
+
+                float nearDistanceGoal = farDistance / 20.0F;
+                float farDistanceGoal = farDistance;
+
+                if (biome.is(AetherIITags.Biomes.ARCTIC)) {
+                    nearDistanceGoal = farDistance / 20.0F;
+                    farDistanceGoal = farDistance / 5.0F;
+                } else if (biome.is(AetherIITags.Biomes.MAGNETIC_FOG)) {
+                    nearDistanceGoal = farDistance / 80.0F;
+                    farDistanceGoal = farDistance / 5.0F;
+                } else if (biome.is(AetherIITags.Biomes.IRRADIATED)) {
+                    nearDistanceGoal = farDistance / 60.0F;
+                    farDistanceGoal = farDistance / 7.5F;
+                }
+
+                if (clientLevel.isRaining()) {
+                    nearDistanceGoal = -15.0F;
+                }
+                if (clientLevel.isThundering()) {
+                    nearDistanceGoal = -30.0F;
+                }
+
+                modifiedNearDistance = Mth.lerp(0.05F, modifiedNearDistance, nearDistanceGoal);
+                modifiedFarDistance = Mth.lerp(0.05F, modifiedFarDistance, farDistanceGoal);
+
+                event.setNearPlaneDistance(modifiedNearDistance);
+                event.setFarPlaneDistance(modifiedFarDistance);
+            } else {
                 modifiedNearDistance = null;
                 modifiedFarDistance = null;
-//            }
+            }
         }
     }
 
