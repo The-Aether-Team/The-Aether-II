b5064d1b3eed40688095a8690ef168e0d5b1c8d2
Builderdog <timon.kempter@gmail.com>
2026-05-06 19:55:46 +0200

    improv: mineshafts can no longer float mid-air

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/AetherIIPools.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/AetherIIPools.java
@@ -1,7 +1,6 @@
 package com.aetherteam.aetherii.data.resources.registries.pools;
 
 import com.aetherteam.aetherii.AetherII;
-import com.aetherteam.aetherii.world.structure.pool.AetherMineshaftPoolElement;
 import com.aetherteam.aetherii.world.structure.pool.AetherPoolElement;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Either;
@@ -80,13 +79,6 @@ public class AetherIIPools {
     }
     public static Function<StructureTemplatePool.Projection, AetherPoolElement> aetherPoolCaves(String id, Holder<StructureProcessorList> processor) {
         return pool -> new AetherPoolElement(Either.left(Identifier.fromNamespaceAndPath(AetherII.MODID, id)), processor, pool, Optional.of(LiquidSettings.IGNORE_WATERLOGGING), 24, 112, true);
-    }
-
-    public static Function<StructureTemplatePool.Projection, AetherMineshaftPoolElement> aetherPoolMineshaft(String id) {
-        return pool -> new AetherMineshaftPoolElement(Either.left(Identifier.fromNamespaceAndPath(AetherII.MODID, id)), EMPTY, pool, Optional.of(LiquidSettings.IGNORE_WATERLOGGING), -32, 384, false);
-    }
-    public static Function<StructureTemplatePool.Projection, AetherMineshaftPoolElement> aetherPoolMineshaft(String id, Holder<StructureProcessorList> processor) {
-        return pool -> new AetherMineshaftPoolElement(Either.left(Identifier.fromNamespaceAndPath(AetherII.MODID, id)), processor, pool, Optional.of(LiquidSettings.IGNORE_WATERLOGGING), -32, 384, false);
     }
 
     public static Function<StructureTemplatePool.Projection, AetherPoolElement> aetherPoolAquatic(String id) {
--- src/main/java/com/aetherteam/aetherii/world/structure/processor/RemoveInAirProcessor.java
+++ src/main/java/com/aetherteam/aetherii/world/structure/processor/RemoveInAirProcessor.java
@@ -4,7 +4,6 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -21,26 +20,9 @@ public class RemoveInAirProcessor extends StructureProcessor {
     @Override
     public StructureTemplate.StructureBlockInfo process(LevelReader level, BlockPos origin, BlockPos centerBottom, StructureTemplate.StructureBlockInfo originalBlockInfo, StructureTemplate.StructureBlockInfo modifiedBlockInfo, StructurePlaceSettings settings, @Nullable StructureTemplate template) {
         assert template != null;
-       /* BoundingBox bounds = template.getBoundingBox(settings, modifiedBlockInfo.pos());
-        if (level.getBlockState(bounds.getCenter().atY(bounds.minY())).is(Blocks.AIR)
-
-                new BlockPos(bounds.minX(), bounds.minY(), bounds.minZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.maxX(), bounds.minY(), bounds.minZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.maxX(), bounds.maxX(), bounds.minZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.minX(), bounds.maxX(), bounds.minZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.minX(), bounds.minY(), bounds.maxZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.maxX(), bounds.minY(), bounds.maxZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.maxX(), bounds.maxX(), bounds.maxZ())).is(Blocks.AIR)
-                && level.getBlockState(new BlockPos(bounds.minX(), bounds.maxX(), bounds.maxZ())).is(Blocks.AIR)
-
-
-        ) {
-            return new StructureTemplate.StructureBlockInfo(modifiedBlockInfo.pos(), Blocks.AIR.defaultBlockState(), modifiedBlockInfo.nbt());
+        if (level.getBlockState(origin).is(Blocks.AIR)) {
+            return null;
         }
-
-        */
-
-
         return super.process(level, origin, centerBottom, originalBlockInfo, modifiedBlockInfo, settings, template);
     }
 
