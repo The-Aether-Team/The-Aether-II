3aa6c54d0b36314bafcca21d292a5ae9c361416b
Builderdog <timon.kempter@gmail.com>
2026-05-13 21:35:16 +0200

    Reapply "Merge branch '26.1.2-alpha.4' into feat/builderdog/more-veradexian-structures"
    This reverts commit 8285c7cac773d91c8cace9dca11c40ffd04ebae7.

--- src/main/java/com/aetherteam/aetherii/advancement/predicate/AetherIIEntitySubPredicates.java
+++ src/main/java/com/aetherteam/aetherii/advancement/predicate/AetherIIEntitySubPredicates.java
@@ -10,6 +10,8 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class AetherIIEntitySubPredicates {
     public static final DeferredRegister<MapCodec<? extends EntitySubPredicate>> ENTITY_SUB_PREDICATES = DeferredRegister.create(BuiltInRegistries.ENTITY_SUB_PREDICATE_TYPE, AetherII.MODID);
 
+    public static final DeferredHolder<MapCodec<? extends EntitySubPredicate>, MapCodec<AndPredicate>> AND = ENTITY_SUB_PREDICATES.register("and", () -> AndPredicate.CODEC);
+    public static final DeferredHolder<MapCodec<? extends EntitySubPredicate>, MapCodec<OnGroundPredicate>> ON_GROUND = ENTITY_SUB_PREDICATES.register("on_ground", () -> OnGroundPredicate.CODEC);
     public static final DeferredHolder<MapCodec<? extends EntitySubPredicate>, MapCodec<AlivePredicate>> ALIVE = ENTITY_SUB_PREDICATES.register("alive", () -> AlivePredicate.CODEC);
     public static final DeferredHolder<MapCodec<? extends EntitySubPredicate>, MapCodec<ArmorSetPredicate>> ARMOR_SET = ENTITY_SUB_PREDICATES.register("armor_set", () -> ArmorSetPredicate.CODEC);
     public static final DeferredHolder<MapCodec<? extends EntitySubPredicate>, MapCodec<EffectBuildupPredicate>> EFFECT_BUILDUP = ENTITY_SUB_PREDICATES.register("effect_buildup", () -> EffectBuildupPredicate.CODEC);
