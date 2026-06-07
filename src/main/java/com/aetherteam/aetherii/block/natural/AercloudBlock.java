b4b4cae45d421fc1cf735d4db0b2930cee51e303
Builderdog <timon.kempter@gmail.com>
2026-06-07 10:31:20 +0200

    fix: fixed landing on aerclouds playing the fall damage sound

--- src/main/java/com/aetherteam/aetherii/block/natural/AercloudBlock.java
+++ src/main/java/com/aetherteam/aetherii/block/natural/AercloudBlock.java
@@ -63,6 +63,19 @@ public class AercloudBlock extends HalfTransparentBlock implements LiquidBlockCo
     }
 
     /**
+     * This block does not cause fall damage, so this method is overridden from {@link Block#fallOn(Level, BlockState, BlockPos, Entity, double)} to be empty.
+     *
+     * @param level        The {@link Level} the block is in.
+     * @param state        The {@link BlockState} of the block.
+     * @param pos          The {@link BlockPos} of the block.
+     * @param entity       The {@link Entity} that fell on the block.
+     * @param fallDistance The fall distance of the entity as a {@link Float}.
+     */
+    @Override
+    public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, double fallDistance) {
+    }
+
+    /**
      * [CODE COPY] - {@link net.minecraft.world.level.block.AbstractGlassBlock#propagatesSkylightDown(BlockState, BlockGetter, BlockPos)}.
      */
     @Override
