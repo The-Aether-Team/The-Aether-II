1524a1219cc4e8cc135e97c9c19058f482271266
Builderdog <timon.kempter@gmail.com>
2026-07-13 17:51:17 +0200

    improv: reduced spawnrate of sentry ruins and undercloud mineshafts

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructureSets.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/AetherIIStructureSets.java
@@ -76,7 +76,7 @@ public class AetherIIStructureSets {
         Holder.Reference<StructureSet> sentryRuins = context.register(AETHER_UNDERGROUND_COMPLEXES, new StructureSet(List.of(
                 StructureSet.entry(structures.getOrThrow(AetherIIStructures.SENTRY_RUINS), 2),
                 StructureSet.entry(structures.getOrThrow(AetherIIStructures.UNDERCLOUD_MINESHAFT), 3)),
-                new RandomSpreadStructurePlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.DEFAULT, 1.0F, 32146754, Optional.empty(), 12, 6, RandomSpreadType.TRIANGULAR))
+                new RandomSpreadStructurePlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.DEFAULT, 1.0F, 32146754, Optional.empty(), 14, 8, RandomSpreadType.TRIANGULAR))
         );
 
         context.register(IRRADIATED_REMNANTS, new StructureSet(List.of(
