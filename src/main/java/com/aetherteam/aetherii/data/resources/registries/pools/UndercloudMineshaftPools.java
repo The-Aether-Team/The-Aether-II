61040849d1dbf8bb8eb7a92bcae1e419ff7854a2
Builderdog <timon.kempter@gmail.com>
2026-07-23 09:32:19 +0200

    improv: bridge improvements, aerclouds now spawn at the bottom of bridge pillars, added more variation to cockatrice nests

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/UndercloudMineshaftPools.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/UndercloudMineshaftPools.java
@@ -1,6 +1,7 @@
 package com.aetherteam.aetherii.data.resources.registries.pools;
 
 import com.aetherteam.aetherii.data.resources.registries.AetherIIProcessorLists;
+import com.aetherteam.aetherii.data.resources.registries.holyisles.HolyIslesPlacedFeatures;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Holder;
@@ -9,6 +10,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.data.worldgen.Pools;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
@@ -17,6 +19,7 @@ public class UndercloudMineshaftPools {
     public static final ResourceKey<StructureTemplatePool> HUB = AetherIIPools.createKey("undercloud_mineshaft/hubs");
     public static final ResourceKey<StructureTemplatePool> BRIDGE = AetherIIPools.createKey("undercloud_mineshaft/bridges");
     public static final ResourceKey<StructureTemplatePool> CORRIDOR = AetherIIPools.createKey("undercloud_mineshaft/corridors");
+    public static final ResourceKey<StructureTemplatePool> AERCLOUD = AetherIIPools.createKey("undercloud_mineshaft/decoration/aercloud");
 
     public static void bootstrap(BootstrapContext<StructureTemplatePool> context) {
         HolderGetter<StructureTemplatePool> templatePools = context.lookup(Registries.TEMPLATE_POOL);
@@ -25,6 +28,8 @@ public class UndercloudMineshaftPools {
         HolderGetter<StructureProcessorList> processors = context.lookup(Registries.PROCESSOR_LIST);
         Holder<StructureProcessorList> processorBridge = processors.getOrThrow(AetherIIProcessorLists.UNDERCLOUD_MINESHAFT_BRIDGE);
         Holder<StructureProcessorList> processorCorridor = processors.getOrThrow(AetherIIProcessorLists.UNDERCLOUD_MINESHAFT_CORRIDOR);
+
+        HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
 
         context.register(HUB, new StructureTemplatePool(
                 fallback,
@@ -48,14 +53,16 @@ public class UndercloudMineshaftPools {
                 fallback,
                 ImmutableList.of(
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_basic", processorCorridor), 11),
-                        Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_cockatrice_nest", processorCorridor), 6),
+                        Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_cockatrice_nest_01", processorCorridor), 2),
+                        Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_cockatrice_nest_02", processorCorridor), 2),
+                        Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_cockatrice_nest_03", processorCorridor), 2),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_elevation", processorCorridor), 4),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_storage_01", processorCorridor), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_storage_02", processorCorridor), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_storage_03", processorCorridor), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_storage_04", processorCorridor), 1),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_storage_03", processorCorridor), 1),
-                        Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_alkahest", processorCorridor), 4),
+                        Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_alkahest", processorCorridor), 2),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_sleeping_barracks", processorCorridor), 2),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_pit", processorCorridor), 3),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/straight_platform", processorCorridor), 2),
@@ -66,6 +73,14 @@ public class UndercloudMineshaftPools {
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/junction_02", processorCorridor), 2),
                         Pair.of(AetherIIPools.aetherPoolUnderground("undercloud_mineshaft/corridors/junction_03", processorCorridor), 1)
 
+                ),
+                StructureTemplatePool.Projection.RIGID)
+        );
+        context.register(AERCLOUD, new StructureTemplatePool(
+                fallback,
+                ImmutableList.of(
+                        Pair.of(StructurePoolElement.feature(placedFeatures.getOrThrow(HolyIslesPlacedFeatures.COLD_AERCLOUD_LIBRARY)), 2),
+                        Pair.of(StructurePoolElement.empty(), 1)
                 ),
                 StructureTemplatePool.Projection.RIGID)
         );
