e6beac0d08d47c6a102b47ad5c205432fb86a7ac
bconlon <bconlon.gml@gmail.com>
2026-08-08 15:19:09 -0700

    refactor: better shape for bushes

--- src/main/java/com/aetherteam/aetherii/world/tree/foliage/AetherBushFoliagePlacer.java
+++ src/main/java/com/aetherteam/aetherii/world/tree/foliage/AetherBushFoliagePlacer.java
@@ -2,6 +2,7 @@ package com.aetherteam.aetherii.world.tree.foliage;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.WorldGenLevel;
@@ -19,13 +20,37 @@ public class AetherBushFoliagePlacer extends BlobFoliagePlacer {
 
     @Override
     protected void createFoliage(WorldGenLevel level, FoliagePlacer.FoliageSetter foliageSetter, RandomSource random, TreeConfiguration config, int treeHeight, FoliagePlacer.FoliageAttachment foliageAttachment, int foliageHeight, int leafRadius, int offset) {
-        this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos(), 1, offset, foliageAttachment.doubleTrunk());
-        this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos(), 2, offset - 1, foliageAttachment.doubleTrunk());
+        Direction randomDirection = Direction.Plane.HORIZONTAL.getRandomDirection(random);
+        Direction oppositeDirection = randomDirection.getOpposite();
+        Direction adjacentDirection = random.nextBoolean() ? randomDirection.getClockWise() : randomDirection.getCounterClockWise();
+
+        FoliagePlacer.tryPlaceLeaf(level, foliageSetter, random, config, foliageAttachment.pos());
+
+        this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos(), 1, -1, false);
+
+        this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos().relative(randomDirection), 1, -1, false);
+        if (random.nextBoolean()) {
+            this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos().relative(adjacentDirection), 1, -1, false);
+            FoliagePlacer.tryPlaceLeaf(level, foliageSetter, random, config, foliageAttachment.pos().relative(randomDirection));
+        } else if (random.nextBoolean()) {
+            FoliagePlacer.tryPlaceLeaf(level, foliageSetter, random, config, foliageAttachment.pos().relative(randomDirection));
+        }
+
+        if (random.nextBoolean()) {
+            FoliagePlacer.tryPlaceLeaf(level, foliageSetter, random, config, foliageAttachment.pos().below().relative(oppositeDirection).relative(adjacentDirection));
+            if (random.nextBoolean()) {
+                FoliagePlacer.tryPlaceLeaf(level, foliageSetter, random, config, foliageAttachment.pos().relative(adjacentDirection));
+            }
+        }
+
+        if (random.nextBoolean()) {
+            FoliagePlacer.tryPlaceLeaf(level, foliageSetter, random, config, foliageAttachment.pos().relative(adjacentDirection.getOpposite()));
+        }
     }
 
     @Override
-    protected boolean shouldSkipLocation(RandomSource random, int dx, int y, int dz, int currentRadius, boolean doubleTrunk) {
-        return Math.sqrt(dx*dx + dz*dz) > currentRadius - random.nextInt(2);
+    protected boolean shouldSkipLocation(RandomSource random, int localX, int localY, int localZ, int range, boolean large) {
+        return localX == range && localZ == range;
     }
 
     @Override
