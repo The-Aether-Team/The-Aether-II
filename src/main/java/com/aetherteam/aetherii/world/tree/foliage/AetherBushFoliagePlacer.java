685d92855d18271283ab8505072c9a85f01f56b3
bconlon <bconlon.gml@gmail.com>
2026-08-08 13:03:28 -0700

    fix: crash from foliage placer name

--- src/main/java/com/aetherteam/aetherii/world/tree/foliage/AetherBushFoliagePlacer.java
+++ src/main/java/com/aetherteam/aetherii/world/tree/foliage/AetherBushFoliagePlacer.java
@@ -11,24 +11,25 @@ import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
 
 public class AetherBushFoliagePlacer extends BlobFoliagePlacer {
-    public static final MapCodec<AetherBushFoliagePlacer> CODEC = RecordCodecBuilder.mapCodec((i) -> {
-        return blobParts(i).apply(i, AetherBushFoliagePlacer::new);
-    });
+    public static final MapCodec<AetherBushFoliagePlacer> CODEC = RecordCodecBuilder.mapCodec((i) -> blobParts(i).apply(i, AetherBushFoliagePlacer::new));
 
     public AetherBushFoliagePlacer(IntProvider radius, IntProvider offset, int height) {
         super(radius, offset, height);
     }
 
-    protected FoliagePlacerType<?> type() {
-        return AetherIIFoliagePlacerTypes.AETHER_BUSH_FOLIAGE_PLACER.get();
-    }
-
+    @Override
     protected void createFoliage(WorldGenLevel level, FoliagePlacer.FoliageSetter foliageSetter, RandomSource random, TreeConfiguration config, int treeHeight, FoliagePlacer.FoliageAttachment foliageAttachment, int foliageHeight, int leafRadius, int offset) {
         this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos(), 1, offset, foliageAttachment.doubleTrunk());
         this.placeLeavesRow(level, foliageSetter, random, config, foliageAttachment.pos(), 2, offset - 1, foliageAttachment.doubleTrunk());
     }
 
+    @Override
     protected boolean shouldSkipLocation(RandomSource random, int dx, int y, int dz, int currentRadius, boolean doubleTrunk) {
         return Math.sqrt(dx*dx + dz*dz) > currentRadius - random.nextInt(2);
+    }
+
+    @Override
+    protected FoliagePlacerType<?> type() {
+        return AetherIIFoliagePlacerTypes.AETHER_BUSH_FOLIAGE_PLACER.get();
     }
 }
