49998d762387c4f67693cb2ad843461b0d0b674b
bconlon <bconlon.gml@gmail.com>
2026-09-04 19:56:41 -0700

    fix: leaf piles not properly generating under leaves

--- src/main/java/com/aetherteam/aetherii/mixin/mixins/common/HeightmapTypesMixin.java
+++ src/main/java/com/aetherteam/aetherii/mixin/mixins/common/HeightmapTypesMixin.java
@@ -1,6 +1,8 @@
 package com.aetherteam.aetherii.mixin.mixins.common;
 
 import com.aetherteam.aetherii.AetherIITags;
+import com.aetherteam.aetherii.block.AetherIIBlockStateProperties;
+import com.aetherteam.aetherii.block.natural.AetherLeavesBlock;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.Heightmap;
@@ -15,7 +17,7 @@ public class HeightmapTypesMixin {
     @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
     private static Predicate<BlockState> injected(Predicate<BlockState> isOpaque, @Local(ordinal = 0, argsOnly = true) String name) {
         if (name.equals("MOTION_BLOCKING_NO_LEAVES")) {
-            return isOpaque.or((state) -> state.is(AetherIITags.Blocks.LEAVES));
+            return isOpaque.or((state) -> state.is(AetherIITags.Blocks.LEAVES) && state.getValueOrElse(AetherLeavesBlock.MOSSY, AetherIIBlockStateProperties.Mossy.NONE) != AetherIIBlockStateProperties.Mossy.NONE);
         }
         return isOpaque;
     }
