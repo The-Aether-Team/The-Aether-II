47c604b1898ae2904f9b7c242239027d58e87e3d
Builderdog <timon.kempter@gmail.com>
2026-09-26 13:44:53 +0200

    improv: added more veradexian ruin variation

--- src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/VeradexianRuinPools.java
+++ src/main/java/com/aetherteam/aetherii/data/resources/registries/pools/VeradexianRuinPools.java
@@ -66,9 +66,12 @@ public class VeradexianRuinPools {
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/small_ruin_03", processorRuinsTerrainMatching), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/small_ruin_04", processorRuinsTerrainMatching), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/small_ruin_05", processorRuinsTerrainMatching), 1),
-                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_01", processorRuinsDecay), 3),
-                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_02", processorRuinsDecay), 3),
-                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_03", processorRuinsDecay), 3),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/large_ruin_01", processorRuinsTerrainMatching), 1),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/large_ruin_02", processorRuinsTerrainMatching), 1),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/large_ruin_03", processorRuinsTerrainMatching), 1),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_01", processorRuinsDecay), 4),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_02", processorRuinsDecay), 4),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_03", processorRuinsDecay), 4),
                         Pair.of(StructurePoolElement.empty(), 5)
                 ),
                 StructureTemplatePool.Projection.RIGID)
@@ -81,6 +84,9 @@ public class VeradexianRuinPools {
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/small_ruin_03", processorRuinsTerrainMatching), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/small_ruin_04", processorRuinsTerrainMatching), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/small_ruin_05", processorRuinsTerrainMatching), 1),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/large_ruin_01", processorRuinsTerrainMatching), 1),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/large_ruin_02", processorRuinsTerrainMatching), 1),
+                        Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/large_ruin_03", processorRuinsTerrainMatching), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_01", processorRuinsDecay), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_02", processorRuinsDecay), 1),
                         Pair.of(AetherIIPools.aetherPool("veradexian_ruins/temperate/ruins/house_03", processorRuinsDecay), 1)
